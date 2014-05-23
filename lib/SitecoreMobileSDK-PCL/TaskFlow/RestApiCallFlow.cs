d63ea71f5d2b33e6d0b3daa3014f80f43385fe90
dodikk <dodikk88.reg@gmail.com>
2014-05-23 17:19:26 +0300

    [Code style] using columns

--- lib/SitecoreMobileSDK-PCL/TaskFlow/RestApiCallFlow.cs
+++ lib/SitecoreMobileSDK-PCL/TaskFlow/RestApiCallFlow.cs
@@ -10,9 +10,9 @@ namespace Sitecore.MobileSDK.TaskFlow
 	{
 		public static async Task<TResult> LoadRequestFromNetworkFlow<TRequest, THttpResult, TResult>(TRequest request, IRestApiCallTasks<TRequest, THttpResult, TResult> stages)
 		{
-			string requestUrl = await stages.BuildRequestUrlForRequestAsync (request);
-			THttpResult serverResponse = await stages.SendRequestForUrlAsync (requestUrl);
-			TResult parsedData = await stages.ParseResponseDataAsync (serverResponse);
+			string requestUrl          = await stages.BuildRequestUrlForRequestAsync (request       );
+			THttpResult serverResponse = await stages.SendRequestForUrlAsync         (requestUrl    );
+			TResult parsedData         = await stages.ParseResponseDataAsync         (serverResponse);
 
 			return parsedData;
 		}
