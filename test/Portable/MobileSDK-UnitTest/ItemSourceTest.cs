d611a44302204434e70be1f9a909a752d5cb1be6
dodikk <dodikk88.reg@gmail.com>
2014-05-26 18:01:32 +0300

    [item source] Added tests for session constructor

--- test/Portable/MobileSDK-UnitTest/ItemSourceTest.cs
+++ test/Portable/MobileSDK-UnitTest/ItemSourceTest.cs
@@ -1,10 +1,44 @@
-﻿using System;
-namespace MobileSDK-UnitTest
+﻿
+
+
+namespace Sitecore.MobileSdkUnitTest
 {
+    using System;
+    using NUnit.Framework;
+    using Sitecore.MobileSDK;
+    using System.Diagnostics;
+
+    [TestFixture]
     public class ItemSourceTest
     {
-        public ItemSourceTest()
+        [Test]
+        public void TestApiSessionConstructorRequiresDefaultSource()
         {
+            SessionConfig config = new SessionConfig ("localhost", "alex.fergusson", "man u is a champion");
+
+            TestDelegate initSessionAction = () =>
+            {
+                ScApiSession result = new ScApiSession (config, null);
+                Debug.WriteLine( result );
+            };
+
+            Assert.Throws<ArgumentNullException>(initSessionAction);
+        }
+
+
+        [Test]
+        public void TestApiSessionConstructorRequiresConfig()
+        {
+            SessionConfig config = new SessionConfig ("localhost", "alex.fergusson", "man u is a champion");
+            ItemSource defaultSource = ItemSource.DefaultSource ();
+
+            TestDelegate initSessionAction = () =>
+            {
+                ScApiSession result = new ScApiSession (null, defaultSource);
+                Debug.WriteLine( result );
+            };
+
+            Assert.Throws<ArgumentNullException>(initSessionAction);
         }
     }
 }
