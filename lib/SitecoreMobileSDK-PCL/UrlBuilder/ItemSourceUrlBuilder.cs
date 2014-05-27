48ca60b957b0fa1b1fc3db53c9c7abeec0be13c2
dodikk <dodikk88.reg@gmail.com>
2014-05-27 16:26:40 +0300

    [item source] Added serialiser class

---
+++ lib/SitecoreMobileSDK-PCL/UrlBuilder/ItemSourceUrlBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sitecore.MobileSDK.UrlBuilder
+{
+    public class ItemSourceUrlBuilder
+    {
+        private ItemSourceUrlBuilder ()
+        {
+        }
+
+        public  ItemSourceUrlBuilder (ItemSource itemSource)
+        {
+            this.itemSource = itemSource;
+            this.Validate ();
+        }
+
+        private void Validate()
+        {
+            if (null == this.itemSource)
+            {
+                throw new ArgumentNullException ("[ItemSourceUrlBuilder.itemSource] Do not pass null");
+            }
+        }
+
+        private ItemSource itemSource;
+    }
+}
+
---
+++ test/Portable/MobileSDK-UnitTest/ItemSourceUrlBuilderTest.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Sitecore.MobileSdkUnitTest
+{
+    using System;
+    using NUnit.Framework;
+
+    using Sitecore.MobileSDK.UrlBuilder;
+
+
+
+    [TestFixture]
+    public class ItemSourceUrlBuilderTest
+    {
+        [Test]
+        public void UrlBuilderRejectsNilSource()
+        {
+            Assert.Throws<ArgumentNullException> (() => new ItemSourceUrlBuilder (null));
+        }
+    }
+}
+
