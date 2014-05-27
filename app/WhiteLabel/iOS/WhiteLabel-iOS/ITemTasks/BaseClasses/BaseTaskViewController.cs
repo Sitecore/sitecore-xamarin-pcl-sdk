c01c2214a8e3a332df06cb0bf7b4d21208a41073
Igor <igor.kh.mail@gmail.com>
2014-05-27 18:13:23 +0300

    base view controller for feature screen added

--- app/WhiteLabel/iOS/WhiteLabel-iOS/ITemTasks/BaseClasses/BaseTaskViewController.cs
+++ app/WhiteLabel/iOS/WhiteLabel-iOS/ITemTasks/BaseClasses/BaseTaskViewController.cs
@@ -9,24 +9,13 @@ namespace WhiteLabeliOS
 {
 	public partial class BaseTaskViewController : UIViewController
 	{
-		public BaseTaskViewController () : base ("BaseTaskViewController", null)
+		public InstanceSettings instanceSettings {get; set;}
+
+		public BaseTaskViewController (IntPtr handle) : base (handle)
 		{
+
 		}
 
-		public override void DidReceiveMemoryWarning ()
-		{
-			// Releases the view if it doesn't have a superview.
-			base.DidReceiveMemoryWarning ();
-			
-			// Release any cached data, images, etc that aren't in use.
-		}
-
-		public override void ViewDidLoad ()
-		{
-			base.ViewDidLoad ();
-			
-			// Perform any additional setup after loading the view, typically from a nib.
-		}
 	}
 }
 
