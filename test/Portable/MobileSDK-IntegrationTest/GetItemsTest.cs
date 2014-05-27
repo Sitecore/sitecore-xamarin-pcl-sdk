387ff766701d43318472e853f714bd3043e1ffe8
dodikk <dodikk88.reg@gmail.com>
2014-05-27 18:42:12 +0300

    Added missing args

--- test/Portable/MobileSDK-IntegrationTest/GetItemsTest.cs
+++ test/Portable/MobileSDK-IntegrationTest/GetItemsTest.cs
@@ -1,23 +1,23 @@
-﻿using NUnit.Framework;
-using System;
-using Sitecore.MobileSDK;
-using System.Net.Http;
-using Sitecore.MobileSDK.Items;
-
+﻿
 namespace MobileSDKIntegrationTest
 {
+    using System;
+    using NUnit.Framework;
+
+    using Sitecore.MobileSDK;
+    using Sitecore.MobileSDK.Items;
+
+
 	[TestFixture]
 	public class GetItemsTest
 	{
 		private ScApiSession sessionWithAnonymousAccess;
 
-		private HttpClient httpClient;
-
 		[SetUp]
 		public void Setup()
 		{
 			SessionConfig config = new SessionConfig("http://mobiledev1ua1.dk.sitecore.net:7119", "extranet\\creatorex", "creatorex");
-			this.sessionWithAnonymousAccess = new ScApiSession(config);
+            this.sessionWithAnonymousAccess = new ScApiSession(config, ItemSource.DefaultSource());
 		}
 
 		[Test]
