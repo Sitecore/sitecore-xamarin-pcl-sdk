416a45371471baa7827ffa466a6c46f09e7c3730
dodikk <dodikk88.reg@gmail.com>
2014-05-27 18:24:00 +0300

    Resolved item source type conflict

--- lib/SitecoreMobileSDK-PCL/Items/ItemsSource.cs
+++ lib/SitecoreMobileSDK-PCL/Items/ItemsSource.cs
@@ -1,21 +1,51 @@
 ﻿using System;
 
-namespace Sitecore.MobileSDK
+namespace Sitecore.MobileSDK.Items
 {
-	public class ItemSource
-	{
-		public string Database { get; private set; }
+    public class ItemSource
+    {
+        public static ItemSource DefaultSource()
+        {
+            return new ItemSource ("web", "en", null);
+        }
 
-		public string Language { get; private set; }
+        public ItemSource ( string database, string language, string version = null )
+        {
+            this.Database = database;
+            this.Language = language;
+            this.Version  = version ;
 
-		public int Version { get; private set; }
+            this.Validate ();
+        }
 
-		public ItemSource (string database, string language, int version)
-		{
-			this.Database = database;
-			this.Language = language;
-			this.Version = version;
-		}
-	}
+        private void Validate()
+        {
+            if (null == this.Database)
+            {
+                throw new ArgumentNullException ("[ItemSource.Database] Do not pass null to constructor");
+            }
+            else if (null == this.Language)
+            {
+                throw new ArgumentNullException ("[ItemSource.Language] Do not pass null to constructor");
+            }
+        }
+
+        public string Database  { get; private set; }
+        public string Language  { get; private set; }
+        public string Version   { get; private set; }
+
+        public int VersionNumber 
+        { 
+            get 
+            { 
+                if (null == this.Version)
+                {
+                    throw new ArgumentNullException ("[ItemSource.VersionNumber] : Cannot convert nil version to int");
+                }
+
+                return Convert.ToInt32 (this.Version); 
+            } 
+        }
+    } 
 }
 
