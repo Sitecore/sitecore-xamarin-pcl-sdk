56494a3a824c3fde23f7596010930375a827fb1f
Alina Zozulya <alinka.zozulya@gmail.com>
2014-05-28 15:44:38 +0300

    tests for Get public key added

--- test/Portable/MobileSDK-IntegrationTest/GetPublicKeyTest.cs
+++ test/Portable/MobileSDK-IntegrationTest/GetPublicKeyTest.cs
@@ -1,8 +1,13 @@
-﻿namespace MobileSDKIntegrationTest
+﻿using System;
+using System.Security;
+using NUnit.Framework.Constraints;
+
+namespace MobileSDKIntegrationTest
 {
     using NUnit.Framework;
 
     using System.Net.Http;
+    using System.Xml;
 
     using Sitecore.MobileSDK;
     using Sitecore.MobileSDK.Items;
@@ -59,8 +64,52 @@
             string response = await this.httpClient.GetStringAsync(url);
 
 
-            string expectedResponse = "{\"statusCode\":200,\"result\":{\"totalCount\":1,\"resultCount\":1,\"items\":[{\"Category\":\"Content\",\"Database\":\"web\",\"DisplayName\":\"Home\",\"HasChildren\":true,\"Icon\":\"/temp/IconCache/Network/16x16/home.png\",\"ID\":\"{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}\",\"Language\":\"en\",\"LongID\":\"/{11111111-1111-1111-1111-111111111111}/{0DE95AE4-41AB-4D01-9EB0-67441B7C2450}/{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}\",\"MediaUrl\":\"/~/icon/Network/48x48/home.png.aspx\",\"Name\":\"Home\",\"Path\":\"/sitecore/content/Home\",\"Template\":\"Sample/Sample Item\",\"TemplateId\":\"{76036F5E-CBCE-46D1-AF0A-4143F9B557AA}\",\"TemplateName\":\"Sample Item\",\"Url\":\"~/link.aspx?_id=110D559FDEA542EA9C1C8A5DF7E70EF9\\u0026amp;_z=z\",\"Version\":1,\"Fields\":{\"{75577384-3C97-45DA-A847-81B00500E250}\":{\"Name\":\"Title\",\"Type\":\"text\",\"Value\":\"Sitecore master\"},\"{A60ACD61-A6DB-4182-8329-C957982CEC74}\":{\"Name\":\"Text\",\"Type\":\"Rich Text\",\"Value\":\"\\u003cdiv\\u003eWelcome to Sitecore!\\u003c/div\\u003e\\n\\u003cdiv\\u003e\\u003cbr /\\u003e\\n\\u003c/div\\u003e\\n\\u003ca href=\\\"~/link.aspx?_id=A2EE64D5BD7A4567A27E708440CAA9CD\\u0026amp;_z=z\\\"\\u003eAccelerometer\\u003c/a\\u003e\"}}}]}}";
-            Assert.AreEqual(response, expectedResponse);
+            string expectedResponse = "{\"statusCode\":200,\"result\":{\"totalCount\":1,\"resultCount\":1,\"items\":";
+            StringAssert.Contains(expectedResponse, response);
+        }
+
+        [Test]
+        public async void TestGetPublicKeyWithInvalidInstanceUrl()
+        {
+            SessionConfig config = new SessionConfig("http://mobiledev1ua1.dddk.sitecore.net", "sitecore\\admin", "b");
+            ScApiSession session = new ScApiSession(config, ItemSource.DefaultSource());
+
+            TestDelegate action = async () =>
+            {
+                var response = await session.GetItemById("{76036F5E-CBCE-46D1-AF0A-4143F9B557AA}");
+            };
+
+            XmlException exception = Assert.Throws<XmlException>(action, "we should get error here");
+        }
+
+        [Test]
+        public async void TestGetPublicKeyWithNullInstanceUrl()
+        {
+            SessionConfig config = new SessionConfig(null, "sitecore\\admin", "b");
+            ScApiSession session = new ScApiSession(config, ItemSource.DefaultSource());
+
+            TestDelegate action = async () =>
+            {
+                var response = await session.GetItemById("{76036F5E-CBCE-46D1-AF0A-4143F9B557AA}");
+            };
+
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException>(action, "we should get error here");
+            StringAssert.Contains("An invalid request URI was provided.", exception.GetBaseException().ToString());
+        }
+
+        [Test]
+        public async void TestGetPublicKeyWithNullItemsSource()
+        {
+            SessionConfig config = new SessionConfig(null, "sitecore\\admin", "b");
+            
+
+            TestDelegate action = async () =>
+            {
+                ScApiSession session = new ScApiSession(config, null);
+            };
+
+            ArgumentNullException exception = Assert.Throws<ArgumentNullException>(action, "we should get error here");
+            StringAssert.Contains("ScApiSession.defaultSource cannot be null", exception.GetBaseException().ToString());
         }
     }
 }
