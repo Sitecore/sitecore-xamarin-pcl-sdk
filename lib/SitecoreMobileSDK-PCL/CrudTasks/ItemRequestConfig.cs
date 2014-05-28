60cd5480dca347951b4996484cd1f48394ccdccc
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2014-05-28 12:14:30 +0300

    [get item] Extracted classes to a separate directory

---
+++ lib/SitecoreMobileSDK-PCL/CrudTasks/ItemRequestConfig.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Sitecore.MobileSDK.CrudTasks
+{
+	using System;
+	using Sitecore.MobileSDK.PublicKey;
+
+
+	public class ItemRequestConfig
+	{
+		public string Id { get; private set; }
+		public string InstanceUrl { get; private set; }
+		public ICredentialsHeadersCryptor CredentialsCryptor { get; private set; }
+		
+
+
+		public ItemRequestConfig (string instanceUrl, string itemId, ICredentialsHeadersCryptor cryptor)
+		{
+			this.Id = itemId;
+			this.InstanceUrl = instanceUrl;
+			this.CredentialsCryptor = cryptor;
+
+			this.Validate ();
+		}
+
+		private void Validate()
+		{
+			// TODO : implement me
+		}
+	}
+}
+
