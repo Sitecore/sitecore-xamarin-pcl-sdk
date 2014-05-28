394a95ecbfbfa96031d5f37acd57b3126563f717
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2014-05-28 12:20:56 +0300

    [get item] Fixed types in flow

--- lib/SitecoreMobileSDK-PCL/TaskFlow/IRestApiCallTasks.cs
+++ lib/SitecoreMobileSDK-PCL/TaskFlow/IRestApiCallTasks.cs
@@ -5,11 +5,11 @@ namespace Sitecore.MobileSDK.TaskFlow
     using System.Threading.Tasks;
 
 
-    public interface IRestApiCallTasks<TRequest, THttpResult, TResult>
+	public interface IRestApiCallTasks<TRequest, THttpRequest, THttpResult, TResult>
     {
-        Task<string> BuildRequestUrlForRequestAsync(TRequest request);
+		Task<THttpRequest> BuildRequestUrlForRequestAsync(TRequest request);
 
-        Task<THttpResult> SendRequestForUrlAsync(string requestUrl);
+		Task<THttpResult> SendRequestForUrlAsync(THttpRequest requestUrl);
         
         Task<TResult> ParseResponseDataAsync(THttpResult httpData);
     }
