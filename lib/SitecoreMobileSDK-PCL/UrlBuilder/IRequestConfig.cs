340f5050de640f2c53f3e87567b5af0e787a6ce0
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2014-05-28 15:48:54 +0300

    [URL builder] Implemented basic URL building flow.

---
+++ lib/SitecoreMobileSDK-PCL/UrlBuilder/IRequestConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Sitecore.MobileSDK.UrlBuilder
+{
+    using System;
+
+    public interface IRequestConfig
+    {
+        string InstanceUrl { get; set; }
+        string WebApiVersion { get; }
+        string ItemId { get; }
+    }
+}
---
+++ lib/SitecoreMobileSDK-PCL/UrlBuilder/WebApiUrlBuilder.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Sitecore.MobileSDK.UrlBuilder
+{
+    using System;
+
+    public class WebApiUrlBuilder
+    {
+        public WebApiUrlBuilder()
+        {
+        }
+
+        public string GetUrlForRequest(IRequestConfig request)
+        {
+            this.ValidateRequest(request);
+
+            string escapedId = Uri.EscapeDataString(request.ItemId);
+
+            if (!request.InstanceUrl.StartsWith("https://") && !request.InstanceUrl.StartsWith("http://"))
+            {
+                request.InstanceUrl = request.InstanceUrl.Insert(0, "http://");
+            }
+
+            string result = request.InstanceUrl + "/-/item/" + request.WebApiVersion + "?sc_itemid=" + escapedId;
+            return result.ToLower();
+        }
+
+        private void ValidateRequest(IRequestConfig request)
+        {
+            if (null == request)
+            {
+                throw new ArgumentNullException("WebApiUrlBuilder.GetUrlForRequest() : do not pass null");
+            }
+        }
+    }
+}
