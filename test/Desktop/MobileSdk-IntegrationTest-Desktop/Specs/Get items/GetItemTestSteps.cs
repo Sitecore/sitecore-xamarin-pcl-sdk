b906f7f16d16cac2a4b24341ad0e173b618d1bd6
Alina Zozulya <alinka.zozulya@gmail.com>
2014-05-29 17:20:50 +0300

    get public key test for specflow added

--- test/Desktop/MobileSdk-IntegrationTest-Desktop/Specs/Get items/GetItemTestSteps.cs
+++ test/Desktop/MobileSdk-IntegrationTest-Desktop/Specs/Get items/GetItemTestSteps.cs
@@ -12,7 +12,7 @@ namespace MobileSdk_IntegrationTest_Desktop
     [Binding]
     public class GetItemTestSteps
     {
-        private readonly string _instanceUrl = ConfigurationManager.AppSettings["anonymousInstanceURL"];
+        private readonly string _instanceUrl = ConfigurationManager.AppSettings["authenticatedInstanceURL"];
         private const string HomeItemId = "{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}";
 
         [Given(@"I logged in as sitecore admin user")]
@@ -23,17 +23,17 @@ namespace MobileSdk_IntegrationTest_Desktop
         }
 
         [When(@"I send request to get Home item by ID")]
-        public async void WhenISendRequestToGetHomeItemById()
+        public void WhenISendRequestToGetHomeItemById()
         {
             
             var apiSession = ScenarioContext.Current.Get<ScApiSession>("ApiSession");
-            ScenarioContext.Current["Response"] = await apiSession.GetItemById(HomeItemId);
+            ScenarioContext.Current["Response"] = apiSession.GetItemById(HomeItemId).Result;
         }
 
         [Then(@"I've got one item in 'Response'")]
         public void ThenIVeGotOneItemInResponse()
         {
-            Thread.Sleep(1000); //how can we avoid delays?!!! does specflow support async operations?
+            //Thread.Sleep(1000); //how can we avoid delays?!!! does specflow support async operations?
             var response = ScenarioContext.Current.Get<ScItemsResponse>("Response");
             Assert.AreEqual(1, response.TotalCount);
             Assert.AreEqual(1, response.Items.Count);
---
+++ test/Desktop/MobileSdk-IntegrationTest-Desktop/Specs/Get items/GetPublicKeyTest.feature
@@ -0,0 +1,10 @@
+﻿Feature: GetPublicKeyTest
+	In order to get data from sitecore securely
+	As a daveloper
+	I want to get public key
+
+@mytag
+Scenario: Get item with correct authentication parameters 
+	Given I logged in as sitecore admin user
+	When I send request to get Home item by ID
+	Then I've got one item in 'Response'
