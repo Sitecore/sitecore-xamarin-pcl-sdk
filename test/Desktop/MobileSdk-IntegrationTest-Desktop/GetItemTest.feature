1444da9c50e1e6d6899c50b751bc73af512666cd
Alina Zozulya <alinka.zozulya@gmail.com>
2014-05-29 16:24:28 +0300

    specflow tests imported

---
+++ test/Desktop/MobileSdk-IntegrationTest-Desktop/GetItemTest.feature
@@ -0,0 +1,10 @@
+﻿Feature: GetItemTest
+	In order to get data from sitecore to mobile app
+	As a daveloper
+	I want to get item using Mobile SDK
+
+@mytag
+Scenario: Get Item by ID
+	Given I logged in as sitecore admin user
+	When I send request to get Home item by ID
+	Then I've got the Home item in response
---
+++ test/Desktop/MobileSdk-IntegrationTest-Desktop/GetItemTestSteps.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Configuration;
+using System.Threading;
+using NUnit.Framework;
+
+using Sitecore.MobileSDK;
+using Sitecore.MobileSDK.Items;
+using TechTalk.SpecFlow;
+
+namespace MobileSdk_IntegrationTest_Desktop
+{
+    [Binding]
+    public class GetItemTestSteps
+    {
+        private readonly string _instanceUrl = ConfigurationManager.AppSettings["anonymousInstanceURL"];
+
+        [Given(@"I logged in as sitecore admin user")]
+        public void GivenILoggedInAsSitecoreAdminUser()
+        {
+            SessionConfig config = new SessionConfig(_instanceUrl, "sitecore\\admin", "b");
+            ScenarioContext.Current["ApiSession"] = new ScApiSession(config, ItemSource.DefaultSource());
+        }
+
+        [When(@"I send request to get Home item by ID")]
+        public async void WhenISendRequestToGetHomeItemById()
+        {
+            const string homeItemId = "{3D6658D8-A0BF-4E75-B3E2-D050FABCF4E1}";
+            var apiSession = ScenarioContext.Current.Get<ScApiSession>("ApiSession");
+            ScenarioContext.Current["Response"] = await apiSession.GetItemById(homeItemId);
+        }
+
+        [Then(@"I've got the Home item in response")]
+        public void ThenIVeGotTheHomeItemInResponse()
+        {
+            Thread.Sleep(1000); //how can we avoid delays?!!! does specflow support async operations?
+            var response = ScenarioContext.Current.Get<ScItemsResponse>("Response");
+            Assert.AreEqual(1, response.TotalCount);
+            Assert.AreEqual(1, response.Items.Count);
+        }
+    }
+}
