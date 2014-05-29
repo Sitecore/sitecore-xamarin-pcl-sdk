1fe43fea8c94867f31ca45bea6f920c117084f87
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2014-05-29 17:48:21 +0300

    [url builder] Added url builder for get item by path request.

---
+++ lib/SitecoreMobileSDK-PCL/Items/ReadItemByPathParameters.cs
@@ -0,0 +1,24 @@
+﻿namespace Sitecore.MobileSDK.Items
+{
+    using Sitecore.MobileSDK.PublicKey;
+    using Sitecore.MobileSDK.UrlBuilder;
+
+    public class ReadItemByPathParameters : IRequestConfig
+    {
+        public ReadItemByPathParameters(string instanceUrl, string webApiVersion, string itemPath, ICredentialsHeadersCryptor cryptor)
+        {
+            this.InstanceUrl = instanceUrl;
+            this.WebApiVersion = webApiVersion;
+            this.ItemPath = itemPath;
+            this.CredentialsHeadersCryptor = cryptor;
+        }
+
+        public string InstanceUrl { get; set; }
+
+        public string WebApiVersion { get; private set; }
+
+        public string ItemPath { get; private set; }
+
+        public ICredentialsHeadersCryptor CredentialsHeadersCryptor { get; private set; }
+    }
+}
---
+++ lib/SitecoreMobileSDK-PCL/UrlBuilder/ItemPathUrlBuilder.cs
@@ -0,0 +1,40 @@
+﻿namespace Sitecore.MobileSDK.UrlBuilder
+{
+    using System;
+    using Sitecore.MobileSDK.Items;
+
+    public class ItemPathUrlBuilder : WebApiUrlBuilder
+    {
+        public ItemPathUrlBuilder(IRestServiceGrammar restGrammar, IWebApiUrlParameters webApiGrammar)
+            : base(restGrammar, webApiGrammar)
+        {
+        }
+
+        public void ValidatePath(string itemPath)
+        {
+            if (string.IsNullOrEmpty(itemPath))
+            {
+                throw new ArgumentNullException("ItemPathUrlBuilder.GetUrlForRequest() : item path cannot be null or empty");
+            }
+
+            if (!itemPath.StartsWith("/"))
+            {
+                throw new ArgumentException("ItemPathUrlBuilder.GetUrlForRequest() : item path should begin with '/'");
+            }
+        }
+
+        public override string GetUrlForRequest(IRequestConfig request)
+        {
+            string result = base.GetUrlForRequest(request);
+            
+            ReadItemByPathParameters config = (ReadItemByPathParameters)request;
+
+            this.ValidatePath(config.ItemPath);
+
+            string escapedPath = Uri.EscapeUriString(config.ItemPath);
+
+            result += escapedPath;
+            return result.ToLowerInvariant();
+        }
+    }
+}
