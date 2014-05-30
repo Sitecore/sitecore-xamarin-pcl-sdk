6a8e45d364c7a03fa2b615763a185c646a33113b
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2014-05-30 13:55:34 +0300

    [tests] Added tests for session config builder.

---
+++ test/Desktop/MobileSDK-UnitTest-Desktop/SessionConfigUrlBuilderTest.cs
@@ -0,0 +1,24 @@
+﻿
+namespace MobileSDK_UnitTest_Desktop
+{
+    using System;
+    using NUnit.Framework;
+    using Sitecore.MobileSDK.SessionSettings;
+    using Sitecore.MobileSDK.UrlBuilder;
+
+
+    [TestFixture]
+    public class SessionConfigUrlBuilderTest
+    {
+
+        [Test]
+        public void TestBuildBaseUrlWithSite()
+        {
+            SessionConfigUrlBuilder builder = new SessionConfigUrlBuilder(RestServiceGrammar.ItemWebApiV2Grammar(), WebApiUrlParameters.ItemWebApiV2UrlParameters());
+            ISessionConfig data = new SessionConfig("localhost", "admin", "b", "/sitecore/shell", "v1");
+
+            string result = builder.BuildUrlString(data);
+            string expected = "http://localhost/sitecore/shell/-/item/v1";
+        }
+    }
+}
