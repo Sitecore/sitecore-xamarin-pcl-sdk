97555f751de7b0bef2aceed29e3e6aa5cf8466f9
dodikk <dodikk88.reg@gmail.com>
2014-05-30 19:05:12 +0300

    [item path] Enabled tests

--- test/Portable/MobileSDK-UnitTest/ItemByPathUrlBuilderTests.cs
+++ test/Portable/MobileSDK-UnitTest/ItemByPathUrlBuilderTests.cs
@@ -20,7 +20,12 @@
             IWebApiUrlParameters webApiGrammar = WebApiUrlParameters.ItemWebApiV2UrlParameters();
 
             this.builder = new ItemByPathUrlBuilder(restGrammar, webApiGrammar);
-            this.sessionConfig = new SessionConfig("http://sitecore.net", "admin", "b");
+
+            SessionConfigPOD mutableSession = new SessionConfigPOD ();
+            mutableSession.InstanceUrl = "http://mobiledev1ua1.dk.sitecore.net";
+            mutableSession.ItemWebApiVersion = "v2";
+            mutableSession.Site = "";
+            this.sessionConfig = mutableSession;
         }
 
         [TearDown]
@@ -29,54 +34,72 @@
             this.builder = null;
             this.sessionConfig = null;
         }
-//
-//        [Test]
-//        public void TestBuildWithValidPath()
-//        {
-//            IGetItemByPathRequest request = null;
-//
-//            ReadItemByPathParameters parameters = new ReadItemByPathParameters("http://mobiledev1ua1.dk.sitecore.net", "v2", "/path/to/item", null);
-//
-//            string result = this.builder.GetUrlForRequest(parameters);
-//            string expected = "http://mobiledev1ua1.dk.sitecore.net/-/item/v2/path/to/item";
-//
-//            Assert.AreEqual(expected, result);
-//        }
-//
-//        [Test]
-//        public void TestBuildWithUnEscapedPath()
-//        {
-//            ReadItemByPathParameters parameters = new ReadItemByPathParameters("http://mobiledev1ua1.dk.sitecore.net", "v2", "/path to item", null);
-//
-//            string result = this.builder.GetUrlForRequest(parameters);
-//            string expected = "http://mobiledev1ua1.dk.sitecore.net/-/item/v2/path%20to%20item";
-//
-//            Assert.AreEqual(expected, result);
-//        }
-//
-//        [Test]
-//        public void TestBuildWithEmptyConfig()
-//        {
-//            TestDelegate action = () => this.builder.GetUrlForRequest(null);
-//            Assert.Throws<ArgumentNullException>(action);
-//        }
-//
-//        [Test]
-//        public void TestBuildWithInvalidPath()
-//        {
-//            ReadItemByPathParameters parameters = new ReadItemByPathParameters("http://mobiledev1ua1.dk.sitecore.net", "v2", "path", null);
-//
-//            TestDelegate action = () => this.builder.GetUrlForRequest(parameters);
-//            Assert.Throws<ArgumentException>(action);
-//        }
-//
-//        [Test]
-//        public void TestBuildWithEmptyPath()
-//        {
-//            ReadItemByPathParameters parameters = new ReadItemByPathParameters("http://mobiledev1ua1.dk.sitecore.net", "v2", "", null);
-//
-//            TestDelegate action = () => this.builder.GetUrlForRequest(parameters);
-//            Assert.Throws<ArgumentNullException>(action);
-//        }
+
+        [Test]
+        public void TestBuildWithValidPath()
+        {
+            MockGetItemsByPathParameters mutableParameters = new MockGetItemsByPathParameters ();
+            mutableParameters.ItemSource = ItemSource.DefaultSource ();
+            mutableParameters.ItemPath = "/path/TO/iTEm";
+            mutableParameters.SessionSettings = this.sessionConfig;
+
+            IGetItemByPathRequest request = mutableParameters;
+
+            string result = this.builder.GetUrlForRequest(request);
+            string expected = "http://mobiledev1ua1.dk.sitecore.net/-/item/v2%2fpath%2fto%2fitem?sc_database=web&sc_lang=en";
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestBuildWithUnEscapedPath()
+        {
+            MockGetItemsByPathParameters mutableParameters = new MockGetItemsByPathParameters ();
+            mutableParameters.ItemSource = ItemSource.DefaultSource ();
+            mutableParameters.ItemPath = "/path TO iTEm";
+            mutableParameters.SessionSettings = this.sessionConfig;
+
+            IGetItemByPathRequest request = mutableParameters;
+
+            string result = this.builder.GetUrlForRequest(request);
+            string expected = "http://mobiledev1ua1.dk.sitecore.net/-/item/v2%2fpath%20to%20item?sc_database=web&sc_lang=en";
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestBuildWithEmptyConfig()
+        {
+            TestDelegate action = () => this.builder.GetUrlForRequest(null);
+            Assert.Throws<ArgumentNullException>(action);
+        }
+
+        [Test]
+        public void TestPathMustStartWithSlash()
+        {
+            MockGetItemsByPathParameters mutableParameters = new MockGetItemsByPathParameters ();
+            mutableParameters.ItemSource = ItemSource.DefaultSource ();
+            mutableParameters.ItemPath = "path without starting slash";
+            mutableParameters.SessionSettings = this.sessionConfig;
+
+            IGetItemByPathRequest request = mutableParameters;
+
+            TestDelegate action = () => this.builder.GetUrlForRequest(request);
+            Assert.Throws<ArgumentException>(action);
+        }
+
+        [Test]
+        public void TestBuildWithEmptyPathCausesArgumentException()
+        {
+            MockGetItemsByPathParameters mutableParameters = new MockGetItemsByPathParameters ();
+            mutableParameters.ItemSource = ItemSource.DefaultSource ();
+            mutableParameters.ItemPath = "";
+            mutableParameters.SessionSettings = this.sessionConfig;
+
+            IGetItemByPathRequest request = mutableParameters;
+
+            TestDelegate action = () => this.builder.GetUrlForRequest(request);
+            Assert.Throws<ArgumentException>(action);
+        }
     }
 }
