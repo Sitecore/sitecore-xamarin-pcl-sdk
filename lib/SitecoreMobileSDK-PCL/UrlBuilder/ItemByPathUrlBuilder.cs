70a808e37ffbe05630feb2f2b83307aacdee4052
dodikk <dodikk88.reg@gmail.com>
2014-05-30 19:07:10 +0300

    [item path] Composition instead of inheritance

--- lib/SitecoreMobileSDK-PCL/UrlBuilder/ItemByPathUrlBuilder.cs
+++ lib/SitecoreMobileSDK-PCL/UrlBuilder/ItemByPathUrlBuilder.cs
@@ -1,4 +1,6 @@
-﻿namespace Sitecore.MobileSDK.UrlBuilder
+﻿using Sitecore.MobileSDK.SessionSettings;
+
+namespace Sitecore.MobileSDK.UrlBuilder
 {
     using System;
     using Sitecore.MobileSDK.Items;
@@ -13,36 +15,64 @@
             this.Validate();
         }
 
-        public void ValidatePath(string itemPath)
+        public string GetUrlForRequest(IGetItemByPathRequest request)
         {
-            if (string.IsNullOrEmpty(itemPath))
+            this.ValidateRequest (request);
+            string escapedPath = Uri.EscapeDataString(request.ItemPath);
+
+            SessionConfigUrlBuilder sessionBuilder = new SessionConfigUrlBuilder(this.restGrammar, this.webApiGrammar);
+            string result = sessionBuilder.BuildUrlString(request.SessionSettings);
+
+            ItemSourceUrlBuilder sourceBuilder = new ItemSourceUrlBuilder (this.restGrammar, this.webApiGrammar, request.ItemSource);
+            string itemSourceArgs = sourceBuilder.BuildUrlQueryString ();
+
+            result += 
+                escapedPath + 
+                this.restGrammar.HostAndArgsSeparator + 
+                itemSourceArgs;
+
+            return result.ToLowerInvariant();
+        }
+
+        private void ValidateRequest(IGetItemByPathRequest request)
+        {
+            if (null == request)
             {
-                throw new ArgumentNullException("ItemByPathUrlBuilder.GetUrlForRequest() : item path cannot be null or empty");
+                throw new ArgumentNullException("ItemByPathUrlBuilder.GetUrlForRequest() : request cannot be null");
             }
 
-            if (!itemPath.StartsWith("/"))
+            this.ValidatePath (request.ItemPath);
+        }
+
+        private void ValidatePath(string itemPath)
+        {
+            if (null == itemPath)
+            {
+                throw new ArgumentNullException ("ItemByPathUrlBuilder.GetUrlForRequest() : item path cannot be null or empty");
+            }
+            else if (string.Empty.Equals (itemPath))
+            {
+                throw new ArgumentException ("ItemByPathUrlBuilder.GetUrlForRequest() : item path cannot be null or empty");
+            }
+            else if (!itemPath.StartsWith("/"))
             {
                 throw new ArgumentException("ItemByPathUrlBuilder.GetUrlForRequest() : item path should begin with '/'");
             }
         }
-
-        public string GetUrlForRequest(IGetItemByPathRequest request)
-        {
-            return null;
-            //            ReadItemByPathParameters config = (ReadItemByPathParameters)request;
-            //
-            //            this.ValidatePath(config.ItemPath);
-            //
-            //            string escapedPath = Uri.EscapeUriString(config.ItemPath);
-            //
-            //            result += escapedPath;
-            //            return result.ToLowerInvariant();
-        }
-
+            
         private void Validate()
         {
-            // TODO : implement me
+            if (null == this.restGrammar)
+            {
+                throw new ArgumentNullException ("[SessionConfigUrlBuilder] restGrammar cannot be null");
+            }
+            else if (null == this.webApiGrammar)
+            {
+                throw new ArgumentNullException ("[SessionConfigUrlBuilder] webApiGrammar cannot be null");
+            }
         }
+
+
 
         private IRestServiceGrammar restGrammar;
         private IWebApiUrlParameters webApiGrammar;
