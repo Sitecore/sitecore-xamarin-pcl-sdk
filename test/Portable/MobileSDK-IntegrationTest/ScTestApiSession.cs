825b0a4acc1db20485f07c072b33e7dc07bc09d4
dodikk <dodikk88.reg@gmail.com>
2014-06-02 16:56:09 +0300

    [test] Added a sub-class to expose private API

---
+++ test/Portable/MobileSDK-IntegrationTest/ScTestApiSession.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Sitecore.MobileSDK;
+using Sitecore.MobileSDK.SessionSettings;
+using Sitecore.MobileSDK.Items;
+using System.Threading.Tasks;
+
+namespace MobileSDKIntegrationTest
+{
+    public class ScTestApiSession : ScApiSession
+    {
+        public ScTestApiSession(SessionConfig config, ItemSource defaultSource) : 
+        base( config, defaultSource)
+        {
+        }
+
+        public async Task<PublicKeyX509Certificate> GetPublicKeyAsync()
+        {
+            return await base.GetPublicKeyAsync ();
+        }
+    }
+}
+
