610371c70b2f7aad3b307c1a9a573d29aba1f2af
Igor <igor.kh.mail@gmail.com>
2014-06-02 11:43:15 +0300

    Merge branch 'master' into igk-1-DEMOUI
    Conflicts:
    	lib/SitecoreMobileSDK-PCL/CrudTasks/AbstractGetItemTask.cs
    	lib/SitecoreMobileSDK-PCL/ScApiSession.cs
    	lib/SitecoreMobileSDK-PCL/SitecoreMobileSDK.csproj
    	test/Portable/MobileSDK-IntegrationTest/GetItemsTest.cs
    
    conflicts resolved

--- lib/SitecoreMobileSDK-PCL/CrudTasks/AbstractGetItemTask.cs
+++ lib/SitecoreMobileSDK-PCL/CrudTasks/AbstractGetItemTask.cs
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:lib/SitecoreMobileSDK-PCL/CrudTasks/GetItemsTasks.cs
-﻿
-=======
 ﻿using Sitecore.MobileSDK.PublicKey;
->>>>>>> master:lib/SitecoreMobileSDK-PCL/CrudTasks/AbstractGetItemTask.cs
 
 namespace Sitecore.MobileSDK.CrudTasks
 {
@@ -17,25 +13,12 @@ namespace Sitecore.MobileSDK.CrudTasks
     using Sitecore.MobileSDK.TaskFlow;
     using Sitecore.MobileSDK.PublicKey;
 
-
-<<<<<<< HEAD:lib/SitecoreMobileSDK-PCL/CrudTasks/GetItemsTasks.cs
-    public class GetItemsTasks : IRestApiCallTasks<ItemRequestConfig, HttpRequestMessage, string, ScItemsResponse>
-    {
-        public GetItemsTasks(HttpClient httpClient)
-        {
-            this.httpClient = httpClient;
-        }
-
-        #region  IRestApiCallTasks
-        public async Task<HttpRequestMessage> BuildRequestUrlForRequestAsync(ItemRequestConfig request)
-=======
     public abstract class AbstractGetItemTask<TRequest> : IRestApiCallTasks<TRequest, HttpRequestMessage, string, ScItemsResponse>
         where TRequest : ICredentialCryptorOwner
     {
         #region  IRestApiCallTasks
 
         public async Task<HttpRequestMessage> BuildRequestUrlForRequestAsync(TRequest request)
->>>>>>> master:lib/SitecoreMobileSDK-PCL/CrudTasks/AbstractGetItemTask.cs
         {
             string url = this.UrlToGetItemWithRequest(request);
             HttpRequestMessage result = new HttpRequestMessage(HttpMethod.Get, url);
@@ -61,22 +44,9 @@ namespace Sitecore.MobileSDK.CrudTasks
 
         #endregion IRestApiCallTasks
 
-<<<<<<< HEAD:lib/SitecoreMobileSDK-PCL/CrudTasks/GetItemsTasks.cs
-        private string UrlToGetItemWithRequest(ItemRequestConfig request)
-        {
-            //TODO : extract me
-            string result = request.InstanceUrl + "/-/item/v1?" + "sc_itemid=" + Uri.EscapeDataString(request.Id);
-            return result;
-
-            //		    return "http://mobiledev1ua1.dk.sitecore.net:7119/-/item/v1?sc_itemid=%7B3D6658D8-A0BF-4E75-B3E2-D050FABCF4E1%7D";
-        }
-
-        private readonly HttpClient httpClient;
-=======
         protected abstract string UrlToGetItemWithRequest (TRequest request);
 
         protected HttpClient httpClient;
->>>>>>> master:lib/SitecoreMobileSDK-PCL/CrudTasks/AbstractGetItemTask.cs
     }
 }
 
