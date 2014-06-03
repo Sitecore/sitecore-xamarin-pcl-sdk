85c41b6913d4e15557e75f56ca4dfcc3c42bd6e5
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2014-06-03 15:43:37 +0300

    [white label] Added settings button at the top of main screen.

--- lib/FormsApp/FormsApp/App.cs
+++ lib/FormsApp/FormsApp/App.cs
@@ -6,67 +6,8 @@
     {
         public static Page GetMainPage()
         {
-            var useButton = new Button
-            {
-                Text = "Use",
-                TextColor = Color.White,
-                BackgroundColor = Color.FromHex("77D065")
-            };
-
-            var settingsPage = new ContentPage
-            {
-                Title = "Settings",
-                Content = new StackLayout
-                {
-                    Padding = 10,
-                    Spacing = 20,
-                    VerticalOptions = LayoutOptions.Fill,
-                    HorizontalOptions = LayoutOptions.Fill,
-                    Children = 
-                    {
-                        new Label
-                        {
-                            HorizontalOptions = LayoutOptions.Center,
-                            Text  = "Settings"
-                        },
-                        new Entry
-                        {
-                            Placeholder = "Instance URL",
-                            Text = "http://mobiledev1ua1.dk.sitecore.net:722/"
-                        },
-                        new Label
-                        {
-                          Text  = ""
-                        },
-                        new Entry
-                        {
-                            Placeholder = "Username",
-                            Text = "extranet\\creatorex"
-                        },
-                        new Entry
-                        {
-                            Placeholder = "Password", 
-                            IsPassword = true,
-                            Text = "creatorex"
-                        },
-                        new Entry
-                        {
-                            Placeholder = "Site"
-                        },
-                        new Entry
-                        {
-                            Placeholder = "Database",
-                            Text = "web"
-                        },
-                        useButton
-                    }
-                }
-            };
-
-            return settingsPage;
+            var mainNav = new NavigationPage(new MainPage());
+            return mainNav;
         }
     }
 }
-
-
-
---
+++ lib/FormsApp/FormsApp/MainPage.cs
@@ -0,0 +1,34 @@
+﻿namespace FormsApp
+{
+    using Xamarin.Forms;
+
+    public class MainPage : ContentPage
+    {
+        private static Config config = new Config();
+
+        public MainPage()
+        {
+            this.Title = "Main";
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    new Label
+                    {
+                        HorizontalOptions = LayoutOptions.Center,
+                        Text = "Main"
+                    }
+            }
+            };
+
+            // BUG: Android doesn't support the icon being null
+            var tbi = new ToolbarItem("+", "ic_lock_power_off", () =>
+                {
+                    var settings = new SettingsPage(config);
+                    Navigation.PushModalAsync(settings);
+                }, 0, 0);
+
+            this.ToolbarItems.Add(tbi);
+        }
+    }
+}
