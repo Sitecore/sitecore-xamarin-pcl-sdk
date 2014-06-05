a722188550e147a4d1b4be60a70727bd704eebab
dodikk <dodikk88.reg@gmail.com>
2014-06-05 16:55:29 +0300

    [error] Added exception wrapping

--- lib/SitecoreMobileSDK-PCL/TaskFlow/RestApiCallFlow.cs
+++ lib/SitecoreMobileSDK-PCL/TaskFlow/RestApiCallFlow.cs
@@ -1,10 +1,10 @@
 ﻿
-
 namespace Sitecore.MobileSDK.TaskFlow
 {
     using System;
     using System.Threading;
     using System.Threading.Tasks;
+    using Sitecore.MobileSDK.Exceptions;
 
 
     public class RestApiCallFlow
@@ -13,28 +13,97 @@ namespace Sitecore.MobileSDK.TaskFlow
             TRequest request, 
             IRestApiCallTasks<TRequest, THttpRequest, THttpResult, TResult> stages,
             CancellationToken cancelToken)
+        where TRequest: class
+        where THttpRequest: class
+        where THttpResult: class
+        where TResult: class
         {
+            THttpRequest requestUrl = null;
+            THttpResult serverResponse = null;
+            TResult parsedData = null;
+
             if (null == request)
             {
                 throw new ArgumentNullException ("[RestApiCallFlow.LoadRequestFromNetworkFlow] user's request cannot be null");
             }
 
 
-            THttpRequest requestUrl = await stages.BuildRequestUrlForRequestAsync(request, cancelToken);
+            try
+            {
+                requestUrl = await stages.BuildRequestUrlForRequestAsync (request, cancelToken);
+            }
+            catch (ObjectDisposedException)
+            {
+                // CancellationToken.ThrowIfCancellationRequested()
+                throw;
+            }
+            catch (OperationCanceledException)
+            {
+                // CancellationToken.ThrowIfCancellationRequested()
+                // and TaskCanceledException
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw new ProcessUserRequestException ("[Sitecore Mobile SDK] Unable to build HTTP request", ex);
+            }
+            ////
+
+
             if (null == requestUrl)
             {
                 throw new ArgumentNullException ("[RestApiCallFlow.LoadRequestFromNetworkFlow] http request cannot be null");
             }
 
+            try
+            {
+                serverResponse = await stages.SendRequestForUrlAsync (requestUrl, cancelToken);
+            }
+            catch (ObjectDisposedException)
+            {
+                // CancellationToken.ThrowIfCancellationRequested()
+                throw;
+            }
+            catch (OperationCanceledException)
+            {
+                // CancellationToken.ThrowIfCancellationRequested()
+                // and TaskCanceledException
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw new LoadDataFromNetworkException ("[Sitecore Mobile SDK] Unable to download data from the internet", ex);
+            }
+            ////
 
-            THttpResult serverResponse = await stages.SendRequestForUrlAsync(requestUrl, cancelToken);
+
             if (null == serverResponse)
             {
                 throw new ArgumentNullException ("[RestApiCallFlow.LoadRequestFromNetworkFlow] back end response cannot be null");
             }
 
 
-            TResult parsedData = await stages.ParseResponseDataAsync(serverResponse, cancelToken);
+            try
+            {
+                parsedData = await stages.ParseResponseDataAsync (serverResponse, cancelToken);
+            }
+            catch (ObjectDisposedException)
+            {
+                // CancellationToken.ThrowIfCancellationRequested()
+                throw;
+            }
+            catch (OperationCanceledException)
+            {
+                // CancellationToken.ThrowIfCancellationRequested()
+                // and TaskCanceledException
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw new ParserException("[Sitecore Mobile SDK] Unable to download data from the internet", ex);
+            }
+
+
             if (null == parsedData)
             {
                 throw new ArgumentNullException ("[RestApiCallFlow.LoadRequestFromNetworkFlow] parsed response cannot be null");
