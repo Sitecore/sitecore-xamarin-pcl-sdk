8f89e8a3eb3a73ec5367fb4b22b62f4c6128f939
dodikk <dodikk88.reg@gmail.com>
2014-06-05 10:08:56 +0300

    [request builder] Testing validation

--- test/Portable/MobileSDK-UnitTest/ItemWebApiRequestBuilderTest.cs
+++ test/Portable/MobileSDK-UnitTest/ItemWebApiRequestBuilderTest.cs
@@ -14,6 +14,7 @@ namespace Sitecore.MobileSdkUnitTest
     [TestFixture]
     public class ItemWebApiRequestBuilderTest
     {
+        #region ItemId
         [Test]
         public void TestItemIdRequestBuilderWithAllFields()
         {
@@ -37,8 +38,63 @@ namespace Sitecore.MobileSdkUnitTest
             Assert.AreEqual ("1", result.ItemSource.Version);
         }
 
+        [Test]
+        public void TestItemIdRequestBuilderWithIdOnly()
+        {
+            ItemWebApiRequestBuilder builder = new ItemWebApiRequestBuilder();
+
+            IReadItemsByIdRequest result =  builder.RequestWithId ("{abra-kadabra}").Build();
+
+            Assert.IsNotNull (result);
+            Assert.IsNotNull (result.ItemSource);
+            Assert.IsNotNull (result.ItemId);
+            Assert.IsNull (result.SessionSettings);
 
 
+            Assert.AreEqual ("{abra-kadabra}", result.ItemId);
+            Assert.IsNull (result.ItemSource.Language);
+            Assert.IsNull (result.ItemSource.Database);
+            Assert.IsNull (result.ItemSource.Version);
+        }
+
+        [Test]
+        public void TestItemIdRequestBuilderWithNullIdCrashes()
+        {
+            ItemWebApiRequestBuilder builder = new ItemWebApiRequestBuilder();
+            Assert.Throws<ArgumentNullException> (() => builder.RequestWithId (null));
+        }
+
+        [Test]
+        public void TestItemIdRequestBuilderWithEmptyIdCrashes()
+        {
+            ItemWebApiRequestBuilder builder = new ItemWebApiRequestBuilder();
+            Assert.Throws<ArgumentNullException> (() => builder.RequestWithId (""));
+        }
+
+        [Test]
+        public void TestItemIdRequestBuilderWithWhitespaceIdCrashes()
+        {
+            ItemWebApiRequestBuilder builder = new ItemWebApiRequestBuilder();
+            Assert.Throws<ArgumentNullException> (() => builder.RequestWithId ("\t \r \n"));
+        }
+
+        [Test]
+        public void TestItemIdWithoutBracesCrashesBuilder()
+        {
+            ItemWebApiRequestBuilder builder = new ItemWebApiRequestBuilder();
+            Assert.Throws<ArgumentException> (() => builder.RequestWithId ("ololololo"));
+        }
+
+        [Test]
+        public void TestItemIdWithBracesOnlyCrashesBuilder()
+        {
+            ItemWebApiRequestBuilder builder = new ItemWebApiRequestBuilder();
+            Assert.Throws<ArgumentException> (() => builder.RequestWithId ("{}"));
+        }
+        #endregion ItemId
+
+
+        #region ItemPath
         [Test]
         public void TestItemPathRequestBuilderWithAllFields()
         {
@@ -61,8 +117,10 @@ namespace Sitecore.MobileSdkUnitTest
             Assert.AreEqual ("master", result.ItemSource.Database);
             Assert.AreEqual ("100500", result.ItemSource.Version);
         }
+        #endregion ItemPath
 
 
+        #region SitecoreQuery
         [Test]
         public void TestSitecoreQueryRequestBuilderWithAllFields()
         {
@@ -85,6 +143,7 @@ namespace Sitecore.MobileSdkUnitTest
             Assert.AreEqual ("core", result.ItemSource.Database);
             Assert.AreEqual ("341", result.ItemSource.Version);
         }
+        #endregion SitecoreQuery
     }
 }
 
