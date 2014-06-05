ce18578f27de3515f258811699fdf1f72a5d2bec
dodikk <dodikk88.reg@gmail.com>
2014-06-05 15:55:04 +0300

    [cancel] Added cancel exception hadling

--- test/Portable/MobileSDK-UnitTest/PublicKeyParsingTest.cs
+++ test/Portable/MobileSDK-UnitTest/PublicKeyParsingTest.cs
@@ -7,6 +7,7 @@ namespace Sitecore.MobileSdkUnitTest
     using System.Text;
     using System.Xml;
     using System.Threading;
+    using System.Threading.Tasks;
 
     using NUnit.Framework;
     using Sitecore.MobileSDK;
@@ -16,6 +17,8 @@ namespace Sitecore.MobileSdkUnitTest
     public class PublicKeyParsingTest
     {
         private PublicKeyXmlParser parser;
+
+        const string VALID_PUBLIC_KEY_XML = "<RSAKeyValue><Modulus>0jvvnZgV2r8hlQ6rPIFcoQxJntKBnu3dsmPVzv+diFpkEHrQxX1XRz3KK2f4EBqXASEXFQrluJda7c0d82p76HFjcORGqF5/iTvnlEXotzgy+dAa4BGYa//LNp4DFOipfdvGQlN7lZJyRZqaXGVryueyBHK6MiT6KPcoDmZNZN8=</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
 
         [SetUp]
         public void Setup()
@@ -52,7 +55,7 @@ namespace Sitecore.MobileSdkUnitTest
         [Test]
         public void TestXmlParserFetchesValidDataCorrectly()
         {
-            string validXml = "<RSAKeyValue><Modulus>0jvvnZgV2r8hlQ6rPIFcoQxJntKBnu3dsmPVzv+diFpkEHrQxX1XRz3KK2f4EBqXASEXFQrluJda7c0d82p76HFjcORGqF5/iTvnlEXotzgy+dAa4BGYa//LNp4DFOipfdvGQlN7lZJyRZqaXGVryueyBHK6MiT6KPcoDmZNZN8=</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
+            string validXml = VALID_PUBLIC_KEY_XML;
             Stream xmlStream = new MemoryStream(Encoding.UTF8.GetBytes(validXml));
 
             PublicKeyX509Certificate result = this.parser.Parse(xmlStream, CancellationToken.None);
@@ -72,6 +75,26 @@ namespace Sitecore.MobileSdkUnitTest
 
             Assert.AreEqual(result.ModulusInBase64, "0jvvnZgV2r8hlQ6rPIFcoQxJntKBnu3dsmPVzv+diFpkEHrQxX1XRz3KK2f4EBqXASEXFQrluJda7c0d82p76HFjcORGqF5/iTvnlEXotzgy+dAa4BGYa//LNp4DFOipfdvGQlN7lZJyRZqaXGVryueyBHK6MiT6KPcoDmZNZN8=");
             Assert.AreEqual(result.ExponentInBase64, "AQAB");
+        }
+
+        [Test]
+        [ExpectedException(typeof(OperationCanceledException))]
+        public async void TestCancellationCausesOperationCanceledException()
+        {
+            var cancel = new CancellationTokenSource ();
+            Stream xmlStream = new MemoryStream(Encoding.UTF8.GetBytes(VALID_PUBLIC_KEY_XML));
+
+            Task<PublicKeyX509Certificate> action = Task.Factory.StartNew( ()=> 
+            {
+                int millisecondTimeout = 10;
+                Thread.Sleep(millisecondTimeout);    
+
+                return this.parser.Parse(xmlStream, cancel.Token);
+            });
+            cancel.Cancel();
+            await action;
+
+            Assert.Fail ("OperationCanceledException not thrown");
         }
     }
 }
