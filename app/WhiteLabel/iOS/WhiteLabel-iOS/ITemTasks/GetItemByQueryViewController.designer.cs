dbefc73f0abf91f10891898d00907f1f9b7689b2
Igor <igor.kh.mail@gmail.com>
2014-06-05 12:06:35 +0300

    view controller for query task added

--- app/WhiteLabel/iOS/WhiteLabel-iOS/ITemTasks/GetItemByQueryViewController.designer.cs
+++ app/WhiteLabel/iOS/WhiteLabel-iOS/ITemTasks/GetItemByQueryViewController.designer.cs
@@ -1,18 +1,29 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
 
 namespace WhiteLabeliOS
 {
 	[Register ("GetItemByQueryViewController")]
 	partial class GetItemByQueryViewController
 	{
+		[Outlet]
+		MonoTouch.UIKit.UITextField queryTextField { get; set; }
+
+		[Action ("getItem:")]
+		partial void getItem (MonoTouch.Foundation.NSObject sender);
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (queryTextField != null) {
+				queryTextField.Dispose ();
+				queryTextField = null;
+			}
 		}
 	}
 }
