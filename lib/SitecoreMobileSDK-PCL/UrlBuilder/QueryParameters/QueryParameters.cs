f46a31675650ad39f9f1e2a201c2a158bd238c7f
dodikk <dodikk88.reg@gmail.com>
2014-06-10 17:43:47 +0300

    [payload] Made setters private

--- lib/SitecoreMobileSDK-PCL/UrlBuilder/QueryParameters/QueryParameters.cs
+++ lib/SitecoreMobileSDK-PCL/UrlBuilder/QueryParameters/QueryParameters.cs
@@ -1,12 +1,17 @@
-﻿namespace Sitecore.MobileSDK.UrlBuilder.QueryParameters
+﻿
+namespace Sitecore.MobileSDK.UrlBuilder.QueryParameters
 {
+    using System.Collections.Generic;
+
     public class QueryParameters : IQueryParameters
     {
-        public QueryParameters(PayloadType payload)
+        public QueryParameters(PayloadType payload, ICollection<string> fields)
         {
             this.Payload = payload;
+            this.Fields = fields;
         }
 
-        public PayloadType Payload { get; set; }
+        public PayloadType Payload { get; private set; }
+        public ICollection<string> Fields { get; private set; }
     }
 }
