54f3af5bc6195129cf163464cc066cb402b2f02b
Alexander Dodatko <adk@sitecore.net>
2014-06-10 12:00:48 +0300

    [payload] Added "fluent interface" builders

--- lib/SitecoreMobileSDK-PCL/UserRequest/IGetItemRequestParametersBuilder.cs
+++ lib/SitecoreMobileSDK-PCL/UserRequest/IGetItemRequestParametersBuilder.cs
@@ -2,12 +2,15 @@
 
 namespace Sitecore.MobileSDK
 {
+    using Sitecore.MobileSDK.UrlBuilder.QueryParameters;
+
     public interface IGetItemRequestParametersBuilder<T>
         where T : class
     {
         IGetItemRequestParametersBuilder<T> Database (string sitecoreDatabase);
         IGetItemRequestParametersBuilder<T> Language (string itemLanguage);
         IGetItemRequestParametersBuilder<T> Version (string itemVersion);
+        IGetItemRequestParametersBuilder<T> Payload(PayloadType payload);
 
         T Build();
     }
--- lib/SitecoreMobileSDK-PCL/UserRequest/ReadItemByPathRequestBuilder.cs
+++ lib/SitecoreMobileSDK-PCL/UserRequest/ReadItemByPathRequestBuilder.cs
@@ -5,19 +5,20 @@ namespace Sitecore.MobileSDK
 {
     using System;
     using Sitecore.MobileSDK.UrlBuilder.ItemByPath;
+    using Sitecore.MobileSDK.UrlBuilder.QueryParameters;
 
     public class ReadItemByPathRequestBuilder : AbstractGetItemRequestBuilder<IReadItemsByPathRequest>
     {
-        public ReadItemByPathRequestBuilder (string itemPath)
+        public ReadItemByPathRequestBuilder(string itemPath)
         {
-            ItemPathValidator.ValidateItemPath (itemPath);
+            ItemPathValidator.ValidateItemPath(itemPath);
 
             this.itemPath = itemPath;
         }
 
         public override IReadItemsByPathRequest Build()
         {
-            var result = new ReadItemByPathParameters (null, this.itemSourceAccumulator, this.itemPath);
+            var result = new ReadItemByPathParameters(null, this.itemSourceAccumulator, this.queryParameters, this.itemPath);
             return result;
         }
 
