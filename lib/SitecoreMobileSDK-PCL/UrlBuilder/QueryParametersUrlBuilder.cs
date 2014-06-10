12e936e18e522080c7a8176144890bafd519fb35
dodikk <dodikk88.reg@gmail.com>
2014-06-10 14:01:32 +0300

    [payload] Switch case statement refactoring

--- lib/SitecoreMobileSDK-PCL/UrlBuilder/QueryParametersUrlBuilder.cs
+++ lib/SitecoreMobileSDK-PCL/UrlBuilder/QueryParametersUrlBuilder.cs
@@ -1,6 +1,7 @@
 ﻿namespace Sitecore.MobileSDK.UrlBuilder
 {
     using System;
+    using System.Collections.Generic;
     using Sitecore.MobileSDK.UrlBuilder.QueryParameters;
     using Sitecore.MobileSDK.UrlBuilder.Rest;
     using Sitecore.MobileSDK.UrlBuilder.WebApi;
@@ -17,38 +18,33 @@
         {
             this.Validate(queryParameters);
 
+            string result = this.PayloadTypeToRestArgumentStatement( queryParameters.Payload );
+            return result;
+        }
+
+
+        private string PayloadTypeToRestArgumentStatement(PayloadType payload)
+        {
+            string strPayload = this.PayloadTypeToString( payload );
             string result = string.Empty;
 
-            if (queryParameters.Payload != PayloadType.None)
+            if ( null != strPayload )
             {
-                string payload = string.Empty;
-                switch (queryParameters.Payload)
-                {
-                    case PayloadType.Content:
-                    {
-                        payload = "content";
-                        break;
-                    }
-                    case PayloadType.Full:
-                    {
-                        payload = "full";
-                        break;
-                    }
-                    case PayloadType.Min:
-                    {
-                        payload = "min";
-                        break;
-                    }
-                    default:
-                    {
-                        throw new ArgumentException("[QueryParametersUrlBuilder] Unexpected payload enum value");
-                        break;
-                    }
-                }
-
-                result += this.webApiGrammar.PayloadParameterName + this.restGrammar.KeyValuePairSeparator + payload;
+                result = this.webApiGrammar.PayloadParameterName + this.restGrammar.KeyValuePairSeparator + strPayload;
             }
 
+            return result;
+        }
+
+        private string PayloadTypeToString(PayloadType payload)
+        {
+            var enumNamesMap = new Dictionary<PayloadType, string>();
+            enumNamesMap.Add( PayloadType.Content, "content" );
+            enumNamesMap.Add( PayloadType.Full   , "full"    );
+            enumNamesMap.Add( PayloadType.Min    , "min"     );
+            enumNamesMap.Add( PayloadType.None   , null      );
+
+            string result = enumNamesMap[payload];
             return result;
         }
 
