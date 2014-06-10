039a2e825f4630db0a3aeced84bfa002540c309c
Alexander Dodatko <adk@sitecore.net>
2014-06-10 11:58:27 +0300

    [payload] @maa - Added payload struct to interfaces

--- test/Portable/SitecoreMobileSDK-MockObjects/MockReadItemByIdParameters.cs
+++ test/Portable/SitecoreMobileSDK-MockObjects/MockReadItemByIdParameters.cs
@@ -6,12 +6,15 @@ namespace MobileSDKUnitTest.Mock
     using Sitecore.MobileSDK.SessionSettings;
     using Sitecore.MobileSDK.UrlBuilder;
     using Sitecore.MobileSDK.UrlBuilder.ItemById;
+    using Sitecore.MobileSDK.UrlBuilder.QueryParameters;
 
     public class MockGetItemsByIdParameters : IReadItemsByIdRequest
     {
         public IItemSource ItemSource { get; set; }
 
         public ISessionConfig SessionSettings { get; set; }
+
+        public IQueryParameters QueryParameters { get; set; }
 
         public string ItemId { get; set; }
     }
