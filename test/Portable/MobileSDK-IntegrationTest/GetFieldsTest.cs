01797615f15b30c86d902ba7d27e1a4756a907ef
Alina Zozulya <alinka.zozulya@gmail.com>
2014-06-11 10:28:18 +0300

    tests added

---
+++ test/Portable/MobileSDK-IntegrationTest/GetFieldsTest.cs
@@ -0,0 +1,44 @@
+﻿namespace MobileSDKIntegrationTest
+{
+  using System;
+  using System.Threading.Tasks;
+  using NUnit.Framework;
+
+  using Sitecore.MobileSDK;
+  using Sitecore.MobileSDK.Exceptions;
+  using Sitecore.MobileSDK.Items;
+  using Sitecore.MobileSDK.SessionSettings;
+
+  [TestFixture]
+  public class GetFieldsTest
+  {
+    private TestEnvironment testData;
+    private ScApiSession sessionAuthenticatedUser;
+
+    [SetUp]
+    public void Setup()
+    {
+      testData = TestEnvironment.DefaultTestEnvironment();
+      var config = new SessionConfig(testData.AuthenticatedInstanceUrl, testData.Users.Admin.Username, testData.Users.Admin.Password);
+      this.sessionAuthenticatedUser = new ScApiSession(config, ItemSource.DefaultSource());
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+      this.sessionAuthenticatedUser = null;
+      this.testData = null;
+    }
+
+    [Test]
+    public async void TestGetItemByIdWithContentFields()
+    {
+      var requestBuilder = new ItemWebApiRequestBuilder();
+      var request = requestBuilder.RequestWithId(testData.Items.Home.Id).Build();
+      var response = await this.sessionAuthenticatedUser.ReadItemByIdAsync(request);
+
+      testData.AssertItemsCount(1, response);
+      testData.AssertItemsAreEqual(testData.Items.Home, response.Items[0]);
+    }
+  }
+}
