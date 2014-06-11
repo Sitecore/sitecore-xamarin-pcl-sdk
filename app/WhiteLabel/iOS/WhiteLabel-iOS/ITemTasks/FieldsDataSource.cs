db1003a8321cc4085b0530e06404ded89e124b8f
dodikk <dodikk88.reg@gmail.com>
2014-06-11 15:23:51 +0300

    [gui] Added field datasource class

---
+++ app/WhiteLabel/iOS/WhiteLabel-iOS/ITemTasks/FieldsDataSource.cs
@@ -0,0 +1,48 @@
+﻿
+
+
+namespace WhiteLabeliOS
+{
+    using System;
+    using MonoTouch.UIKit;
+    using Sitecore.MobileSDK.Items;
+
+    public class FieldsDataSource : UITableViewDataSource
+    {
+        public ScItem SitecoreItem
+        { 
+            get
+            {
+                return this.sitecoreItem;
+            }
+            set
+            {
+                if (null != this.sitecoreItem)
+                {
+                    throw new InvalidOperationException("FieldsDataSource.Item cannot be assigned twice");
+                }
+
+                this.sitecoreItem = value;
+            }
+        }
+
+        public override int NumberOfSections(UITableView tableView)
+        {
+            return 1;
+        }
+
+        public override int RowsInSection(UITableView tableView, int section)
+        {
+            if (null == this.sitecoreItem)
+            {
+                return 0;
+            }
+
+            return this.sitecoreItem.
+        }
+
+
+        private ScItem sitecoreItem;
+    }
+}
+
