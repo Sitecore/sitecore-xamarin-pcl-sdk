28af0dfc8b7ee1ffbbf02d7d9018457d9fd28acd
dodikk <dodikk88.reg@gmail.com>
2014-06-16 15:44:18 +0300

    [encode url] Fixed case sensitivity regressions

--- lib/SitecoreMobileSDK-PCL/Utils/UrlBuilderUtils.cs
+++ lib/SitecoreMobileSDK-PCL/Utils/UrlBuilderUtils.cs
@@ -9,10 +9,28 @@ namespace Sitecore.MobileSDK.Utils
         {
             string result = Uri.EscapeDataString(originalString);
 
+            result = result.Replace("=", "%3d");
+            result = result.Replace("%3D", "%3d");
+
+            result = result.Replace("[", "%5b");
+            result = result.Replace("%5B", "%5b");
+
+            result = result.Replace("]", "%5d");
+            result = result.Replace("%5D", "%5d");
+
+
             result = result.Replace("(", "%28");
             result = result.Replace(")", "%29");
 
+            result = result.Replace(":", "%3a");
+            result = result.Replace("%3A", "%3a");
+
             result = result.Replace("*", "%2a");
+            result = result.Replace("%2A", "%2a");
+
+            result = result.Replace("/", "%2f");
+            result = result.Replace("%2F", "%2f");
+
             result = result.Replace("'", "%27");
             result = result.Replace("!", "%21");
 
