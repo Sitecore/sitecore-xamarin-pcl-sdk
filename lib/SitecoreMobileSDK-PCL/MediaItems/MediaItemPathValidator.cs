4164f02e9e228e0c482bc86310ca06d6d7fc36bb
Igor <igor.kh.mail@gmail.com>
2014-06-17 18:27:40 +0300

    media path validation implemented

--- lib/SitecoreMobileSDK-PCL/MediaItems/MediaItemPathValidator.cs
+++ lib/SitecoreMobileSDK-PCL/MediaItems/MediaItemPathValidator.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 
-namespace Sitecore.MobileSDK
+namespace Sitecore.MobileSDK.MediaItems
 {
 	public class MediaItemPathValidator
 	{
 		public MediaItemPathValidator ()
 		{
+		}
+
+		public static void ValidateMediaItemPath(string itemPath)
+		{
+			if ( string.IsNullOrWhiteSpace(itemPath) )
+			{
+				throw new ArgumentNullException ("Media Item path cannot be null or empty");
+			}
+			else if (!itemPath.StartsWith("/"))
+			{
+				throw new ArgumentException("Media Item path should begin with '/'");
+			}
 		}
 	}
 }
