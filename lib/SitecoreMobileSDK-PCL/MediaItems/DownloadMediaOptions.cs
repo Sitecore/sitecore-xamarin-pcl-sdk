b144e383efb6bda024d3449feffc9156c50524bb
Igor <igor.kh.mail@gmail.com>
2014-06-17 18:27:23 +0300

    some options added

--- lib/SitecoreMobileSDK-PCL/MediaItems/DownloadMediaOptions.cs
+++ lib/SitecoreMobileSDK-PCL/MediaItems/DownloadMediaOptions.cs
@@ -4,9 +4,92 @@ namespace Sitecore.MobileSDK.MediaItems
 {
 	public class DownloadMediaOptions
 	{
+		private string width;
+		private string height;
+		private string maxWidth;
+		private string maxHeight;
+		private string backgroundColor;
+		private string disableMediaCache;
+		private string allowStrech;
+		private string scale;
+		private string displayAsThumbnail;
+
 		public DownloadMediaOptions ()
 		{
 		}
+
+		public string Width
+		{
+			get
+			{ 
+				return this.width;
+			}
+		}
+
+		public void SetWidth(int width)
+		{
+			if (width > 0)
+			{
+				throw new ArgumentException("[DownloadMediaOptions] width must be > 0");
+			}
+
+			this.width = width.ToString ();
+		}
+
+		public string Height
+		{
+			get
+			{ 
+				return this.height;
+			}
+		}
+
+		public void SetHeight(int height)
+		{
+			if (height > 0)
+			{
+				throw new ArgumentException("[DownloadMediaOptions] height must be > 0");
+			}
+
+			this.height = height.ToString ();
+		}
+
+		public string MaxWidth
+		{
+			get
+			{ 
+				return this.maxWidth;
+			}
+		}
+
+		public void SetMaxWidth(int maxWidth)
+		{
+			if (maxWidth > 0)
+			{
+				throw new ArgumentException("[DownloadMediaOptions] maxWidth must be > 0");
+			}
+
+			this.maxWidth = maxWidth.ToString ();
+		}
+
+		public string MaxHeight
+		{
+			get
+			{ 
+				return this.maxHeight;
+			}
+		}
+
+		public void SetMaxHeight(int maxHeight)
+		{
+			if (maxHeight > 0)
+			{
+				throw new ArgumentException("[DownloadMediaOptions] maxHeight must be > 0");
+			}
+
+			this.maxHeight = maxHeight.ToString ();
+		}
+
 	}
 }
 
