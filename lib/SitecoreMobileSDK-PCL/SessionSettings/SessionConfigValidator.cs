0a3d9bcf3862409294c01e54c1181ec6046e8a45
dodikk <dodikk88.reg@gmail.com>
2014-06-19 15:43:40 +0300

    [instance url] Added a validator class

---
+++ lib/SitecoreMobileSDK-PCL/SessionSettings/SessionConfigValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Sitecore.MobileSDK.SessionSettings
+{
+  public class SessionConfigValidator
+  {
+    private SessionConfigValidator()
+    {
+    }
+
+
+    public static bool IsValidSchemeOfInstanceUrl(string url)
+    {
+      string lowercaseUrl = url.ToLowerInvariant();
+
+      bool isHttps = lowercaseUrl.StartsWith("https://");
+      bool isHttp  = lowercaseUrl.StartsWith("http://");
+      bool result  = (isHttps || isHttp);
+
+      return result;
+    }
+
+  }
+}
+
