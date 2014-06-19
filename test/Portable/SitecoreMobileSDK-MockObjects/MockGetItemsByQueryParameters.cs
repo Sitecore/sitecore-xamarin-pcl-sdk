7f1eef352a3d3f6c9d74304a121558c22792548b
dodikk <dodikk88.reg@gmail.com>
2014-06-19 12:11:37 +0300

    [code style] Fixed spacing

--- test/Portable/SitecoreMobileSDK-MockObjects/MockGetItemsByQueryParameters.cs
+++ test/Portable/SitecoreMobileSDK-MockObjects/MockGetItemsByQueryParameters.cs
@@ -1,22 +1,23 @@
-﻿using System;
+﻿
 
 namespace MobileSDKUnitTest.Mock
 {
-    using Sitecore.MobileSDK.Items;
-    using Sitecore.MobileSDK.SessionSettings;
-    using Sitecore.MobileSDK.UrlBuilder;
-    using Sitecore.MobileSDK.UrlBuilder.ItemByQuery;
-    using Sitecore.MobileSDK.UrlBuilder.QueryParameters;
+  using System;
+  using Sitecore.MobileSDK.Items;
+  using Sitecore.MobileSDK.SessionSettings;
+  using Sitecore.MobileSDK.UrlBuilder;
+  using Sitecore.MobileSDK.UrlBuilder.ItemByQuery;
+  using Sitecore.MobileSDK.UrlBuilder.QueryParameters;
 
-    public class MockGetItemsByQueryParameters : IReadItemsByQueryRequest
-    {
-        public IItemSource ItemSource { get; set; }
+  public class MockGetItemsByQueryParameters : IReadItemsByQueryRequest
+  {
+    public IItemSource ItemSource { get; set; }
 
-        public ISessionConfig SessionSettings { get; set; }
-        
-        public IQueryParameters QueryParameters { get; set; }
+    public ISessionConfig SessionSettings { get; set; }
 
-        public string SitecoreQuery { get; set; }
-    }
+    public IQueryParameters QueryParameters { get; set; }
+
+    public string SitecoreQuery { get; set; }
+  }
 }
 
