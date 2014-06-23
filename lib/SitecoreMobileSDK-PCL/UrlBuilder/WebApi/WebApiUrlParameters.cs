54f8fe4ad23d5f8cf748584bb3a5063ed143408c
dodikk <dodikk88.reg@gmail.com>
2014-06-23 14:19:04 +0300

    [sdk] Renamed REST arg
    sc_lang ===> language

--- lib/SitecoreMobileSDK-PCL/UrlBuilder/WebApi/WebApiUrlParameters.cs
+++ lib/SitecoreMobileSDK-PCL/UrlBuilder/WebApi/WebApiUrlParameters.cs
@@ -2,43 +2,43 @@
 
 namespace Sitecore.MobileSDK.UrlBuilder.WebApi
 {
-    public class WebApiUrlParameters : IWebApiUrlParameters
+  public class WebApiUrlParameters : IWebApiUrlParameters
+  {
+    public static WebApiUrlParameters ItemWebApiV2UrlParameters()
     {
-        public static WebApiUrlParameters ItemWebApiV2UrlParameters()
-        {
-            WebApiUrlParameters result = new WebApiUrlParameters();
-            result.DatabaseParameterName = "sc_database";
-            result.LanguageParameterName = "sc_lang";
-            result.VersionParameterName = "sc_itemversion";
+      WebApiUrlParameters result = new WebApiUrlParameters();
+      result.DatabaseParameterName = "sc_database";
+      result.LanguageParameterName = "language";
+      result.VersionParameterName = "sc_itemversion";
 
-            result.PayloadParameterName = "payload";
-            result.FieldsListParameterName = "fields";
+      result.PayloadParameterName = "payload";
+      result.FieldsListParameterName = "fields";
 
-            result.ItemIdParameterName = "sc_itemid";
-            result.SitecoreQueryParameterName = "query";
+      result.ItemIdParameterName = "sc_itemid";
+      result.SitecoreQueryParameterName = "query";
 
-            result.ItemWebApiEndpoint = "/-/item/";
+      result.ItemWebApiEndpoint = "/-/item/";
 
-            return result;
-        }
-
-        private WebApiUrlParameters()
-        {
-        }
-
-
-        public string DatabaseParameterName { get; private set; }
-        public string LanguageParameterName { get; private set; }
-        public string VersionParameterName { get; private set; }
-
-
-        public string PayloadParameterName { get; private set; }
-        public string FieldsListParameterName { get;  private set; }
-
-        public string ItemIdParameterName { get; private set; }
-        public string SitecoreQueryParameterName { get; private set; }
-
-        public string ItemWebApiEndpoint { get; private set; }
+      return result;
     }
+
+    private WebApiUrlParameters()
+    {
+    }
+
+
+    public string DatabaseParameterName { get; private set; }
+    public string LanguageParameterName { get; private set; }
+    public string VersionParameterName { get; private set; }
+
+
+    public string PayloadParameterName { get; private set; }
+    public string FieldsListParameterName { get;  private set; }
+
+    public string ItemIdParameterName { get; private set; }
+    public string SitecoreQueryParameterName { get; private set; }
+
+    public string ItemWebApiEndpoint { get; private set; }
+  }
 }
 
