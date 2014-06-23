3d6f304c7fdf590781992ea4259f4ecd1a0f2f12
Igor <igor.kh.mail@gmail.com>
2014-06-23 16:56:24 +0300

    code style fix

--- lib/SitecoreMobileSDK-PCL/ItemWebApiRequestBuilder.cs
+++ lib/SitecoreMobileSDK-PCL/ItemWebApiRequestBuilder.cs
@@ -10,24 +10,24 @@ namespace Sitecore.MobileSDK
 
     public class ItemWebApiRequestBuilder
     {
-        private ItemWebApiRequestBuilder()
-        {
-        }
+    private ItemWebApiRequestBuilder()
+    {
+    }
 
-        public static IGetItemRequestParametersBuilder<IReadItemsByIdRequest> ReadItemsRequestWithId(string itemId)
-        {
-            return new ReadItemByIdRequestBuilder(itemId);
-        }
+    public static IGetItemRequestParametersBuilder<IReadItemsByIdRequest> ReadItemsRequestWithId(string itemId)
+    {
+        return new ReadItemByIdRequestBuilder(itemId);
+    }
 
-        public static IGetItemRequestParametersBuilder<IReadItemsByPathRequest> ReadItemsRequestWithPath(string itemPath)
-        {
-            return new ReadItemByPathRequestBuilder(itemPath);
-        }
+    public static IGetItemRequestParametersBuilder<IReadItemsByPathRequest> ReadItemsRequestWithPath(string itemPath)
+    {
+        return new ReadItemByPathRequestBuilder(itemPath);
+    }
 
-        public static IGetItemRequestParametersBuilder<IReadItemsByQueryRequest> ReadItemsRequestWithSitecoreQuery(string sitecoreQuery)
-        {
-            return new ReadItemByQueryRequestBuilder(sitecoreQuery);
-        }
+    public static IGetItemRequestParametersBuilder<IReadItemsByQueryRequest> ReadItemsRequestWithSitecoreQuery(string sitecoreQuery)
+    {
+        return new ReadItemByQueryRequestBuilder(sitecoreQuery);
+    }
 
 		public static IGetMediaItemRequestParametersBuilder<IReadMediaItemRequest> ReadMediaItemRequest(string mediaItemPath)
 		{
