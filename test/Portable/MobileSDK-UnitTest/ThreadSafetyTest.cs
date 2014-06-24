20281d87431df131e708576e8744e0d051da0370
dodikk <dodikk88.reg@gmail.com>
2014-06-24 18:48:26 +0300

    [thread safety] Added a test

---
+++ test/Portable/MobileSDK-UnitTest/ThreadSafetyTest.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace Sitecore.MobileSdkUnitTest
+{
+  using System;
+  using NUnit.Framework;
+
+  using Sitecore.MobileSDK;
+  using Sitecore.MobileSDK.Items;
+  using Sitecore.MobileSDK.SessionSettings;
+
+
+
+  [TestFixture]
+  public class ThreadSafetyTest
+  {
+    public void TestSessionDefaultSourceCannotBeMutated()
+    {
+      var anonymous = new SessionConfig("localhost", null, null);
+      var defaultSource = ItemSource.DefaultSource();
+
+      var session = new ScApiSession(anonymous, defaultSource);
+
+      Assert.AreEqual(defaultSource, session.DefaultSource);
+      Assert.AreNotSame(defaultSource, session.DefaultSource);
+
+      anonymous.InstanceUrl = "sitecore.net";
+      Assert.AreNotEqual(defaultSource, session.DefaultSource);
+      Assert.AreNotSame(defaultSource, session.DefaultSource);
+    }
+  }
+}
+
