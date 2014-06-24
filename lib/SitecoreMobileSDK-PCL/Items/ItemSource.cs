b468beaf2728b4a4a038cb01cecde9775de4ef4f
dodikk <dodikk88.reg@gmail.com>
2014-06-24 10:50:11 +0300

    [source] Fixed spacing

--- lib/SitecoreMobileSDK-PCL/Items/ItemSource.cs
+++ lib/SitecoreMobileSDK-PCL/Items/ItemSource.cs
@@ -2,35 +2,35 @@
 
 namespace Sitecore.MobileSDK.Items
 {
-    public class ItemSource : ItemSourcePOD
+  public class ItemSource : ItemSourcePOD
+  {
+    public const string DefaultDatabase = "web";
+    public const string DefaultLanguage = "en";
+
+    public static ItemSource DefaultSource()
     {
-        public const string DefaultDatabase = "web";
-        public const string DefaultLanguage = "en";
+      return new ItemSource (ItemSource.DefaultDatabase, ItemSource.DefaultLanguage, null);
+    }
 
-        public static ItemSource DefaultSource()
-        {
-			return new ItemSource (ItemSource.DefaultDatabase, ItemSource.DefaultLanguage, null);
-        }
-
-		public ItemSource(string database, string language, string version = null) 
-            : base(database, language, version)
-        {
-            this.Validate ();
-        }
+    public ItemSource(string database, string language, string version = null) 
+      : base(database, language, version)
+    {
+      this.Validate ();
+    }
 
 
-        /// <exception cref="ArgumentNullException">[ItemSource.Database] Do not pass null to constructor</exception>
-        private void Validate()
-        {
-            if (null == this.Database)
-            {
-                throw new ArgumentNullException ("[ItemSource.Database] Do not pass null to constructor");
-            }
-            else if (null == this.Language)
-            {
-                throw new ArgumentNullException ("[ItemSource.Language] Do not pass null to constructor");
-            }
-        }
-    } 
+    /// <exception cref="ArgumentNullException">[ItemSource.Database] Do not pass null to constructor</exception>
+    private void Validate()
+    {
+      if (null == this.Database)
+      {
+        throw new ArgumentNullException ("[ItemSource.Database] Do not pass null to constructor");
+      }
+      else if (null == this.Language)
+      {
+        throw new ArgumentNullException ("[ItemSource.Language] Do not pass null to constructor");
+      }
+    }
+  } 
 }
 
