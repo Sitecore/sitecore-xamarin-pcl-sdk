cacab1979b0b4fe46303c12d52ff699efaec8cb1
Igor <igor.kh.mail@gmail.com>
2014-06-25 17:25:11 +0300

    extra abstract class removed

--- lib/SitecoreMobileSDK-PCL/UrlBuilder/MediaItem/UserRequest/ReadMediaItemRequestBuilder.cs
+++ lib/SitecoreMobileSDK-PCL/UrlBuilder/MediaItem/UserRequest/ReadMediaItemRequestBuilder.cs
@@ -3,23 +3,66 @@
 namespace Sitecore.MobileSDK
 {
 	using System;
+  using Sitecore.MobileSDK.Items;
 	using Sitecore.MobileSDK.UrlBuilder;
   using Sitecore.MobileSDK.UrlBuilder.MediaItem;
 
-	public class ReadMediaItemRequestBuilder : AbstractGetMediaItemRequestBuilder<IReadMediaItemRequest>
+  public class ReadMediaItemRequestBuilder : IGetMediaItemRequestParametersBuilder<IReadMediaItemRequest>
 	{
-		public ReadMediaItemRequestBuilder(string itemPath)
-		{
-			MediaItemPathValidator.ValidateMediaItemPath(itemPath);
 
-			this.mediaItemPath = itemPath;
-		}
+    public ReadMediaItemRequestBuilder(string itemPath)
+    {
+      MediaItemPathValidator.ValidateMediaItemPath(itemPath);
 
-		public override IReadMediaItemRequest Build()
+      this.mediaItemPath = itemPath;
+    }
+      
+    public IGetMediaItemRequestParametersBuilder<IReadMediaItemRequest> Database (string sitecoreDatabase)
+    {
+      this.itemSourceAccumulator = new ItemSourcePOD (
+        sitecoreDatabase, 
+        this.itemSourceAccumulator.Language, 
+        this.itemSourceAccumulator.Version);
+
+      return this;
+    }
+
+    public IGetMediaItemRequestParametersBuilder<IReadMediaItemRequest> Language (string itemLanguage)
+    {
+      this.itemSourceAccumulator = new ItemSourcePOD (
+        this.itemSourceAccumulator.Database, 
+        itemLanguage, 
+        this.itemSourceAccumulator.Version);
+
+      return this;
+    }
+
+    public IGetMediaItemRequestParametersBuilder<IReadMediaItemRequest> Version (string itemVersion)
+    {
+      this.itemSourceAccumulator = new ItemSourcePOD (
+        this.itemSourceAccumulator.Database, 
+        this.itemSourceAccumulator.Language,
+        itemVersion);
+
+      return this;
+    }
+
+    public IGetMediaItemRequestParametersBuilder<IReadMediaItemRequest> DownloadOptions (IDownloadMediaOptions downloadMediaOptions)
+    {
+      this.downloadMediaOptions = (DownloadMediaOptions)downloadMediaOptions.ShallowCopy();
+
+      return this;
+    }
+      
+		
+		public IReadMediaItemRequest Build()
 		{
 			var result = new ReadMediaItemParameters(null, this.itemSourceAccumulator, this.downloadMediaOptions, this.mediaItemPath);
 			return result;
 		}
+
+    protected ItemSourcePOD itemSourceAccumulator = new ItemSourcePOD( null, null, null );
+    protected IDownloadMediaOptions downloadMediaOptions = null;
 
 		private string mediaItemPath;
 	}
