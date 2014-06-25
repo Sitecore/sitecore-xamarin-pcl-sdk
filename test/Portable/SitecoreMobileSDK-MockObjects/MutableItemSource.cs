858c9eea88838b910542cbb69f5ded76ba39ff9e
dodikk <dodikk88.reg@gmail.com>
2014-06-25 10:24:32 +0300

    [test] Added mock classes

--- test/Portable/SitecoreMobileSDK-MockObjects/MutableItemSource.cs
+++ test/Portable/SitecoreMobileSDK-MockObjects/MutableItemSource.cs
@@ -1,11 +1,28 @@
 ﻿using System;
+using Sitecore.MobileSDK.Items;
 
 namespace SitecoreMobileSDKMockObjects
 {
-  public class MutableItemSource
+  public class MutableItemSource : ItemSource
   {
-    public MutableItemSource()
+    public MutableItemSource(string database, string language, string version = null) 
+      : base(database, language, version)
     {
+    }
+
+    public void SetDatabase(string value)
+    {
+      this.Database = value;
+    }
+
+    public void SetLanguage(string value)
+    {
+      this.Language = value;
+    }
+
+    public void SetVersion(string value)
+    {
+      this.Version = value;
     }
   }
 }
