430ba4ce441da85da72fa5c1654787c46f337c00
Igor <igor.kh.mail@gmail.com>
2014-06-25 17:00:32 +0300

    new tests added

---
+++ test/Portable/MobileSDK-UnitTest/DownloadMediaOptionsTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Sitecore.MobileSdkUnitTest
+{
+  [TestFixture()]
+  public class DownloadMediaOptionsTest
+  {
+    [Test()]
+    public void TestCase()
+    {
+    }
+  }
+}
+
