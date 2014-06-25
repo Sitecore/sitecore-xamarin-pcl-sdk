b04db73b4f142e1106f210843f0a1c40a966ed5b
Igor <igor.kh.mail@gmail.com>
2014-06-25 17:01:58 +0300

    new tests added

--- test/Portable/MobileSDK-UnitTest/DownloadMediaOptionsTest.cs
+++ test/Portable/MobileSDK-UnitTest/DownloadMediaOptionsTest.cs
@@ -3,13 +3,59 @@ using System;
 
 namespace Sitecore.MobileSdkUnitTest
 {
+  using Sitecore.MobileSDK.UrlBuilder.MediaItem;
+
   [TestFixture()]
   public class DownloadMediaOptionsTest
   {
+
     [Test()]
-    public void TestCase()
+    public void TestCopyEmptyOptions()
     {
+      DownloadMediaOptions options = new DownloadMediaOptions();
+      DownloadMediaOptions copy = (DownloadMediaOptions)options.ShallowCopy();
+      Assert.True (copy.IsEmpty);
     }
+
+    [Test()]
+    public void TestCopyWithOptions()
+    {
+      DownloadMediaOptions options = new DownloadMediaOptions();
+      options.SetWidth(100);
+      options.SetScale(1.5f);
+
+      DownloadMediaOptions copy = (DownloadMediaOptions)options.ShallowCopy();
+      Assert.AreEqual("100", copy.Width);
+      Assert.AreEqual("1.5", copy.Scale);
+    }
+
+    [Test()]
+    public void TestCopyIsIndependent()
+    {
+      DownloadMediaOptions options = new DownloadMediaOptions();
+      options.SetWidth(100);
+      options.SetScale(1.5f);
+
+      DownloadMediaOptions copy = (DownloadMediaOptions)options.ShallowCopy();
+
+      options.SetWidth(200);
+
+      Assert.AreEqual("100", copy.Width);
+      Assert.AreEqual("1.5", copy.Scale);
+    }
+
+    [Test()]
+    public void TestBoolHasProperValue()
+    {
+      DownloadMediaOptions options = new DownloadMediaOptions();
+      options.SetDisplayAsThumbnail(true);
+      options.SetDisableMediaCache(true);
+      options.SetDisableMediaCache(false);
+
+      Assert.AreEqual("1", options.DisplayAsThumbnail);
+      Assert.IsNull(options.DisableMediaCache);
+    }
+
   }
 }
 
