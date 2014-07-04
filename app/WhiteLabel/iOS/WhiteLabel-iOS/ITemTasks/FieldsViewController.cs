4f07d53eac4623e724fe2b00cb8e21b8a548f76a
Igor <igor.kh.mail@gmail.com>
2014-07-04 16:31:16 +0300

    separate controller to show fields list added

--- app/WhiteLabel/iOS/WhiteLabel-iOS/ITemTasks/FieldsViewController.cs
+++ app/WhiteLabel/iOS/WhiteLabel-iOS/ITemTasks/FieldsViewController.cs
@@ -1,32 +1,85 @@
 ﻿
-using System;
-using System.Drawing;
-
-using MonoTouch.Foundation;
-using MonoTouch.UIKit;
 
 namespace WhiteLabeliOS
 {
-  public partial class FieldsViewController : UIViewController
-  {
-    public FieldsViewController () : base ("FieldsViewController", null)
-    {
-    }
+  using System;
+  using System.Drawing;
 
-    public override void DidReceiveMemoryWarning ()
+  using MonoTouch.Foundation;
+  using MonoTouch.UIKit;
+  using WhiteLabeliOS.FieldsTableView;
+  using Sitecore.MobileSDK.Items.Fields;
+  using Sitecore.MobileSDK.Items;
+
+  public partial class FieldsViewController : BaseTaskTableViewController
+  {
+    public FieldsViewController (IntPtr handle) : base (handle)
     {
-      // Releases the view if it doesn't have a superview.
-      base.DidReceiveMemoryWarning ();
-      
-      // Release any cached data, images, etc that aren't in use.
     }
 
     public override void ViewDidLoad ()
     {
       base.ViewDidLoad ();
-      
-      // Perform any additional setup after loading the view, typically from a nib.
+      this.TableView = this.FieldsTableView;
+
     }
+
+    public void ShowFieldsForItem( ISitecoreItem item )
+    {
+      BeginInvokeOnMainThread(delegate
+      {
+
+        this.Title = item.DisplayName;
+
+        this.CleanupTableViewBindingsSync();
+
+        this.fieldsDataSource = new FieldsDataSource();
+        this.fieldsTableDelegate = new FieldCellSelectionHandler();
+
+
+        FieldsDataSource dataSource = this.fieldsDataSource;
+        dataSource.SitecoreItem = item;
+        dataSource.TableView = this.TableView;
+
+
+        FieldCellSelectionHandler tableDelegate = this.fieldsTableDelegate;
+        tableDelegate.TableView = this.TableView;
+        tableDelegate.SitecoreItem = item;
+
+        FieldCellSelectionHandler.TableViewDidSelectFieldAtIndexPath onFieldSelected = 
+          delegate (UITableView tableView, IField itemField, NSIndexPath indexPath)
+        {
+          AlertHelper.ShowLocalizedAlertWithOkOption("Field Raw Value", itemField.RawValue);
+        };
+        tableDelegate.OnFieldCellSelectedDelegate = onFieldSelected;
+
+        this.TableView.DataSource = dataSource;
+        this.TableView.Delegate = tableDelegate;
+        this.TableView.ReloadData();
+      });
+    }
+
+    protected void CleanupTableViewBindingsSync()
+    {
+      this.TableView.DataSource = null;
+      this.TableView.Delegate = null;
+
+      if (this.fieldsDataSource != null)
+      {
+        this.fieldsDataSource.Dispose ();
+        this.fieldsDataSource = null;
+      }
+
+      if (this.fieldsTableDelegate != null)
+      {
+        this.fieldsTableDelegate.Dispose ();
+        this.fieldsTableDelegate = null;
+      }
+    }
+
+    protected FieldsDataSource fieldsDataSource;
+    protected FieldCellSelectionHandler fieldsTableDelegate;
+
   }
 }
 
--- app/WhiteLabel/iOS/WhiteLabel-iOS/ITemTasks/FieldsViewController.designer.cs
+++ app/WhiteLabel/iOS/WhiteLabel-iOS/ITemTasks/FieldsViewController.designer.cs
@@ -1,18 +1,26 @@
 ﻿// WARNING
 //
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
 
 namespace WhiteLabeliOS
 {
-  [Register ("FieldsViewController")]
-  partial class FieldsViewController
-  {
-    void ReleaseDesignerOutlets ()
-    {
-    }
-  }
+	[Register ("FieldsViewController")]
+	partial class FieldsViewController
+	{
+		[Outlet]
+		MonoTouch.UIKit.UITableView FieldsTableView { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (FieldsTableView != null) {
+				FieldsTableView.Dispose ();
+				FieldsTableView = null;
+			}
+		}
+	}
 }
