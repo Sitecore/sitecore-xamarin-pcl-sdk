b93af2165203a4650490faebdb08ab9f70734bf7
Igor <igor.kh.mail@gmail.com>
2014-07-07 17:49:59 +0300

    Interface classes for create items requests

---
+++ lib/SitecoreMobileSDK-PCL/UrlBuilder/CreateItem/CreateItemByPath/ICreateItemByPathRequest.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Sitecore.MobileSDK.UrlBuilder.CreateItemByPath
+{
+  using System;
+  using System.Collections.Generic;
+  using Sitecore.MobileSDK.UrlBuilder.ItemByPath;
+
+  public interface ICreateItemByPathRequest : IReadItemsByPathRequest
+  {
+    string ItemName{ get; }
+    string ItemTemplate{ get; }
+    Dictionary<string, string> FieldsRawValuesByName{ get; }
+  }
+}
+
