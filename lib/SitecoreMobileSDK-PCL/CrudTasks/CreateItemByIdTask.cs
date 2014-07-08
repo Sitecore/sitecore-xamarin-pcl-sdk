7c9da4ac59b1098c392e44a655c20762ea47541e
Igor <igor.kh.mail@gmail.com>
2014-07-08 18:11:31 +0300

    task to create item implementation

--- lib/SitecoreMobileSDK-PCL/CrudTasks/CreateItemByIdTask.cs
+++ lib/SitecoreMobileSDK-PCL/CrudTasks/CreateItemByIdTask.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿
 
-namespace Sitecore.MobileSDK
+namespace Sitecore.MobileSDK.CrudTasks
 {
-  public class CreateItemByIdTask
+  using System;
+  using System.Net.Http;
+
+  using Sitecore.MobileSDK.PublicKey;
+  using Sitecore.MobileSDK.UrlBuilder.CreateItem;
+
+  public class CreateItemByIdTask : AbstractGetItemTask<ICreateItemByIdRequest>
   {
-    public CreateItemByIdTask ()
+    public CreateItemByIdTask(CreateItemByIdUrlBuilder urlBuilder, HttpClient httpClient, ICredentialsHeadersCryptor credentialsHeadersCryptor) 
+      : base(httpClient, credentialsHeadersCryptor)
     {
+      this.urlBuilder = urlBuilder;
     }
+
+    protected override string UrlToGetItemWithRequest(ICreateItemByIdRequest request)
+    {
+      return this.urlBuilder.GetUrlForRequest(request);
+    }
+
+    private readonly CreateItemByIdUrlBuilder urlBuilder;
   }
 }
 
