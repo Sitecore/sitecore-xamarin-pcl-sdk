ef3dfc4f9592b44cca24f5e56fce155b0a7e42f8
Igor <igor.kh.mail@gmail.com>
2014-07-08 18:04:02 +0300

    using create in white label for fast testing

--- app/WhiteLabel/iOS/WhiteLabel-iOS/ITemTasks/CreateEditItemViewController.cs
+++ app/WhiteLabel/iOS/WhiteLabel-iOS/ITemTasks/CreateEditItemViewController.cs
@@ -4,6 +4,9 @@ using System.Drawing;
 
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using Sitecore.MobileSDK;
+using Sitecore.MobileSDK.Items;
+using System.Linq;
 
 namespace WhiteLabeliOS
 {
@@ -26,13 +29,49 @@ namespace WhiteLabeliOS
 		partial void OnCreateItemButtonTapped (MonoTouch.Foundation.NSObject sender)
 		{
 			this.saveButton.Enabled = true;
-			AlertHelper.ShowLocalizedNotImlementedAlert();
+			//AlertHelper.ShowLocalizedNotImlementedAlert();
+      this.SendRequest();
 		}
 
 		partial void OnSaveItemButtonTapped (MonoTouch.Foundation.NSObject sender)
 		{
 			AlertHelper.ShowLocalizedNotImlementedAlert();
 		}
+
+    private async void SendRequest()
+    {
+      try
+      {
+        ScApiSession session = this.instanceSettings.GetSession();
+
+        var request = ItemWebApiRequestBuilder.CreateItemRequestWithId(pathField.Text)
+          .ItemTemplate("Sample/Sample Item")
+          .Build();
+
+        this.ShowLoader();
+
+        ScItemsResponse response = await session.CreateItemAsync(request);
+        if (response.Items.Any())
+        {
+          AlertHelper.ShowLocalizedAlertWithOkOption("Message", "OK");
+        }
+        else
+        {
+          AlertHelper.ShowLocalizedAlertWithOkOption("Message", "Item is not exist");
+        }
+      }
+      catch(Exception e) 
+      {
+        AlertHelper.ShowLocalizedAlertWithOkOption("Error", e.Message);
+      }
+      finally
+      {
+        BeginInvokeOnMainThread(delegate
+        {
+          this.HideLoader();
+        });
+      }
+    }
 	}
 }
 
