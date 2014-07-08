502172d99ad5b19f5e0af251ee69851a6873fc57
Igor <igor.kh.mail@gmail.com>
2014-07-08 14:12:13 +0300

    template parameter added, code style fixed

--- lib/SitecoreMobileSDK-PCL/UrlBuilder/WebApi/IWebApiUrlParameters.cs
+++ lib/SitecoreMobileSDK-PCL/UrlBuilder/WebApi/IWebApiUrlParameters.cs
@@ -2,29 +2,30 @@
 
 namespace Sitecore.MobileSDK.UrlBuilder.WebApi
 {
-    public interface IWebApiUrlParameters
-    {
-        #region Item Source
-        string DatabaseParameterName { get; }
-        string LanguageParameterName { get; }
-        string VersionParameterName { get; }
-        #endregion Item Source
+  public interface IWebApiUrlParameters
+  {
+    #region Item Source
+    string DatabaseParameterName { get; }
+    string LanguageParameterName { get; }
+    string VersionParameterName { get; }
+    #endregion Item Source
 
 
-        #region Item Identifiers
-        string SitecoreQueryParameterName { get; }
-        string ItemIdParameterName { get; }
-        #endregion Item Identifiers
+    #region Item Identifiers
+    string SitecoreQueryParameterName { get; }
+    string ItemIdParameterName { get; }
+    #endregion Item Identifiers
 
-        #region query parameters
-        string PayloadParameterName { get; }
-        string ScopeParameterName { get; }
-        string FieldsListParameterName { get; }
-        #endregion query parameters
+    #region query parameters
+    string PayloadParameterName { get; }
+    string TemplateParameterName { get; }
+    string ScopeParameterName { get; }
+    string FieldsListParameterName { get; }
+    #endregion query parameters
 
-        #region Item Web API Actions
-        string ItemWebApiEndpoint { get; }
-        #endregion Item Web API Actions
-    }
+    #region Item Web API Actions
+    string ItemWebApiEndpoint { get; }
+    #endregion Item Web API Actions
+  }
 }
 
