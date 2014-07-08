adfdcd75b56382c19fc76961d98e37b7e89ec03f
Igor <igor.kh.mail@gmail.com>
2014-07-08 18:12:20 +0300

    create item by id url builder

--- lib/SitecoreMobileSDK-PCL/UrlBuilder/CreateItem/CreateItemById/CreateItemByIdUrlBuilder.cs
+++ lib/SitecoreMobileSDK-PCL/UrlBuilder/CreateItem/CreateItemById/CreateItemByIdUrlBuilder.cs
@@ -1,11 +1,35 @@
-﻿using System;
+﻿
 
-namespace Sitecore.MobileSDK
+namespace Sitecore.MobileSDK.UrlBuilder.CreateItem
 {
-  public class CreateItemByIdUrlBuilder
+  using System;
+  using Sitecore.MobileSDK;
+  using Sitecore.MobileSDK.Utils;
+  using Sitecore.MobileSDK.UrlBuilder.Rest;
+  using Sitecore.MobileSDK.UrlBuilder.WebApi;
+  using Sitecore.MobileSDK.UrlBuilder.ItemById;
+
+  public class CreateItemByIdUrlBuilder : AbstractGetItemUrlBuilder<ICreateItemByIdRequest>
   {
-    public CreateItemByIdUrlBuilder ()
+    public CreateItemByIdUrlBuilder(IRestServiceGrammar restGrammar, IWebApiUrlParameters webApiGrammar)
+      : base( restGrammar, webApiGrammar )
     {
+    }
+
+    protected override string GetSpecificPartForRequest(ICreateItemByIdRequest request)
+    {
+      string escapedId = UrlBuilderUtils.EscapeDataString(request.ItemId);
+      string escapedTemplate = UrlBuilderUtils.EscapeDataString(request.ItemTemplate);
+
+      string result = this.webApiGrammar.ItemIdParameterName + this.restGrammar.KeyValuePairSeparator + escapedId;
+      result += this.restGrammar.FieldSeparator + this.webApiGrammar.TemplateParameterName + this.restGrammar.KeyValuePairSeparator + escapedTemplate;
+
+      return result.ToLowerInvariant();
+    }
+
+    protected override void ValidateSpecificRequest(ICreateItemByIdRequest request)
+    {
+      ItemIdValidator.ValidateItemId(request.ItemId);
     }
   }
 }
