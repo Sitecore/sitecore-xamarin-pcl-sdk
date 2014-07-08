ccdf35d1015d6e5b3199d11a010222f1222287f1
Igor <igor.kh.mail@gmail.com>
2014-07-08 18:03:34 +0300

    create task flow classes

---
+++ lib/SitecoreMobileSDK-PCL/CrudTasks/CreateItemByIdTask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sitecore.MobileSDK
+{
+  public class CreateItemByIdTask
+  {
+    public CreateItemByIdTask ()
+    {
+    }
+  }
+}
+
---
+++ lib/SitecoreMobileSDK-PCL/UrlBuilder/CreateItem/CreateItemById/CreateItemByIdUrlBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sitecore.MobileSDK
+{
+  public class CreateItemByIdUrlBuilder
+  {
+    public CreateItemByIdUrlBuilder ()
+    {
+    }
+  }
+}
+
