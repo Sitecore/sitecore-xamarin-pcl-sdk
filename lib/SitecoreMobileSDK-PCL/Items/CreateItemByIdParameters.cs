cbe3125880aa258ad0a091c53e13e664e0ba75cf
Igor <igor.kh.mail@gmail.com>
2014-07-08 18:12:00 +0300

    create item parameters storage

--- lib/SitecoreMobileSDK-PCL/Items/CreateItemByIdParameters.cs
+++ lib/SitecoreMobileSDK-PCL/Items/CreateItemByIdParameters.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using Sitecore.MobileSDK.UrlBuilder.CreateItem;
+using Sitecore.MobileSDK.Items;
+using Sitecore.MobileSDK.SessionSettings;
+using Sitecore.MobileSDK.UrlBuilder.QueryParameters;
+using System.Collections.Generic;
+using Sitecore.MobileSDK.UrlBuilder.ItemById;
+using Sitecore.MobileSDK.UrlBuilder;
 
 namespace Sitecore.MobileSDK
 {
-  public class CreateItemByIdParameters
+  public class CreateItemByIdParameters : ICreateItemByIdRequest
   {
-    public CreateItemByIdParameters ()
+    public CreateItemByIdParameters(ISessionConfig sessionSettings, IItemSource itemSource, IQueryParameters queryParameters, string itemId)
     {
+      this.SessionSettings = sessionSettings;
+      this.ItemSource = itemSource;
+      this.ItemId = itemId;
+      this.QueryParameters = queryParameters;
     }
+
+    public virtual IReadItemsByIdRequest DeepCopyGetItemByIdRequest()
+    {
+      ISessionConfig connection = null;
+      IItemSource itemSrc = null;
+      IQueryParameters payload = null;
+
+      if (null != this.SessionSettings)
+      {
+        connection = this.SessionSettings.SessionConfigShallowCopy();
+      }
+
+      if (null != this.ItemSource)
+      {
+        itemSrc = this.ItemSource.ShallowCopy();
+      }
+
+      if (null != this.QueryParameters)
+      {
+        payload = this.QueryParameters.DeepCopy();
+      }
+
+      return new ReadItemsByIdParameters(this.SessionSettings, this.ItemSource, this.QueryParameters, this.ItemId);
+    }
+
+    public virtual IBaseGetItemRequest DeepCopyBaseGetItemRequest()
+    {
+      return this.DeepCopyGetItemByIdRequest();
+    }
+
+
+    public string ItemId { get; private set; }
+
+    public IItemSource ItemSource { get; private set; }
+
+    public ISessionConfig SessionSettings { get; private set; }
+
+    public IQueryParameters QueryParameters { get; private set; }
+
+
+    public string ItemName { get; private set; }
+    public string ItemTemplate { get; private set; }
+    public Dictionary<string, string> FieldsRawValuesByName{ get; private set; }
   }
 }
 
