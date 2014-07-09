abc191b4e5c9db21a53644751701589569402f54
Igor <igor.kh.mail@gmail.com>
2014-07-09 18:37:02 +0300

    item name added to url builder

--- lib/SitecoreMobileSDK-PCL/UrlBuilder/CreateItem/CreateItemById/CreateItemByIdUrlBuilder.cs
+++ lib/SitecoreMobileSDK-PCL/UrlBuilder/CreateItem/CreateItemById/CreateItemByIdUrlBuilder.cs
@@ -19,10 +19,12 @@ namespace Sitecore.MobileSDK.UrlBuilder.CreateItem
     protected override string GetSpecificPartForRequest(ICreateItemByIdRequest request)
     {
       string escapedId = UrlBuilderUtils.EscapeDataString(request.ItemId);
-      string escapedTemplate = UrlBuilderUtils.EscapeDataString(request.ItemTemplate);
+      string escapedTemplate = UrlBuilderUtils.EscapeDataString(request.CreateParameters.ItemTemplate);
+      string escapedName = UrlBuilderUtils.EscapeDataString(request.CreateParameters.ItemName);
 
       string result = this.webApiGrammar.ItemIdParameterName + this.restGrammar.KeyValuePairSeparator + escapedId;
       result += this.restGrammar.FieldSeparator + this.webApiGrammar.TemplateParameterName + this.restGrammar.KeyValuePairSeparator + escapedTemplate;
+      result += this.restGrammar.FieldSeparator + this.webApiGrammar.ItemNameParameterName + this.restGrammar.KeyValuePairSeparator + escapedName;
 
       return result.ToLowerInvariant();
     }
