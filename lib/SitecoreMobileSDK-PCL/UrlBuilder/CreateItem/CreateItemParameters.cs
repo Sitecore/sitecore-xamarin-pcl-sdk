c6194124ac24026ed53bf2805e554e86291158a4
Igor <igor.kh.mail@gmail.com>
2014-07-09 18:29:59 +0300

    class to collect parameters for create items requests

---
+++ lib/SitecoreMobileSDK-PCL/UrlBuilder/CreateItem/CreateItemParameters.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Sitecore.MobileSDK.UrlBuilder.CreateItem
+{
+  using System;
+  using System.Collections.Generic;
+
+  public class CreateItemParameters
+  {
+    public CreateItemParameters (string itemName, string itemTemplate, Dictionary<string, string> fieldsRawValuesByName)
+    {
+      this.ItemName = itemName;
+      this.ItemTemplate = itemTemplate;
+      this.FieldsRawValuesByName = fieldsRawValuesByName;
+    }
+
+    public string ItemName{ get; private set; }
+    public string ItemTemplate{ get; private set; }
+    public Dictionary<string, string> FieldsRawValuesByName{ get; private set; }
+  }
+}
+
