778d2d9827d3a82afd8faac54e4b41094e745dbc
Igor <igor.kh.mail@gmail.com>
2014-07-09 18:38:01 +0300

    additional parameters added to create item params builder interface

--- lib/SitecoreMobileSDK-PCL/UserRequest/CreateRequest/ICreateItemRequestParametersBuilder.cs
+++ lib/SitecoreMobileSDK-PCL/UserRequest/CreateRequest/ICreateItemRequestParametersBuilder.cs
@@ -1,11 +1,16 @@
-﻿using System;
-
+﻿
 namespace Sitecore.MobileSDK
 {
+  using System;
+  using System.Collections.Generic;
+
   public interface ICreateItemRequestParametersBuilder<T> : IGetItemRequestParametersBuilder<T>
     where T : class
   {
     ICreateItemRequestParametersBuilder<T> ItemTemplate(string template);
+    ICreateItemRequestParametersBuilder<T> ItemName(string template);
+    ICreateItemRequestParametersBuilder<T> AddFieldsRawValuesByName (Dictionary<string, string> fieldsRawValuesByName);
+    ICreateItemRequestParametersBuilder<T> AddFieldsRawValuesByName (string fieldKey, string fieldValue);
   }
 }
 
