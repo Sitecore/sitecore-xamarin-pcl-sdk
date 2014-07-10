8e898fb50971e9d3a686395282a75766ea3cb1a4
Igor <igor.kh.mail@gmail.com>
2014-07-10 17:34:25 +0300

    copy method added for create item parameters

--- lib/SitecoreMobileSDK-PCL/UrlBuilder/CreateItem/CreateItemParameters.cs
+++ lib/SitecoreMobileSDK-PCL/UrlBuilder/CreateItem/CreateItemParameters.cs
@@ -13,6 +13,11 @@ namespace Sitecore.MobileSDK.UrlBuilder.CreateItem
       this.FieldsRawValuesByName = fieldsRawValuesByName;
     }
 
+    public CreateItemParameters ShallowCopy()
+    {
+      return new CreateItemParameters(this.ItemName, this.ItemTemplate, this.FieldsRawValuesByName);
+    }
+
     public string ItemName{ get; private set; }
     public string ItemTemplate{ get; private set; }
     public Dictionary<string, string> FieldsRawValuesByName{ get; private set; }
