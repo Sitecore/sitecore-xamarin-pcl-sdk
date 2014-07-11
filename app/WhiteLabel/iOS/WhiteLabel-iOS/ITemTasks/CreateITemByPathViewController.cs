5eeed0a40e8ae700bc8dc40c7fe57b97962c671d
Igor <igor.kh.mail@gmail.com>
2014-07-11 17:40:54 +0300

    view controller for create item by path added

--- app/WhiteLabel/iOS/WhiteLabel-iOS/ITemTasks/CreateITemByPathViewController.cs
+++ app/WhiteLabel/iOS/WhiteLabel-iOS/ITemTasks/CreateITemByPathViewController.cs
@@ -1,32 +1,76 @@
 ﻿
-using System;
-using System.Drawing;
-
-using MonoTouch.Foundation;
-using MonoTouch.UIKit;
-
 namespace WhiteLabeliOS
 {
-  public partial class CreateITemByPathViewController : UIViewController
+  using System;
+  using System.Drawing;
+  using System.Linq;
+
+  using MonoTouch.Foundation;
+  using MonoTouch.UIKit;
+  using Sitecore.MobileSDK;
+  using Sitecore.MobileSDK.Items;
+
+  public partial class CreateITemByPathViewController : BaseTaskViewController
   {
-    public CreateITemByPathViewController () : base ("CreateITemByPathViewController", null)
+    public CreateITemByPathViewController (IntPtr handle) : base (handle)
     {
+      Title = NSBundle.MainBundle.LocalizedString ("createItemByPath", null);
     }
-
-    public override void DidReceiveMemoryWarning ()
-    {
-      // Releases the view if it doesn't have a superview.
-      base.DidReceiveMemoryWarning ();
       
-      // Release any cached data, images, etc that aren't in use.
-    }
-
     public override void ViewDidLoad ()
     {
       base.ViewDidLoad ();
-      
-      // Perform any additional setup after loading the view, typically from a nib.
+
+      this.nameField.ShouldReturn = this.HideKeyboard;
+      this.pathField.ShouldReturn = this.HideKeyboard;
+      this.textField.ShouldReturn = this.HideKeyboard;
+      this.titleField.ShouldReturn = this.HideKeyboard;
     }
+
+    partial void OnCreateItemButtonTapped (MonoTouch.UIKit.UIButton sender)
+    {
+      this.SendRequest();
+    }
+
+    private async void SendRequest()
+    {
+      try
+      {
+        ScApiSession session = this.instanceSettings.GetSession();
+
+        var request = ItemWebApiRequestBuilder.CreateItemRequestWithPath(this.pathField.Text)
+          .ItemTemplate("Sample/Sample Item")
+          .ItemName(this.nameField.Text)
+          .AddFieldsRawValuesByName("Title", titleField.Text)
+          .AddFieldsRawValuesByName("Text", textField.Text)
+          .Build();
+
+        this.ShowLoader();
+
+        ScItemsResponse response = await session.CreateItemAsync(request);
+        if (response.Items.Any())
+        {
+          ISitecoreItem item = response.Items[0];
+          AlertHelper.ShowLocalizedAlertWithOkOption("The item created successfully", "Item path: " + item.Path);
+        }
+        else
+        {
+          AlertHelper.ShowLocalizedAlertWithOkOption("Message", "Item is not exist");
+        }
+      }
+      catch(Exception e) 
+      {
+        AlertHelper.ShowLocalizedAlertWithOkOption("Error", e.Message);
+      }
+      finally
+      {
+        BeginInvokeOnMainThread(delegate
+        {
+          this.HideLoader();
+        });
+      }
+    }
+
   }
 }
 
