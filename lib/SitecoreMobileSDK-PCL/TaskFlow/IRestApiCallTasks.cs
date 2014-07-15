e0e7b40422b5333e746dced7fe9a968854fcbbbe
dodikk <dodikk88.reg@gmail.com>
2014-07-15 12:02:16 +0300

    fixed spaces

--- lib/SitecoreMobileSDK-PCL/TaskFlow/IRestApiCallTasks.cs
+++ lib/SitecoreMobileSDK-PCL/TaskFlow/IRestApiCallTasks.cs
@@ -3,18 +3,18 @@
 
 namespace Sitecore.MobileSDK.TaskFlow
 {
-    using System;
-    using System.Threading;
-    using System.Threading.Tasks;
+  using System;
+  using System.Threading;
+  using System.Threading.Tasks;
 
 
   public interface IRestApiCallTasks<TRequest, THttpRequest, THttpResult, TResult> : IDownloadApiCallTasks<TRequest, THttpRequest, THttpResult>
-        where TRequest: class
-        where THttpRequest: class
-        where THttpResult: class
-        where TResult: class
-    {
-        Task<TResult> ParseResponseDataAsync(THttpResult httpData, CancellationToken cancelToken);
-    }
+    where TRequest: class
+    where THttpRequest: class
+    where THttpResult: class
+    where TResult: class
+  {
+    Task<TResult> ParseResponseDataAsync(THttpResult httpData, CancellationToken cancelToken);
+  }
 }
 
