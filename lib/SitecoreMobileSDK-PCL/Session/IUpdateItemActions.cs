0b19c9b8c4e8e46497edb7e979cd30c478d8a13f
Igor <igor.kh.mail@gmail.com>
2014-07-16 15:52:07 +0300

    Update interface for session created

---
+++ lib/SitecoreMobileSDK-PCL/Session/IUpdateItemActions.cs
@@ -0,0 +1,16 @@
+﻿namespace Sitecore.MobileSDK.Session
+{
+  using System.Threading;
+  using System.Threading.Tasks;
+
+  using Sitecore.MobileSDK.Items;
+  using Sitecore.MobileSDK.UrlBuilder.CreateItem;
+
+
+  public interface ICreateItemActions
+  {
+    Task<ScItemsResponse> CreateItemAsync(ICreateItemByIdRequest   request, CancellationToken cancelToken = default(CancellationToken));
+    Task<ScItemsResponse> CreateItemAsync(ICreateItemByPathRequest request, CancellationToken cancelToken = default(CancellationToken));
+  }
+}
+
