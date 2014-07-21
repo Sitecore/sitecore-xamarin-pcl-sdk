9b5f5cb50745f9052dc78034b9552e8d85edca57
dodikk <dodikk88.reg@gmail.com>
2014-07-21 14:25:06 +0300

    [validation] Fixed spacing

--- lib/SitecoreMobileSDK-PCL/Validators/SitecoreQueryValidator.cs
+++ lib/SitecoreMobileSDK-PCL/Validators/SitecoreQueryValidator.cs
@@ -1,20 +1,20 @@
-﻿using System;
-
-namespace Sitecore.MobileSDK
+﻿namespace Sitecore.MobileSDK
 {
-    public class SitecoreQueryValidator
-    {
-        private SitecoreQueryValidator ()
-        {
-        }
+  using System;
 
-        public static void ValidateSitecoreQuery(string sitecoreQuery)
-        {
-            if ( string.IsNullOrWhiteSpace(sitecoreQuery) )
-            {
-                throw new ArgumentNullException ("SitecoreQuery cannot be null");
-            }
-        }
+  public class SitecoreQueryValidator
+  {
+    private SitecoreQueryValidator ()
+    {
     }
+
+    public static void ValidateSitecoreQuery(string sitecoreQuery)
+    {
+      if ( string.IsNullOrWhiteSpace(sitecoreQuery) )
+      {
+        throw new ArgumentNullException ("SitecoreQuery cannot be null");
+      }
+    }
+  }
 }
 
