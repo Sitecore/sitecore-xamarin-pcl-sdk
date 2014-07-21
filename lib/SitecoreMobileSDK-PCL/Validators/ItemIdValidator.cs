faee61d7a14a4e2f1cd06d838380d3095d7318e9
dodikk <dodikk88.reg@gmail.com>
2014-07-21 14:23:58 +0300

    [validation] Fixed exception type

--- lib/SitecoreMobileSDK-PCL/Validators/ItemIdValidator.cs
+++ lib/SitecoreMobileSDK-PCL/Validators/ItemIdValidator.cs
@@ -1,30 +1,30 @@
-﻿using System;
-
-namespace Sitecore.MobileSDK
+﻿namespace Sitecore.MobileSDK.Validators
 {
-    public class ItemIdValidator
+  using System;
+
+  public class ItemIdValidator
+  {
+    private ItemIdValidator ()
     {
-        private ItemIdValidator ()
-        {
-        }
-
-        public static void ValidateItemId(string itemId)
-        {
-            if (string.IsNullOrWhiteSpace(itemId))
-            {
-                throw new ArgumentNullException("Item id cannot be null");
-            }
-
-            bool hasOpeningBrace = itemId.StartsWith("{");
-            bool hasClosingBrace = itemId.EndsWith("}");
-            bool hasNonBraceSymbols = (itemId.Length > 2);
-
-            bool isValidId = hasOpeningBrace && hasClosingBrace && hasNonBraceSymbols;
-            if (!isValidId)
-            {
-                throw new ArgumentException("Item id must have curly braces '{}'");
-            }
-        }
     }
+
+    public static void ValidateItemId(string itemId)
+    {
+      if (string.IsNullOrWhiteSpace(itemId))
+      {
+        throw new ArgumentException("Item id cannot be null");
+      }
+
+      bool hasOpeningBrace = itemId.StartsWith("{");
+      bool hasClosingBrace = itemId.EndsWith("}");
+      bool hasNonBraceSymbols = (itemId.Length > 2);
+
+      bool isValidId = hasOpeningBrace && hasClosingBrace && hasNonBraceSymbols;
+      if (!isValidId)
+      {
+        throw new ArgumentException("Item id must have curly braces '{}'");
+      }
+    }
+  }
 }
 
