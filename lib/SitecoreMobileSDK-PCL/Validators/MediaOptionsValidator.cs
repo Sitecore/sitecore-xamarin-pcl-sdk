20cccb2eb926640c8761dec453347fec950f5a0b
dodikk <dodikk88.reg@gmail.com>
2014-07-22 15:35:14 +0300

    [media] Extracted a validation class

---
+++ lib/SitecoreMobileSDK-PCL/Validators/MediaOptionsValidator.cs
@@ -0,0 +1,22 @@
+﻿namespace Sitecore.MobileSDK.Validators
+{
+  using Sitecore.MobileSDK.API.Request.Parameters;
+
+  public static class MediaOptionsValidator
+  {
+    public static bool IsValidMediaOptions(IDownloadMediaOptions options)
+    {
+      if (null == options)
+      {
+        return false;
+      }
+      else if (options.IsEmpty)
+      {
+        return false;
+      }
+
+      return true;
+    }
+  }
+}
+
