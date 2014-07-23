c205c0d94604614402b5fce485ebcde5055e7f95
dodikk <dodikk88.reg@gmail.com>
2014-07-23 13:11:06 +0300

    [media] Extracted an interface for future refactoring

---
+++ lib/SitecoreMobileSDK-PCL/API/IMediaLibrarySettings.cs
@@ -0,0 +1,21 @@
+﻿namespace Sitecore.MobileSDK.API
+{
+  public interface IMediaLibrarySettings
+  {
+    string MediaLybraryRoot
+    {
+      get;
+    }
+
+    string DefaultMediaResourceExtension
+    {
+      get;
+    }
+
+    string MediaPrefix
+    {
+      get;
+    }
+  }
+}
+
