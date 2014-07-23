11233e739ea19867bf51fa6eb06e48bc2d92411b
Alina Zozulya <alinka.zozulya@gmail.com>
2014-07-23 16:07:33 +0300

    tests added

--- test/Portable/MobileSDK-IntegrationTest/DeleteItemsTest.cs
+++ test/Portable/MobileSDK-IntegrationTest/DeleteItemsTest.cs
@@ -1,9 +1,11 @@
 ﻿namespace MobileSDKIntegrationTest
 {
+  using System;
   using System.Threading.Tasks;
   using NUnit.Framework;
 
   using Sitecore.MobileSDK.API;
+  using Sitecore.MobileSDK.API.Exceptions;
   using Sitecore.MobileSDK.API.Items;
   using Sitecore.MobileSDK.API.Request.Parameters;
   using Sitecore.MobileSDK.API.Session;
@@ -99,7 +101,161 @@
       Assert.AreEqual(item2.Id, result.ItemsIds[1]);
     }
 
+    [Test]
+    public async void TestDeleteItemByIdbWithParentAndChildrenScope()
+    {
 
+      ISitecoreItem parentItem = await this.CreateItem("Parent item");
+      ISitecoreItem selfItem = await this.CreateItem("Self item", parentItem);
+      ISitecoreItem childItem = await this.CreateItem("Child item", selfItem);
+
+      var request = ItemWebApiRequestBuilder.DeleteItemRequestWithPath(selfItem.Path)
+        .AddScope(ScopeType.Parent)
+        .AddScope(ScopeType.Children)
+        .Build();
+
+      var result = await this.session.DeleteItemAsync(request);
+      Assert.AreEqual(2, result.Count);
+      Assert.AreEqual(parentItem.Id, result.ItemsIds[0]);
+      Assert.AreEqual(childItem.Id, result.ItemsIds[1]);
+    }
+
+    [Test]
+    public async void TestDeleteInternationalItemByPathbWithChildrenScope()
+    {
+
+      ISitecoreItem selfItem = await this.CreateItem("インターナショナル عالمي JJ ж");
+      ISitecoreItem childItem = await this.CreateItem("インターナショナル", selfItem);
+
+      var request = ItemWebApiRequestBuilder.DeleteItemRequestWithPath(selfItem.Path)
+        .AddScope(ScopeType.Children)
+        .Build();
+
+      var result = await this.session.DeleteItemAsync(request);
+      Assert.AreEqual(1, result.Count);
+      Assert.AreEqual(childItem.Id, result.ItemsIds[0]);
+    }
+
+    [Test]
+    public async void TestDeleteItemByQueryWithChildrenAndSelfScope()
+    {
+
+      ISitecoreItem parentItem = await this.CreateItem("Parent item");
+      ISitecoreItem selfItem = await this.CreateItem("Self item", parentItem);
+      await this.CreateItem("Child item", selfItem);
+
+      var request = ItemWebApiRequestBuilder.DeleteItemRequestWithSitecoreQuery(testData.Items.CreateItemsHere.Path + "/descendant::*[@@templatename='Sample Item']")
+        .AddScope(ScopeType.Children)
+        .AddScope(ScopeType.Self)
+        .Build();
+
+      var result = await this.session.DeleteItemAsync(request);
+      Assert.AreEqual(5, result.Count);  //but 3 items was deleted in fact (Item Web API issue)
+
+    }
+
+    [Test]
+    public async void TestDeleteItemByIdAsAnonymousFromShellSiteReturnsException()
+    {
+      var anonymousSession = SitecoreWebApiSessionBuilder.AnonymousSessionWithHost(testData.InstanceUrl)
+        .DefaultDatabase("master")
+        .Site(testData.ShellSite)
+        .BuildSession();
+
+      ISitecoreItem item = await this.CreateItem("Item to delete as anonymous");
+
+      var request = ItemWebApiRequestBuilder.DeleteItemRequestWithPath(item.Path)
+        .Build();
+
+      TestDelegate testCode = async () =>
+      {
+        var task = anonymousSession.DeleteItemAsync(request);
+        await task;
+      };
+      Exception exception = Assert.Throws<ParserException>(testCode);
+      Assert.AreEqual("[Sitecore Mobile SDK] Unable to download data from the internet", exception.Message);
+      Assert.AreEqual("Sitecore.MobileSDK.API.Exceptions.WebApiJsonErrorException", exception.InnerException.GetType().ToString());
+      Assert.AreEqual("Access to site is not granted.", exception.InnerException.Message);
+
+      await session.DeleteItemAsync(request);
+    }
+
+    [Test]
+    public async void TestDeleteItemByPathWithoutDeleteAccessReturnsException()
+    {
+      var noAccessSession = SitecoreWebApiSessionBuilder.AuthenticatedSessionWithHost(testData.InstanceUrl)
+        .Credentials(testData.Users.NoCreateAccess)
+        .DefaultDatabase("master")
+        .Site(testData.ShellSite)
+        .BuildSession();
+
+      ISitecoreItem item = await this.CreateItem("Item to delete without delete access");
+
+      var request = ItemWebApiRequestBuilder.DeleteItemRequestWithPath(item.Path)
+        .Build();
+
+      TestDelegate testCode = async () =>
+      {
+        var task = noAccessSession.DeleteItemAsync(request);
+        await task;
+      };
+      Exception exception = Assert.Throws<ParserException>(testCode);
+      Assert.AreEqual("[Sitecore Mobile SDK] Unable to download data from the internet", exception.Message);
+      Assert.AreEqual("Sitecore.MobileSDK.API.Exceptions.WebApiJsonErrorException", exception.InnerException.GetType().ToString());
+      Assert.True(exception.InnerException.Message.Contains("DeleteItem - Delete right required"));
+
+      await session.DeleteItemAsync(request);
+    }
+
+    [Test]
+    public  void TestDeleteItemByIdWithDuplicateScopeReturnsException()
+    {
+      var exception = Assert.Throws<InvalidOperationException>(() => ItemWebApiRequestBuilder.DeleteItemRequestWithId("{SAMPLEID-7808-4798-A461-1FB3EB0A43E5}")
+        .AddScope(ScopeType.Self)
+        .AddScope(ScopeType.Self)
+        .Build());
+      Assert.AreEqual("Adding scope parameter duplicates is forbidden", exception.Message);
+
+    }
+
+    [Test]
+    public void TestDeleteItemWithInvalidPathReturnsException()
+    {
+      var exception = Assert.Throws<ArgumentException>(() => ItemWebApiRequestBuilder.DeleteItemRequestWithPath("invalid path )").Build());
+      Assert.AreEqual("Item path should begin with '/'", exception.Message);
+
+    }
+
+    [Test]
+    public void TestDeleteItemByQueryWithNullScopeReturnsException()
+    {
+      var exception = Assert.Throws<ArgumentException>(() => ItemWebApiRequestBuilder.DeleteItemRequestWithSitecoreQuery("sample query")
+        .AddScope(null)
+        .Build());
+      Assert.AreEqual("DeleteItemItemByQueryRequestBuilder.Scope: The input cannot be null or empty", exception.Message);
+
+    }
+
+    [Test]
+    public void TestDeleteItemByQueryWithTwoDatabasesReturnsException()
+    {
+      var exception = Assert.Throws<InvalidOperationException>(() => ItemWebApiRequestBuilder.DeleteItemRequestWithSitecoreQuery("sample query")
+        .Database("1")
+        .Database("2")
+        .Build());
+      Assert.AreEqual("DeleteItemItemByQueryRequestBuilder.Database : The input cannot be set twice", exception.Message);
+
+    }
+
+    [Test]
+    public void TestDeleteItemByPathWithNullDatabaseReturnsException()
+    {
+      var exception = Assert.Throws<ArgumentException>(() => ItemWebApiRequestBuilder.DeleteItemRequestWithPath("/sample path")
+        .Database(null)
+        .Build());
+      Assert.AreEqual("DeleteItemItemByPathRequestBuilder.Database: The input cannot be null or empty", exception.Message);
+
+    }
 
     private async Task<ISitecoreItem> CreateItem(string itemName, ISitecoreItem parentItem = null, ISitecoreWebApiSession itemSession = null)
     {
