81ef8c57458603aa265a2fae5897359f074d8a6d
dodikk <dodikk88.reg@gmail.com>
2014-07-25 18:50:34 +0300

    [session] Split mock objects

---
+++ test/Portable/SitecoreMobileSDK-MockObjects/MutableWebApiCredentialsPOD.cs
@@ -0,0 +1,32 @@
+﻿namespace SitecoreMobileSDKMockObjects
+{
+  using System;
+  using Sitecore.MobileSDK.API;
+
+  public class MutableWebApiCredentialsPOD : IWebApiCredentials
+  {
+    public MutableWebApiCredentialsPOD(string username, string password)
+    {
+      this.Username = username;
+      this.Password = password;
+    }
+
+    public IWebApiCredentials CredentialsShallowCopy()
+    {
+      return new MutableWebApiCredentialsPOD(this.Username, this.Password);
+    }
+
+    public string Username
+    {
+      get;
+      set;
+    }
+
+    public string Password
+    {
+      get;
+      set;
+    }  
+  }
+}
+
