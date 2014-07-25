6a920e4b3bc0a963f59c1c7173075e717e8cb4d1
dodikk <dodikk88.reg@gmail.com>
2014-07-25 18:54:43 +0300

    [session] Removed unwanted properties from SessionConfig class

--- lib/SitecoreMobileSDK-PCL/SessionSettings/SessionConfig.cs
+++ lib/SitecoreMobileSDK-PCL/SessionSettings/SessionConfig.cs
@@ -9,117 +9,32 @@ namespace Sitecore.MobileSDK.SessionSettings
   using Sitecore.MobileSDK.Validators;
 
   // TODO : remove this class
-  public class SessionConfig : 
-    ISessionConfig, 
-    IWebApiCredentials,
-    IMediaLibrarySettings // TODO : remove this interface
+  public class SessionConfig : ISessionConfig
   {
-    #region Constructor
-    public static SessionConfig NewAnonymousSessionConfig(
+    public SessionConfig(
       string instanceUrl, 
       string site = null, 
       string itemWebApiVersion = "v1")
-    {
-      return new SessionConfig(
-        instanceUrl, 
-        null, 
-        null, 
-        site, 
-        itemWebApiVersion, 
-        "/sitecore/media library",
-        "ashx",
-        "~/media");
-    }
-
-    public static SessionConfig NewAuthenticatedSessionConfig(
-      string instanceUrl, 
-      string login, 
-      string password, 
-      string site = null, 
-      string itemWebApiVersion = "v1")
-    {
-      return new SessionConfig(
-        instanceUrl, 
-        login, password, 
-        site, 
-        itemWebApiVersion,
-        "/sitecore/media library",
-        "ashx",
-        "~/media");
-    }
-
-    public static SessionConfig NewSessionConfig(
-      string instanceUrl, 
-      string login, 
-      string password, 
-      string site, 
-      string itemWebApiVersion,
-      string mediaLibraryRoot,
-      string defaultMediaResourceExtension,
-      string mediaPrefix)
-    {
-      return new SessionConfig(
-        instanceUrl, 
-        login, 
-        password, 
-        site, 
-        itemWebApiVersion,
-        mediaLibraryRoot,
-        defaultMediaResourceExtension,
-        mediaPrefix);
-    }
-
-    protected SessionConfig(
-      string instanceUrl, 
-      string login, 
-      string password, 
-      string site, 
-      string itemWebApiVersion,
-      string mediaLibraryRoot,
-      string defaultMediaResourceExtension,
-      string mediaPrefix)
     {
       this.InstanceUrl = instanceUrl;
-      this.Username    = login;
-      this.Password    = password;
       this.Site        = site;
       this.ItemWebApiVersion = itemWebApiVersion;
 
-      this.MediaLibraryRoot = mediaLibraryRoot;
-      this.DefaultMediaResourceExtension = defaultMediaResourceExtension;
-      this.MediaPrefix = mediaPrefix;
-
       this.Validate();
     }
-    #endregion Constructor
 
     #region ICloneable
     public virtual SessionConfig ShallowCopy()
     {
       SessionConfig result = new SessionConfig(
         this.InstanceUrl, 
-        this.Username, 
-        this.Password, 
         this.Site, 
-        this.ItemWebApiVersion,
-        this.MediaLibraryRoot,
-        this.DefaultMediaResourceExtension,
-        this.MediaPrefix);
+        this.ItemWebApiVersion);
 
       return result;
     }
 
     public virtual ISessionConfig SessionConfigShallowCopy()
-    {
-      return this.ShallowCopy();
-    }
-
-    public virtual IWebApiCredentials CredentialsShallowCopy()
-    {
-      return this.ShallowCopy();
-    }
-
-    public IMediaLibrarySettings MediaSettingsShallowCopy()
     {
       return this.ShallowCopy();
     }
@@ -137,30 +52,14 @@ namespace Sitecore.MobileSDK.SessionSettings
       get; 
       protected set; 
     }
-
-    public string Username
-    {
-      get;
-      protected set;
-    }
-
-    public string Password
-    {
-      get;
-      protected set;
-    }
-
     public string ItemWebApiVersion
     {
       get; 
       protected set;
     }
+    #endregion Properties
 
-    public bool IsAnonymous()
-    {
-      return string.IsNullOrEmpty(this.Username) && string.IsNullOrEmpty(this.Password);
-    }
-
+    #region Validation
     private void Validate()
     {
       WebApiParameterValidator.ValidateParameterAndThrowErrorWithMessage
@@ -174,51 +73,13 @@ namespace Sitecore.MobileSDK.SessionSettings
         "SessionConfig.ItemWebApiVersion is required"
       );
 
-      bool hasLogin = !string.IsNullOrWhiteSpace(this.Username);
-      bool hasPassword = !string.IsNullOrWhiteSpace(this.Password);
-      if (!hasLogin && hasPassword)
-      {
-        throw new ArgumentException("SessionConfig.Credentials : login is required for authenticated session");
-      }
-
-      if (hasLogin && !hasPassword)
-      {
-        throw new ArgumentException("SessionConfig.Credentials : password is required for authenticated session");
-      }
 
       if (!SessionConfigValidator.IsValidSchemeOfInstanceUrl(this.InstanceUrl))
       {
         Debug.WriteLine("[WARNING] : SessionConfig - instance URL does not have a scheme");
       }
     }
-
-    #endregion Properties
-
-    #region Media Properties
-
-    public string MediaLibraryRoot
-    {
-      get;
-      private set;
-    }
-
-    public string DefaultMediaResourceExtension
-    {
-      get;
-      private set;
-    }
-
-    public string MediaPrefix
-    {
-      get;
-      private set;
-    }
-
-    #endregion Media Properties
-
-    #region Instance Variables
-    private string mediaLibraryRoot;
-    #endregion Instance Variables
+    #endregion Validation
   }
 }
 
