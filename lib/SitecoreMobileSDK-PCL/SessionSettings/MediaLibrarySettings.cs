6b3635084af3d2918452b344e8d407ea3650333f
dodikk <dodikk88.reg@gmail.com>
2014-07-25 16:56:20 +0300

    [media] Added a class for settings

---
+++ lib/SitecoreMobileSDK-PCL/SessionSettings/MediaLibrarySettings.cs
@@ -0,0 +1,45 @@
+﻿namespace Sitecore.MobileSDK.SessionSettings
+{
+  using Sitecore.MobileSDK.API;
+
+  public class MediaLibrarySettings : IMediaLibrarySettings
+  {
+    public MediaLibrarySettings(
+      string mediaLibraryRoot,
+      string defaultMediaResourceExtension,
+      string mediaPrefix)
+    {
+      this.MediaLibraryRoot = mediaLibraryRoot;
+      this.DefaultMediaResourceExtension = defaultMediaResourceExtension;
+      this.MediaPrefix = mediaPrefix;
+    }
+
+    public IMediaLibrarySettings MediaSettingsShallowCopy()
+    {
+      return new MediaLibrarySettings(
+        this.MediaLibraryRoot,
+        this.DefaultMediaResourceExtension,
+        this.MediaPrefix);
+    }
+
+    public string MediaLibraryRoot
+    {
+      get;
+      private set;
+    }
+
+    public string DefaultMediaResourceExtension
+    {
+      get;
+      private set;
+    }
+
+    public string MediaPrefix
+    {
+      get;
+      private set;
+    }
+
+  }
+}
+
