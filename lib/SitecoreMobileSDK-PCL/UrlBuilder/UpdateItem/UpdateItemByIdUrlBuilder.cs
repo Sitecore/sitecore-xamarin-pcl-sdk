f248aadaceba404e93f137a1d30c89f7cdcf0bf7
Igor <igor.kh.mail@gmail.com>
2014-07-28 15:53:02 +0300

    class moved to root folder

---
+++ lib/SitecoreMobileSDK-PCL/UrlBuilder/UpdateItem/UpdateItemByIdUrlBuilder.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Sitecore.MobileSDK.UrlBuilder.UpdateItem
+{
+  using System;
+  using Sitecore.MobileSDK.Utils;
+  using Sitecore.MobileSDK.UrlBuilder.Rest;
+  using Sitecore.MobileSDK.UrlBuilder.WebApi;
+  using Sitecore.MobileSDK.Validators;
+  using Sitecore.MobileSDK.API.Request;
+
+  public class UpdateItemByIdUrlBuilder : AbstractUpdateItemUrlBuilder<IUpdateItemByIdRequest>
+  {
+    public UpdateItemByIdUrlBuilder(IRestServiceGrammar restGrammar, IWebApiUrlParameters webApiGrammar)
+      : base( restGrammar, webApiGrammar )
+    {
+    }
+
+    //TODO: igk common part for all requests "by id", we should merge it somehow!!!
+    protected override string GetSpecificPartForRequest(IUpdateItemByIdRequest request)
+    {
+      string escapedId = UrlBuilderUtils.EscapeDataString(request.ItemId).ToLowerInvariant();
+
+      string result = 
+          this.restGrammar.FieldSeparator 
+        + this.webApiGrammar.ItemIdParameterName
+        + this.restGrammar.KeyValuePairSeparator
+        + escapedId;
+
+      return result;
+    }
+
+    protected override void ValidateSpecificRequest(IUpdateItemByIdRequest request)
+    {
+      ItemIdValidator.ValidateItemId(request.ItemId);
+    }
+  }
+}
+
