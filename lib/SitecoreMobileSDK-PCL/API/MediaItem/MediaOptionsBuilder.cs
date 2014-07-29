2c1d001bef0ed626a0b70237e3ae2fded831b7f3
dodikk <dodikk88.reg@gmail.com>
2014-07-29 18:20:48 +0300

    [media] Fixed DSL to force using ".Set" syntax

--- lib/SitecoreMobileSDK-PCL/API/MediaItem/MediaOptionsBuilder.cs
+++ lib/SitecoreMobileSDK-PCL/API/MediaItem/MediaOptionsBuilder.cs
@@ -1,97 +1,26 @@
-﻿namespace Sitecore.MobileSDK.API.MediaItem
+﻿
+
+namespace Sitecore.MobileSDK.API.MediaItem
 {
   using System;
 
+  using Sitecore.MobileSDK.Media;
   using Sitecore.MobileSDK.API.Request.Parameters;
   using Sitecore.MobileSDK.UrlBuilder.MediaItem;
 
 
   public class MediaOptionsBuilder
   {
-    public MediaOptionsBuilder()
-    {
-      this.options = new DownloadMediaOptions();
-    }
-
-
-    public MediaOptionsBuilder Set
+    public IMediaOptionsBuilder Set
     {
       // @adk : as suggested in the sample
       // http://stackoverflow.com/questions/1622662/creating-api-that-is-fluent
 
       get
       {
-        return this;
+        return new MediaOptionsBuilderImpl();
       }
     }
-
-
-    // before fix - as described in Wikipedia http://ru.wikipedia.org/wiki/Fluent_interface
-    public MediaOptionsBuilder Width(int width)
-    {
-      this.options.SetWidth(width);
-      return this;
-    }
-
-    public MediaOptionsBuilder Height(int height)
-    {
-      this.options.SetHeight(height);
-      return this;
-    }
-
-    public MediaOptionsBuilder MaxWidth(int maxWidth)
-    {
-      this.options.SetMaxWidth(maxWidth);
-      return this;
-    }
-
-    public MediaOptionsBuilder MaxHeight(int maxHeight)
-    {
-      this.options.SetMaxHeight(maxHeight);
-      return this;
-    }
-
-    public MediaOptionsBuilder BackgroundColor(string color)
-    {
-      this.options.SetBackgroundColor(color);
-      return this;
-    }
-
-    public MediaOptionsBuilder DisableMediaCache(bool value)
-    {
-      this.options.SetDisableMediaCache(value);
-      return this;
-    }
-
-    public MediaOptionsBuilder AllowStrech(bool value)
-    {
-      this.options.SetAllowStrech(value);
-      return this;
-    }
-
-    public MediaOptionsBuilder Scale(float scale)
-    {
-      this.options.SetScale(scale);
-      return this;
-    }
-
-    public MediaOptionsBuilder DisplayAsThumbnail(bool value)
-    {
-      this.options.SetDisplayAsThumbnail(value);
-      return this;
-    }
-
-    public IDownloadMediaOptions Build()
-    {
-      if (null == this.options)
-      {
-        throw new InvalidOperationException("[MediaOptionsBuilder] Nothing to build since no options have been specified by the user");
-      }
-
-      return this.options;
-    }
-
-    private DownloadMediaOptions options;
   }
 }
 
---
+++ lib/SitecoreMobileSDK-PCL/Media/MediaOptionsBuilderImpl.cs
@@ -0,0 +1,84 @@
+﻿using Sitecore.MobileSDK.API.MediaItem;
+using Sitecore.MobileSDK.UrlBuilder.MediaItem;
+using Sitecore.MobileSDK.API.Request.Parameters;
+using System;
+
+namespace Sitecore.MobileSDK.Media
+{
+
+  public class MediaOptionsBuilderImpl : IMediaOptionsBuilder
+  {
+    public MediaOptionsBuilderImpl()
+    {
+      this.options = new DownloadMediaOptions();
+    }
+
+    // before fix - as described in Wikipedia http://ru.wikipedia.org/wiki/Fluent_interface
+    public IMediaOptionsBuilder Width(int width)
+    {
+      this.options.SetWidth(width);
+      return this;
+    }
+
+    public IMediaOptionsBuilder Height(int height)
+    {
+      this.options.SetHeight(height);
+      return this;
+    }
+
+    public IMediaOptionsBuilder MaxWidth(int maxWidth)
+    {
+      this.options.SetMaxWidth(maxWidth);
+      return this;
+    }
+
+    public IMediaOptionsBuilder MaxHeight(int maxHeight)
+    {
+      this.options.SetMaxHeight(maxHeight);
+      return this;
+    }
+
+    public IMediaOptionsBuilder BackgroundColor(string color)
+    {
+      this.options.SetBackgroundColor(color);
+      return this;
+    }
+
+    public IMediaOptionsBuilder DisableMediaCache(bool value)
+    {
+      this.options.SetDisableMediaCache(value);
+      return this;
+    }
+
+    public IMediaOptionsBuilder AllowStrech(bool value)
+    {
+      this.options.SetAllowStrech(value);
+      return this;
+    }
+
+    public IMediaOptionsBuilder Scale(float scale)
+    {
+      this.options.SetScale(scale);
+      return this;
+    }
+
+    public IMediaOptionsBuilder DisplayAsThumbnail(bool value)
+    {
+      this.options.SetDisplayAsThumbnail(value);
+      return this;
+    }
+
+    public IDownloadMediaOptions Build()
+    {
+      if (null == this.options)
+      {
+        throw new InvalidOperationException("[MediaOptionsBuilder] Nothing to build since no options have been specified by the user");
+      }
+
+      return this.options;
+    }
+
+    private DownloadMediaOptions options;
+  }
+}
+
