052fdbdedfe4672037b040de7f84cf841e0ad1b7
dodikk <dodikk88.reg@gmail.com>
2014-07-30 16:37:44 +0300

    [code style] Fixed spaces

--- lib/SitecoreMobileSDK-PCL/PublicKey/AuthenticedSessionCryptor.cs
+++ lib/SitecoreMobileSDK-PCL/PublicKey/AuthenticedSessionCryptor.cs
@@ -1,50 +1,50 @@
 ﻿namespace Sitecore.MobileSDK.PublicKey
 {
-	using System;
+  using System;
   using System.Threading;
   using System.Threading.Tasks;
-	using System.Net.Http;
+  using System.Net.Http;
 
 
   // TODO : use secure string
-	public class AuthenticedSessionCryptor : ICredentialsHeadersCryptor
-	{
-		public AuthenticedSessionCryptor (string login, string password, PublicKeyX509Certificate certificate)
-		{
-//			TODO: validate params
-			this.login = login;
-			this.password = password;
-			this.certificate = certificate;
-		}
+  public class AuthenticedSessionCryptor : ICredentialsHeadersCryptor
+  {
+    public AuthenticedSessionCryptor (string login, string password, PublicKeyX509Certificate certificate)
+    {
+      //      TODO: validate params
+      this.login = login;
+      this.password = password;
+      this.certificate = certificate;
+    }
 
-        public async Task<HttpRequestMessage> AddEncryptedCredentialHeadersAsync (HttpRequestMessage httpRequest, CancellationToken cancelToken)
-		{
-			Func<HttpRequestMessage> setupEncryptedHeaders = () =>
-			{
-				return this.AddEncryptedCredentialHeaders( httpRequest );
-			};
-            HttpRequestMessage requestMessage = await Task.Factory.StartNew (setupEncryptedHeaders, cancelToken);
-			return requestMessage;
-		}
+    public async Task<HttpRequestMessage> AddEncryptedCredentialHeadersAsync (HttpRequestMessage httpRequest, CancellationToken cancelToken)
+    {
+      Func<HttpRequestMessage> setupEncryptedHeaders = () =>
+      {
+        return this.AddEncryptedCredentialHeaders( httpRequest );
+      };
+      HttpRequestMessage requestMessage = await Task.Factory.StartNew (setupEncryptedHeaders, cancelToken);
+      return requestMessage;
+    }
 
-		public HttpRequestMessage AddEncryptedCredentialHeaders (HttpRequestMessage httpRequest)
-		{
-			EncryptionUtil cryptor = new EncryptionUtil (this.certificate);
+    public HttpRequestMessage AddEncryptedCredentialHeaders (HttpRequestMessage httpRequest)
+    {
+      EncryptionUtil cryptor = new EncryptionUtil (this.certificate);
 
-			var encryptedLogin = cryptor.Encrypt (this.login);
-			var encryptedPassword = cryptor.Encrypt (this.password);
+      var encryptedLogin = cryptor.Encrypt (this.login);
+      var encryptedPassword = cryptor.Encrypt (this.password);
 
-			httpRequest.Headers.Add ("X-Scitemwebapi-Username", encryptedLogin);
-			httpRequest.Headers.Add ("X-Scitemwebapi-Password", encryptedPassword);
-			httpRequest.Headers.Add ("X-Scitemwebapi-Encrypted", "1");
+      httpRequest.Headers.Add ("X-Scitemwebapi-Username", encryptedLogin);
+      httpRequest.Headers.Add ("X-Scitemwebapi-Password", encryptedPassword);
+      httpRequest.Headers.Add ("X-Scitemwebapi-Encrypted", "1");
 
-			return httpRequest;
+      return httpRequest;
 
-		}
+    }
 
-		private readonly string password;
-		private readonly string login;
-		private PublicKeyX509Certificate certificate;
-	}
+    private readonly string password;
+    private readonly string login;
+    private PublicKeyX509Certificate certificate;
+  }
 }
 
