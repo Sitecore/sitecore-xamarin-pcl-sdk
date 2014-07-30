d5e4262bbcc26cf3701ce13bb8b3ab1a93666146
dodikk <dodikk88.reg@gmail.com>
2014-07-30 18:03:01 +0300

    [DefaultSource] More constants removal

--- lib/SitecoreMobileSDK-PCL/Items/ItemSource.cs
+++ lib/SitecoreMobileSDK-PCL/Items/ItemSource.cs
@@ -1,15 +1,10 @@
-﻿
-
-namespace Sitecore.MobileSDK.Items
+﻿namespace Sitecore.MobileSDK.Items
 {
   using System;
   using Sitecore.MobileSDK.API.Items;
 
   public class ItemSource : ItemSourcePOD
   {
-    public const string DefaultDatabase = "web";
-    public const string DefaultLanguage = "en";
-
     public ItemSource(string database, string language, string version = null) 
       : base(database, language, version)
     {
