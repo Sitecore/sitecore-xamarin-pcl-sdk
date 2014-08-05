f009abaf87597cacc93b4ba4648303091298c410
Igor <igor.kh.mail@gmail.com>
2014-08-05 16:01:47 +0300

    read requests builders moved to the separate folder

---
+++ lib/SitecoreMobileSDK-PCL/UserRequest/ReadRequest/ReadItemByQueryRequestBuilder.cs
@@ -0,0 +1,24 @@
+﻿namespace Sitecore.MobileSDK.UserRequest
+{
+  using Sitecore.MobileSDK.API.Request;
+  using Sitecore.MobileSDK.Validators;
+
+  public class ReadItemByQueryRequestBuilder : AbstractBaseRequestBuilder<IReadItemsByQueryRequest>
+  {
+    public ReadItemByQueryRequestBuilder(string sitecoreQuery)
+    {
+      BaseValidator.CheckForNullEmptyAndWhiteSpaceOrThrow(sitecoreQuery, this.GetType().Name + ".SitecoreQuery");
+
+      this.sitecoreQuery = sitecoreQuery;
+    }
+
+    public override IReadItemsByQueryRequest Build()
+    {
+      var result = new ReadItemByQueryParameters(null, this.itemSourceAccumulator, this.queryParameters, this.sitecoreQuery);
+      return result;
+    }
+
+    private string sitecoreQuery;
+  }
+}
+
