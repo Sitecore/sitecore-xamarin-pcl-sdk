2d27ef5eb3d5ab7c2a1c3dc3173f4bf2b2631db8
Igor <igor.kh.mail@gmail.com>
2014-08-07 16:48:49 +0300

    credentials insecure implementation for WL demo

---
+++ app/WhiteLabel/iOS/WhiteLabel-iOS/Helpers/WebApiCredentialsPODInsequredDemo.cs
@@ -0,0 +1,38 @@
+﻿namespace WhiteLabeliOS
+{
+  using System;
+  using Sitecore.MobileSDK.API;
+
+  public class WebApiCredentialsPODInsequredDemo : IWebApiCredentials
+  {
+    public WebApiCredentialsPODInsequredDemo(string username, string password)
+    {
+      this.Username = username;
+      this.Password = password;
+    }
+
+    public IWebApiCredentials CredentialsShallowCopy()
+    {
+      return new WebApiCredentialsPODInsequredDemo(this.Username, this.Password);
+    }
+
+    public void Dispose()
+    {
+      this.Username = null;
+      this.Password = null;
+    }
+
+    public string Username
+    {
+      get;
+      private set;
+    }
+
+    public string Password
+    {
+      get;
+      private set;
+    }
+  }
+}
+
