75ee1f93ba9678a5d7a92a46782ab40c5a40d398
Igor <igor.kh.mail@gmail.com>
2014-08-07 16:49:49 +0300

    authentication WL demo implementation

--- app/WhiteLabel/iOS/WhiteLabel-iOS/ITemTasks/AuthViewController.designer.cs
+++ app/WhiteLabel/iOS/WhiteLabel-iOS/ITemTasks/AuthViewController.designer.cs
@@ -1,18 +1,53 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
 
 namespace WhiteLabeliOS
 {
-  [Register ("AuthViewController")]
-  partial class AuthViewController
-  {
-    void ReleaseDesignerOutlets ()
-    {
-    }
-  }
+	[Register ("AuthViewController")]
+	partial class AuthViewController
+	{
+		[Outlet]
+		MonoTouch.UIKit.UIButton authButton { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITextField loginField { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITextField passwordField { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITextField urlField { get; set; }
+
+		[Action ("OnAuthButtonTapped:")]
+		partial void OnAuthButtonTapped (MonoTouch.UIKit.UIButton sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (authButton != null) {
+				authButton.Dispose ();
+				authButton = null;
+			}
+
+			if (loginField != null) {
+				loginField.Dispose ();
+				loginField = null;
+			}
+
+			if (passwordField != null) {
+				passwordField.Dispose ();
+				passwordField = null;
+			}
+
+			if (urlField != null) {
+				urlField.Dispose ();
+				urlField = null;
+			}
+		}
+	}
 }
