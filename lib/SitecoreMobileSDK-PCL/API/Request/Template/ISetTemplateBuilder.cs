ebf41e465c36eb8f0893c7db3b346ac0265263c2
dodikk <dodikk88.reg@gmail.com>
2014-08-08 19:37:20 +0300

    [template] Added a builder for mutually exclusive setters

---
+++ lib/SitecoreMobileSDK-PCL/API/Request/Template/ISetTemplateBuilder.cs
@@ -0,0 +1,9 @@
+﻿namespace Sitecore.MobileSDK.API.Request.Template
+{
+  public interface ISetTemplateBuilder<T> where T : class
+  {
+    ICreateItemRequestParametersBuilder<T> ItemTemplatePath(string template);
+    ICreateItemRequestParametersBuilder<T> ItemTemplateId(string template);
+  }
+}
+
