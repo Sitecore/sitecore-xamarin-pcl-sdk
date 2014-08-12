bbe7a5b4d8635dee581c3ce1848861a99fec4fc4
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2014-08-12 15:23:21 +0300

    [javadoc] Added javadoc for exceptions.

--- lib/SitecoreMobileSDK-PCL/API/Exceptions/RsaHandshakeException.cs
+++ lib/SitecoreMobileSDK-PCL/API/Exceptions/RsaHandshakeException.cs
@@ -2,8 +2,16 @@ namespace Sitecore.MobileSDK.API.Exceptions
 {
   using System;
 
+  /// <summary>
+  /// The exception that is thrown when error happens during encryption user's data.
+  /// </summary>
   public class RsaHandshakeException : SitecoreMobileSdkException
   {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RsaHandshakeException"/> class.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for this exception.</param>
+    /// <param name="inner">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
     public RsaHandshakeException(string message, Exception inner = null)
       : base(message, inner)
     {
