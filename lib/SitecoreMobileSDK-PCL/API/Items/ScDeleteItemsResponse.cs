c46ffa95668282270e2b6cb4b13b902e7b08f98d
dodikk <dodikk88.reg@gmail.com>
2014-08-12 13:52:24 +0300

    [delete response] Made "Count" property virtual

--- lib/SitecoreMobileSDK-PCL/API/Items/ScDeleteItemsResponse.cs
+++ lib/SitecoreMobileSDK-PCL/API/Items/ScDeleteItemsResponse.cs
@@ -4,13 +4,19 @@
 
   public class ScDeleteItemsResponse
   {
-    public int Count { get; private set; }
+    public int Count 
+    { 
+      get
+      {
+        return this.ItemsIds.Count;
+      }
+    }
 
     public List<string> ItemsIds { get; private set; }
 
     public ScDeleteItemsResponse(int count, List<string> itemsIds)
     {
-      this.Count = count;
+//      this.Count = count;
       this.ItemsIds = itemsIds;
     }
   }
