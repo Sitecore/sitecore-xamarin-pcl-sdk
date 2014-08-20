48d0e2d5df89104df1400bb282129d93af007846
Igor <igor.kh.mail@gmail.com>
2014-08-20 13:59:07 +0300

    java doc fix

--- lib/SitecoreMobileSDK-PCL/API/Session/IBaseSessionBuilder.cs
+++ lib/SitecoreMobileSDK-PCL/API/Session/IBaseSessionBuilder.cs
@@ -14,7 +14,7 @@ namespace Sitecore.MobileSDK.API.Session
     ISitecoreWebApiSession BuildSession();
 
     /// <summary>
-    /// Builds readonly session.
+    /// Builds session which contain read only operations.
     /// </summary>
     /// <returns>
     ///   <see cref="ISitecoreWebApiReadonlySession" />
@@ -23,18 +23,20 @@ namespace Sitecore.MobileSDK.API.Session
 
     /// <summary>
     /// Specifies site parameter.
-    /// For example:"/sitecore/shell"
+    /// This parameter value will be used if appropriate parameter will be missed in request object.
+    /// For example: "/sitecore/shell"
     /// 
     /// The value is case insensitive.
     /// </summary>
-    /// <param name="site">Site parameter</param>
+    /// <param name="site">Site parameter.</param>
     /// <returns>
     /// this
     /// </returns>
     IBaseSessionBuilder Site(string site);
 
     /// <summary>
-    /// Specifies WebAPI version, "v1" by default.
+    /// Specifies WebAPI version, 'v1' by default.
+    /// This parameter value will be used if appropriate parameter will be missed in request object.
     /// 
     /// The value is case insensitive.
     /// </summary>
@@ -45,33 +47,35 @@ namespace Sitecore.MobileSDK.API.Session
     IBaseSessionBuilder WebApiVersion(string webApiVersion);
 
     /// <summary>
-    /// Specifies item language.
-    /// For example: "en"
+    /// Specifies default database.
+    /// This parameter value will be used if appropriate parameter will be missed in request object.
+    /// For example: "web".
     /// 
     /// The value is case insensitive.
     /// </summary>
-    /// <param name="defaultDatabase">Database name value</param>
+    /// <param name="defaultDatabase">Database</param>
     /// <returns>
     /// this
     /// </returns>
     IBaseSessionBuilder DefaultDatabase(string defaultDatabase);
 
     /// <summary>
-    /// Specifies item language.
-    /// For example: "en"
+    /// Specifies default language.
+    /// This parameter value will be used if appropriate parameter will be missed in request object.
+    /// For example: "en".
     /// 
     /// The value is case insensitive.
     /// </summary>
-    /// <param name="defaultLanguage">Language value</param>
+    /// <param name="defaultLanguage">Language</param>
     /// <returns>
     /// this
     /// </returns>
     IBaseSessionBuilder DefaultLanguage(string defaultLanguage);
 
     /// <summary>
-    /// Specifies path to media lybrary root, '/sitecore/media library' by default.
+    /// Specifies path to media lybrary root, "/sitecore/media library" by default.
     /// </summary>
-    /// <param name="mediaLibraryRootItem">Media lybrary root.</param>
+    /// <param name="mediaLibraryRootItem">Media lybrary root</param>
     /// <returns>
     /// this
     /// </returns>
@@ -80,14 +84,14 @@ namespace Sitecore.MobileSDK.API.Session
     /// <summary>
     /// Defaults the media resource extension, "ashx" by default.
     /// </summary>
-    /// <param name="defaultExtension">The default extension for media requests.</param>
+    /// <param name="defaultExtension">The default extension.</param>
     /// <returns>
     /// this
     /// </returns>
     IBaseSessionBuilder DefaultMediaResourceExtension(string defaultExtension);
 
     /// <summary>
-    /// Prefix to build media requests, "~/media" by default.
+    /// Prefix to build request for resource download, "~/media" by default.
     /// </summary>
     /// <param name="mediaPrefix">The media prefix.</param>
     /// <returns>
--- lib/SitecoreMobileSDK-PCL/API/Session/ICreateItemActions.cs
+++ lib/SitecoreMobileSDK-PCL/API/Session/ICreateItemActions.cs
@@ -13,20 +13,20 @@ namespace Sitecore.MobileSDK.API.Session
     /// <summary>
     /// Creates item with parent GUID asynchronously.
     /// </summary>
-    /// <param name="request">The request.</param>
-    /// <param name="cancelToken">The cancel token.</param>
+    /// <param name="request"><see cref="ICreateItemByIdRequest" />The request.</param>
+    /// <param name="cancelToken">The cancel token, should be called in case when you want to terminate request execution.</param>
     /// <returns>
-    ///   <see cref="Task{TResult}" />
+    ///   <see cref="ScItemsResponse" />Created Item.
     /// </returns>
     Task<ScItemsResponse> CreateItemAsync(ICreateItemByIdRequest request, CancellationToken cancelToken = default(CancellationToken));
 
     /// <summary>
     /// Creates item with parent path asynchronously.
     /// </summary>
-    /// <param name="request">The request.</param>
-    /// <param name="cancelToken">The cancel token.</param>
+    /// <param name="request"><see cref="ICreateItemByPathRequest" />The request.</param>
+    /// <param name="cancelToken">The cancel token, should be called in case when you want to terminate request execution.</param>
     /// <returns>
-    ///   <see cref="Task{TResult}" />
+    ///   <see cref="ScItemsResponse" />Created Item.
     /// </returns>
     Task<ScItemsResponse> CreateItemAsync(ICreateItemByPathRequest request, CancellationToken cancelToken = default(CancellationToken));
   }
