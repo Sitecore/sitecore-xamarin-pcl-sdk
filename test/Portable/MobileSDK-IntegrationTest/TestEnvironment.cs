abfad717dd59638c3cc36994e8334cb46d18b040
dodikk <dodikk88.reg@gmail.com>
2014-09-25 14:35:17 +0300

    [ci] Fixed endpoint to check help desk issue

--- test/Portable/MobileSDK-IntegrationTest/TestEnvironment.cs
+++ test/Portable/MobileSDK-IntegrationTest/TestEnvironment.cs
@@ -12,7 +12,7 @@
     {
       var result = new TestEnvironment
       {
-        InstanceUrl = "http://cms72u2.test24dk1.dk.sitecore.net",
+        InstanceUrl = "http://mobiledev1ua1.dk.sitecore.net:7220",
         ShellSite = "/sitecore/shell"
       };
 
