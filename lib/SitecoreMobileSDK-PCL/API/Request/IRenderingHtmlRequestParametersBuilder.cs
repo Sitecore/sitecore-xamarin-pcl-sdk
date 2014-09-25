b94ed51ecbe7b625d3fb812d53f427d1e30aae17
Igor <igor.kh.mail@gmail.com>
2014-09-25 13:51:23 +0300

    HTML renamed to Html

---
+++ lib/SitecoreMobileSDK-PCL/API/Request/IRenderingHtmlRequestParametersBuilder.cs
@@ -0,0 +1,18 @@
+﻿namespace Sitecore.MobileSDK.API.Request.Parameters
+{
+  public interface IRenderingHtmlRequestParametersBuilder<out T>
+    where T : class
+  {
+  
+    IRenderingHtmlRequestParametersBuilder<T> SourceAndRenderingDatabase(string database);
+
+    IRenderingHtmlRequestParametersBuilder<T> SourceAndRenderingLanguage(string itemLanguage);
+
+    IRenderingHtmlRequestParametersBuilder<T> SourceVersion(int? itemVersion);
+
+    IRenderingHtmlRequestParametersBuilder<T> AddAdditionalParameterNameValue(string parameterName, string parameterValue);
+
+    T Build();
+  }
+}
+
