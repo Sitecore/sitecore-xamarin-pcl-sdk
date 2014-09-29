7faede9ab0eef07e05f0c0f8914ad3b2077bbaa4
dodikk <dodikk88.reg@gmail.com>
2014-09-29 15:57:11 +0300

    [paging] Added a field to read requests

--- lib/SitecoreMobileSDK-PCL/API/Request/IBaseReadItemsRequest.cs
+++ lib/SitecoreMobileSDK-PCL/API/Request/IBaseReadItemsRequest.cs
@@ -1,9 +1,13 @@
 ﻿namespace Sitecore.MobileSDK.API.Request
 {
+  using Sitecore.MobileSDK.API.Request.Parameters;
+
+
   /// <summary>
   /// Interface represents data set for read item requests.
   /// </summary>
   public interface IBaseReadItemsRequest : IBaseItemRequest
   {
+    IPagingParameters PagingSettings { get; }
   }
 }
