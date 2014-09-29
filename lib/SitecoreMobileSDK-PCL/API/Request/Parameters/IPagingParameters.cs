591f2d658a59dbbc278680c5893c331b89281d2d
dodikk <dodikk88.reg@gmail.com>
2014-09-29 12:28:47 +0300

    [paging] Added an interface

---
+++ lib/SitecoreMobileSDK-PCL/API/Request/Parameters/IPagingParameters.cs
@@ -0,0 +1,25 @@
+﻿namespace Sitecore.MobileSDK.API.Request.Parameters
+{
+  using System;
+
+  /// <summary>
+  /// An interface containing paging options. These options split large responses to smaller chunks.
+  /// Using them can make your UI more responsive.
+  ///  </summary>
+  public interface IPagingParameters
+  {
+    /// <summary>
+    /// An upper limit of items amount for the response.
+    /// </summary>
+    /// <returns>A positive number</returns>
+    int ItemsPerPageCount { get; }
+
+
+    /// <summary>
+    /// The number of a page to load.
+    /// </summary>
+    /// <returns>A positive number</returns>
+    int PageNumber { get; }
+  }
+}
+
