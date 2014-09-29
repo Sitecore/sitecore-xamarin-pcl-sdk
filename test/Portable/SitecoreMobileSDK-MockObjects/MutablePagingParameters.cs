7e9da35b3a81d00ae1c2d6b164396971e42e2e52
dodikk <dodikk88.reg@gmail.com>
2014-09-29 17:04:45 +0300

    [paging] Added a mutable mock

---
+++ test/Portable/SitecoreMobileSDK-MockObjects/MutablePagingParameters.cs
@@ -0,0 +1,20 @@
+﻿namespace SitecoreMobileSDKMockObjects
+{
+  using System;
+  using Sitecore.MobileSDK.API.Request.Parameters;
+
+
+
+  public class MutablePagingParameters : IPagingParameters
+  {
+    public MutablePagingParameters(int pageNumber, int perPageCount)
+    {
+      this.PageNumber = pageNumber;
+      this.ItemsPerPageCount = perPageCount;
+    }
+      
+    public int ItemsPerPageCount { get; set; }
+    public int PageNumber { get; set; }
+  }
+}
+
