0a33328c011b08072450923c882a98a08d7e7595
dodikk <dodikk88.reg@gmail.com>
2014-09-30 16:46:26 +0300

    [paging] Added IPagingParameters impl

---
+++ lib/SitecoreMobileSDK-PCL/Items/PagingParameters.cs
@@ -0,0 +1,39 @@
+﻿namespace Sitecore.MobileSDK.Items
+{
+  using System;
+  using Sitecore.MobileSDK.API.Request.Parameters;
+
+
+  internal class PagingParameters : IPagingParameters
+  {
+    public int? OptionalItemsPerPage { get; private set; }
+    public int? OptionalPageNumber { get; private set; }
+
+    public PagingParameters(int? optionalItemsPerPage, int? optionalPageNumber)
+    {
+      this.OptionalItemsPerPage = optionalItemsPerPage;
+      this.OptionalPageNumber = optionalPageNumber;
+    }
+
+    #region IPagingParameters
+    public int ItemsPerPageCount 
+    { 
+      get
+      {
+        return this.OptionalItemsPerPage.Value;
+      }
+    }
+
+    public int PageNumber 
+    { 
+      get
+      {
+        return this.OptionalPageNumber.Value;
+      }
+    }
+    #endregion
+
+
+  }
+}
+
