9dbe53d2ba5f5e265cf219761047cb4a7bdde1ec
Igor <igor.kh.mail@gmail.com>
2014-10-01 18:06:54 +0300

    upload image classes added

---
+++ lib/SitecoreMobileSDK-PCL/UrlBuilder/MediaItem/CreateMediaParameters.cs
@@ -0,0 +1,24 @@
+﻿namespace Sitecore.MobileSDK.UrlBuilder.CreateItem
+{
+  using System.Collections.Generic;
+
+  public class CreateItemParameters
+  {
+    public CreateItemParameters(string itemName, string itemTemplate, IDictionary<string, string> fieldsRawValuesByName)
+    {
+      this.ItemName = itemName;
+      this.ItemTemplate = itemTemplate;
+      this.FieldsRawValuesByName = fieldsRawValuesByName;
+    }
+
+    public CreateItemParameters ShallowCopy()
+    {
+      return new CreateItemParameters(this.ItemName, this.ItemTemplate, this.FieldsRawValuesByName);
+    }
+
+    public string ItemName { get; private set; }
+    public string ItemTemplate { get; private set; }
+    public IDictionary<string, string> FieldsRawValuesByName { get; private set; }
+  }
+}
+
