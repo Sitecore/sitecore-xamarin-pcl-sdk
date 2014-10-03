250ba2f181017e297d676550cba5abcb3afa5621
dodikk <dodikk88.reg@gmail.com>
2014-10-03 20:31:20 +0300

    [hash url] added a test for parser

---
+++ test/Portable/MobileSDK-UnitTest/HashedUrlResponseTest.cs
@@ -0,0 +1,25 @@
+﻿namespace Sitecore.MobileSdkUnitTest
+{
+  using NUnit.Framework;
+  using System;
+  using System.Threading;
+  using Sitecore.MobileSDK.CrudTasks.Resource;
+
+
+  [TestFixture]
+  public class HashedUrlResponseTest
+  {
+    [Test]
+    public void TestValidResponseFromTheRealInstance()
+    {
+      string response = "{\"statusCode\":200,\"result\":\"http://cms75.test24dk1.dk.sitecore.net/~/media/images/green_mineraly1.ashx?thn=1\\u0026hash=0F741932010066E0442017C7E37A26AF2B55FCAA\"}";
+      string result = HashedMediaUrlParser.Parse(response, default(CancellationToken));
+
+      string expected = "http://cms75.test24dk1.dk.sitecore.net/~/media/images/green_mineraly1.ashx?thn=1&hash=0F741932010066E0442017C7E37A26AF2B55FCAA";
+      Assert.AreEqual(expected, result);
+    }
+  
+
+  }
+}
+
