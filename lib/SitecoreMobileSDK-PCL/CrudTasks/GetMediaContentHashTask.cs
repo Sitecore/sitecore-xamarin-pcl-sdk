a40374093cd76e509ff8862a41a7834de7dfb31a
dodikk <dodikk88.reg@gmail.com>
2014-10-03 19:00:53 +0300

    [resize] Added a class for hash URL loading workflow

---
+++ lib/SitecoreMobileSDK-PCL/CrudTasks/GetMediaContentHashTask.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using System.Threading;
+
+namespace Sitecore.MobileSDK.CrudTasks
+{
+  using System;
+  using System.Net.Http;
+
+  using Sitecore.MobileSDK.TaskFlow;
+  using Sitecore.MobileSDK.PublicKey;
+  using Sitecore.MobileSDK.UrlBuilder.MediaItem;
+
+  using Sitecore.MobileSDK.API.Request;
+
+
+  public class GetMediaContentHashTask : IRestApiCallTasks
+  <
+    IMediaResourceDownloadRequest, 
+    HttpRequestMessage, 
+    string, // JSON response
+    string  // parsed URL
+  >
+  {
+    private HttpClient httpClient;
+    MediaItemUrlBuilder urlBuilder;
+    private ICredentialsHeadersCryptor credentialsHeadersCryptor;
+
+    public GetMediaContentHashTask(
+      MediaItemUrlBuilder urlBuilder, 
+      HttpClient httpClient, 
+      ICredentialsHeadersCryptor credentialsHeadersCryptor)
+    {
+      this.httpClient = httpClient;
+      this.urlBuilder = urlBuilder;
+      this.credentialsHeadersCryptor = credentialsHeadersCryptor;
+    }
+
+
+    public Task<HttpRequestMessage> BuildRequestUrlForRequestAsync(IMediaResourceDownloadRequest request, CancellationToken cancelToken)
+    {
+      return null;
+    }
+
+    public Task<string> SendRequestForUrlAsync(HttpRequestMessage requestUrl, CancellationToken cancelToken)
+    {
+      return null;
+    }
+
+    public Task<string> ParseResponseDataAsync(string httpData, CancellationToken cancelToken)
+    {
+      return null;
+    }
+
+  }
+}
+
