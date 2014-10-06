4335706b6ca07afb4522d95cded6596e7ccac4fd
dodikk <dodikk88.reg@gmail.com>
2014-10-06 10:33:59 +0300

    [refactoring] Extracted a class for constants

---
+++ lib/SitecoreMobileSDK-PCL/Session/TaskFlowErrorMessages.cs
@@ -0,0 +1,11 @@
+﻿namespace Sitecore.MobileSDK.Session
+{
+  using System;
+
+  internal static class TaskFlowErrorMessages
+  {
+    public const string NETWORK_EXCEPTION_MESSAGE = "[Sitecore Mobile SDK] Unable to download data from the internet";
+
+  }
+}
+
