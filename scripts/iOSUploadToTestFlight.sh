fa97d10ca163286a7828bbb2ad9b8c26418f1d5e
dodikk <dodikk88.reg@gmail.com>
2014-10-07 17:05:20 +0300

    [ci] Added build and deployment instructions

--- scripts/iOSUploadToTestFlight.sh
+++ scripts/iOSUploadToTestFlight.sh
@@ -1,55 +1,105 @@
 LAUNCH_DIR=$PWD
-SOLUTIONS_DIR="$LAUNCH_DIR/Solutions"
-RELEASE_DIR="$LAUNCH_DIR/JetStreamIOS/JetStreamIOS/bin/iPhone/Release"
-IPA_FILE="$RELEASE_DIR/JetStreamIOS.ipa"
+SCRIPTS_DIR=$LAUNCH_DIR
 
+cd ..
+REPOSITORY_ROOT=$PWD
+cd "$LAUNCH_DIR"
+
+
+DEPLOYMENT_DIR="$REPOSITORY_ROOT/deployment"
+SOLUTIONS_DIR="$REPOSITORY_ROOT/solutions"
 MDTOOL_EXE="/Applications/Xamarin Studio.app/Contents/MacOS/mdtool"
 
 
+
 echo "LAUNCH_DIR - $LAUNCH_DIR"
-echo "RELEASE_DIR - $RELEASE_DIR"
-echo "IPA_FILE - $IPA_FILE"
+echo "REPOSITORY_ROOT - $REPOSITORY_ROOT"
+echo "SCRIPTS_DIR - $SCRIPTS_DIR"
+echo "SOLUTIONS_DIR - $SOLUTIONS_DIR"
+echo "MDTOOL_EXE - $MDTOOL_EXE"
 
-function restore_nuget_packages
-{
-    cd "$SOLUTIONS_DIR"
-    nuget restore JetStreamIOS.sln
-    cd "$LAUNCH_DIR"
-}
 
-function build_application
-{
-	"$MDTOOL_EXE" -v build "--configuration:Release|iPhone" Solutions/JetStreamIOS.sln
-}
+rm -rf "$DEPLOYMENT_DIR"
+mkdir -p "$DEPLOYMENT_DIR"
 
-function make_ipa_file 
-{
-	cd $RELEASE_DIR
 
-	mkdir -p "Payload"
-	cp -r  "JetStreamIOS.app" "Payload/JetStreamIOS.app"
-	zip $IPA_FILE -r "Payload"
-	
-	cd "$LAUNCH_DIR"
-}
+## Clean temporary build data
+cd "$REPOSITORY_ROOT"
+find . \( -name "bin" -o  -name "obj" \)  -exec rm -rf {} \;
 
-function upload_ipa_to_testflight 
-{
-	cd $RELEASE_DIR
-	
+
+cd "$SOLUTIONS_DIR"
+	## Unit test and integration test
+	rm -rf "$PWD/packages"
+	nuget restore MobileSDKTest-iOS.sln
+	"$MDTOOL_EXE" --verbose build "--configuration:Release|iPhone" MobileSDKTest-iOS.sln
+
+	## White label iOS app
+	rm -rf "$PWD/packages"
+	nuget restore WhiteLabel-iOS.sln
+	"$MDTOOL_EXE" --verbose build "--configuration:Release|iPhone" WhiteLabel-iOS.sln
+cd "$LAUNCH_DIR"
+
+
+## create *.ipa
+xcrun -sdk iphoneos \
+   PackageApplication "$REPOSITORY_ROOT/test/iOS/MobileSDKUnitTest-iOS/bin/iPhone/Release/MobileSDKUnitTestiOS.app" \
+   -o "$PWD/deployment/MobileSDKUnitTestiOS.ipa"
+
+xcrun -sdk iphoneos \
+   PackageApplication "$REPOSITORY_ROOT/test/iOS/MobileSDK-IntegrationTest-iOS/bin/iPhone/Release/MobileSDKIntegrationTestiOS.app" \
+   -o "$PWD/deployment/MobileSDKIntegrationTestiOS.ipa"
+
+xcrun -sdk iphoneos \
+   PackageApplication "$REPOSITORY_ROOT/app/WhiteLabel/iOS/WhiteLabel-iOS/bin/iPhone/Release/WhiteLabeliOS.app" \
+   -o "$PWD/deployment/WhiteLabeliOS.ipa"
+
+
+
+## copy *.dsym
+cd "$REPOSITORY_ROOT/test/iOS/MobileSDKUnitTest-iOS/bin/iPhone/Release/"
+zip -r MobileSDKUnitTestiOS.app.dSYM.zip MobileSDKUnitTestiOS.app.dSYM
+cp MobileSDKUnitTestiOS.app.dSYM.zip "$DEPLOYMENT_DIR"
+
+
+cd "$REPOSITORY_ROOT/test/iOS/MobileSDK-IntegrationTest-iOS/bin/iPhone/Release/"
+zip -r MobileSDKIntegrationTestiOS.app.dSYM.zip MobileSDKIntegrationTestiOS.app.dSYM
+cp MobileSDKIntegrationTestiOS.app.dSYM.zip "$DEPLOYMENT_DIR"
+
+cd "$REPOSITORY_ROOT/app/WhiteLabel/iOS/WhiteLabel-iOS/bin/iPhone/Release/"
+zip -r WhiteLabeliOS.app.dSYM.zip WhiteLabeliOS.app.dSYM
+cp WhiteLabeliOS.app.dSYM.zip "$DEPLOYMENT_DIR"
+
+
+
+
+## Upload to Testflight
+cd "$DEPLOYMENT_DIR"
     curl http://testflightapp.com/api/builds.json \
-    -F file=@$IPA_FILE \
+    -F file=@MobileSDKUnitTestiOS.ipa \
     -F api_token='a3620a9760dc97411328bc73864c9a82_Mzk2NDIxMjAxMi0wNC0xMyAwNDowNzozMS42MDQ2NTQ' \
     -F team_token='9a4cf26c2ba6dab0ae956567f92fc0c0_ODA1NzEyMDEyLTA0LTEzIDA0OjEzOjM5LjA5OTQ0Nw' \
     -F notes='Built by Hudson' \
     -F notify=True \
     -F distribution_lists='Mobile iOS Team' \
-    -F dsym=@$DSYM_FILE
-	
-	cd "$LAUNCH_DIR"
-}
+    -F dsym=@MobileSDKUnitTestiOS.app.dSYM.zip
 
-restore_nuget_packages
-build_application
-make_ipa_file
-upload_ipa_to_testflight
+
+    curl http://testflightapp.com/api/builds.json \
+    -F file=@MobileSDKIntegrationTestiOS.ipa \
+    -F api_token='a3620a9760dc97411328bc73864c9a82_Mzk2NDIxMjAxMi0wNC0xMyAwNDowNzozMS42MDQ2NTQ' \
+    -F team_token='9a4cf26c2ba6dab0ae956567f92fc0c0_ODA1NzEyMDEyLTA0LTEzIDA0OjEzOjM5LjA5OTQ0Nw' \
+    -F notes='Built by Hudson' \
+    -F notify=True \
+    -F distribution_lists='Mobile iOS Team' \
+    -F dsym=@MobileSDKIntegrationTestiOS.app.dSYM.zip
+
+
+    curl http://testflightapp.com/api/builds.json \
+    -F file=@WhiteLabeliOS.ipa \
+    -F api_token='a3620a9760dc97411328bc73864c9a82_Mzk2NDIxMjAxMi0wNC0xMyAwNDowNzozMS42MDQ2NTQ' \
+    -F team_token='9a4cf26c2ba6dab0ae956567f92fc0c0_ODA1NzEyMDEyLTA0LTEzIDA0OjEzOjM5LjA5OTQ0Nw' \
+    -F notes='Built by Hudson' \
+    -F notify=True \
+    -F distribution_lists='Mobile iOS Team' \
+    -F dsym=@WhiteLabeliOS.app.dSYM.zip
