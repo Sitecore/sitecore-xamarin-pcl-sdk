1aea9635d1b1d0e08030bad267ce4eac22281c04
dodikk <dodikk88.reg@gmail.com>
2014-10-08 10:55:37 +0300

    [ci] Added report to socket creation

--- test/iOS/MobileSDKUnitTest-iOS/UnitTestAppDelegate.cs
+++ test/iOS/MobileSDKUnitTest-iOS/UnitTestAppDelegate.cs
@@ -5,18 +5,21 @@ using System.Linq;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 using MonoTouch.NUnit.UI;
+using System.IO;
+using System.Text;
+using MonoTouch.NUnit;
 
 namespace MobileSDKUnitTestiOS
 {
   // The UIApplicationDelegate for the application. This class is responsible for launching the
   // User Interface of the application, as well as listening (and optionally responding) to
   // application events from iOS.
-  [Register ("UnitTestAppDelegate")]
+  [Register("UnitTestAppDelegate")]
   public partial class UnitTestAppDelegate : UIApplicationDelegate
   {
     // class-level declarations
-    UIWindow window;
-    TouchRunner runner;
+    private UIWindow window;
+    private TouchRunner runner;
 
     //
     // This method is invoked when the application has loaded and is ready to run. In this
@@ -25,22 +28,45 @@ namespace MobileSDKUnitTestiOS
     //
     // You have 17 seconds to return from this method, or iOS will terminate your application.
     //
-    public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+    public override bool FinishedLaunching(UIApplication app, NSDictionary options)
     {
       // create a new window instance based on the screen size
-      window = new UIWindow (UIScreen.MainScreen.Bounds);
-      runner = new TouchRunner (window);
+      this.window = new UIWindow(UIScreen.MainScreen.Bounds);
+      this.runner = new TouchRunner(this.window);
+
+      this.ConfigureRunnerForCI();
 
 
       // register every tests included in the main application/assembly
-      runner.Add (System.Reflection.Assembly.GetExecutingAssembly ());
+      var thisAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+      this.runner.Add(thisAssembly);
 
-      window.RootViewController = new UINavigationController (runner.GetViewController ());
+
+      var viewControllerForTestRunner = this.runner.GetViewController();
+      this.window.RootViewController = new UINavigationController(viewControllerForTestRunner);
 
       // make the window visible
-      window.MakeKeyAndVisible ();
+      this.window.MakeKeyAndVisible();
 
       return true;
+    }
+
+    private void ConfigureRunnerForCI()
+    {
+      this.runner.AutoStart = true;
+      this.runner.TerminateAfterExecution = true;
+
+
+
+      var reportStream = new NUnitLite.Runner.NUnit2XmlOutputWriter();
+      var targetStreamOnBuildServer = new TcpTextWriter("darthvader.local", 16384);
+
+      runner.Writer = new NUnitOutputTextWriter(runner, targetStreamOnBuildServer, reportStream);
+
+//      string reportPath = null;
+//      bool isStreamForAppending = false;
+//      var writer = new StreamWriter(reportPath, isStreamForAppending, Encoding.UTF8);
+//      runner.Writer = writer;
     }
   }
 }
