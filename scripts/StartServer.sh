20c733bbe9ebea0dceb3bf5779b6997168b5674c
dodikk <dodikk88.reg@gmail.com>
2014-10-08 18:18:13 +0300

    [CI] Added instructions for device run

--- scripts/StartServer.sh
+++ scripts/StartServer.sh
@@ -1,23 +1,48 @@
 mono Touch.Server.exe \
   --verbose           \
   --autoexit          \
-  --port=16384        \
+  --port=16390        \
   --launchsim="/Volumes/untitled/tfs/Mobile.SDK.dotNET/test/iOS/MobileSDKUnitTest-iOS/bin/iPhoneSimulator/Debug/MobileSDKUnitTestiOS.app" \
   --logfile=UnitTestReport.xml
 ##############
 
 
-/Developer/MonoTouch/usr/bin/mtouch --installdev "/Volumes/untitled/tfs/Mobile.SDK.dotNET/test/iOS/MobileSDKUnitTest-iOS/bin/iPhone/Debug/MobileSDKUnitTestiOS.app"
-/Developer/MonoTouch/usr/bin/mtouch --killdev "net.sitecore.MobileSDKUnitTestiOS"
+##devname does not work properly for Touch.Server.exe
+
+
+/Developer/MonoTouch/usr/bin/mtouch \
+  --devname="2" \
+  --installdev "/Volumes/untitled/tfs/Mobile.SDK.dotNET/test/iOS/MobileSDKUnitTest-iOS/bin/iPhone/Debug/MobileSDKUnitTestiOS.app" 
+
+
+/Developer/MonoTouch/usr/bin/mtouch \
+  --devname="2" \
+  --killdev "net.sitecore.MobileSDKUnitTestiOS"
 
 mono Touch.Server.exe \
   --verbose           \
   --autoexit          \
-  --port=16388        \
+  --port=16394        \
   --launchdev="net.sitecore.MobileSDKUnitTestiOS" \
+  --devname="2" \
+  --logfile=UnitTestReport-Device-iOS8.xml
+##############
+
+
+/Developer/MonoTouch/usr/bin/mtouch \
+  --devname="1" \
+  --installdev "/Volumes/untitled/tfs/Mobile.SDK.dotNET/test/iOS/MobileSDKUnitTest-iOS/bin/iPhone/Debug/MobileSDKUnitTestiOS.app" 
+
+
+/Developer/MonoTouch/usr/bin/mtouch \
+  --devname="1" \
+  --killdev "net.sitecore.MobileSDKUnitTestiOS"
+
+mono Touch.Server.exe \
+  --verbose           \
+  --autoexit          \
+  --port=16394        \
+  --launchdev="net.sitecore.MobileSDKUnitTestiOS" \
+  --devname="1" \
   --logfile=UnitTestReport-Device.xml
-
-
-  --device=""
-
-https://evolve.xamarin.com/live
+##############
