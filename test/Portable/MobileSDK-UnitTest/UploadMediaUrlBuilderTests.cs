d7c01ce90fdfecd54a1c2e83ac4cf46d1bee103f
Igor <igor.kh.mail@gmail.com>
2014-10-09 16:25:19 +0300

    test added

--- test/Portable/MobileSDK-UnitTest/UploadMediaUrlBuilderTests.cs
+++ test/Portable/MobileSDK-UnitTest/UploadMediaUrlBuilderTests.cs
@@ -1,5 +1,4 @@
 ﻿
-
 namespace Sitecore.MobileSdkUnitTest
 {
   using System;
@@ -13,20 +12,18 @@ namespace Sitecore.MobileSdkUnitTest
   using Sitecore.MobileSDK.API.Request;
   using Sitecore.MobileSDK.API.Request.Parameters;
 
-  using Sitecore.MobileSDK.UrlBuilder.UpdateItem;
   using Sitecore.MobileSDK.SessionSettings;
-  using Sitecore.MobileSDK.UrlBuilder.QueryParameters;
   using Sitecore.MobileSDK.UserRequest;
+  using Sitecore.MobileSDK.Items;
+  using Sitecore.MobileSDK.UrlBuilder.MediaItem;
   using Sitecore.MobileSDK.UrlBuilder.Rest;
   using Sitecore.MobileSDK.UrlBuilder.WebApi;
-  using Sitecore.MobileSDK.Items;
-
 
 
   [TestFixture]
-  public class UpdateItemByIdUrlBuilderTests
+  public class UploadMediaUrlBuilderTests
   {
-    private UpdateItemByIdUrlBuilder builder;
+    private UploadMediaUrlBuilder builder;
     private UserRequestMerger requestMerger;
 
     [SetUp]
@@ -35,7 +32,7 @@ namespace Sitecore.MobileSdkUnitTest
       IRestServiceGrammar restGrammar = RestServiceGrammar.ItemWebApiV2Grammar();
       IWebApiUrlParameters webApiGrammar = WebApiUrlParameters.ItemWebApiV2UrlParameters();
 
-      this.builder = new UpdateItemByIdUrlBuilder(restGrammar, webApiGrammar);
+      this.builder = new UploadMediaUrlBuilder(restGrammar, webApiGrammar);
 
       SessionConfigPOD mutableSessionConfig = new SessionConfigPOD();
       mutableSessionConfig.ItemWebApiVersion = "v234";
@@ -54,10 +51,10 @@ namespace Sitecore.MobileSdkUnitTest
     }
       
     [Test]
-    public void TestCorrectParamsWithoutFields()
+    public void TestCorrectParams()
     {
-      IUpdateItemByIdRequest request = ItemWebApiRequestBuilder.UpdateItemRequestWithId("{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}")
-        .Build();
+      Stream stream = new Stream
+      IUpdateItemByIdRequest request = ItemWebApiRequestBuilder.UploadResourceRequest()
 
       IUpdateItemByIdRequest autocompletedRequest = this.requestMerger.FillUpdateItemByIdGaps (request);
 
@@ -74,129 +71,7 @@ namespace Sitecore.MobileSdkUnitTest
       Assert.AreEqual(expectedFieldsResult, fieldsResult);
     }
 
-    [Test]
-    public void TestCorrectParamsWithFields()
-    {
-      IUpdateItemByIdRequest request = ItemWebApiRequestBuilder.UpdateItemRequestWithId("{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}")
-        .Version(1)
-        .AddFieldsRawValuesByNameToSet("field1","value1")
-        .AddFieldsRawValuesByNameToSet("field2","value2")
-        .Build();
 
-      IUpdateItemByIdRequest autocompletedRequest = this.requestMerger.FillUpdateItemByIdGaps (request);
 
-      string result = this.builder.GetUrlForRequest(autocompletedRequest);
-      string expected = "http://mobiledev1ua1.dk.sitecore.net:7119/-/item/v234%2fsitecore%2fshell"+
-        "?sc_database=web"+
-        "&language=en"+
-        "&sc_itemversion=1"+
-        "&sc_itemid=%7b110d559f-dea5-42ea-9c1c-8a5df7e70ef9%7d";
-
-      string fieldsResult = this.builder.GetFieldValuesList(autocompletedRequest);
-      string expectedFieldsResult = "field1=value1&field2=value2";
-
-      Assert.AreEqual(expected, result);
-      Assert.AreEqual(expectedFieldsResult, fieldsResult);
-    }
-
-    [Test]
-    public void TestFieldsValuesIsCaseInsensitive()
-    {
-      IUpdateItemByIdRequest request = ItemWebApiRequestBuilder.UpdateItemRequestWithId("{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}")
-        .AddFieldsRawValuesByNameToSet("field1","VaLuE1")
-        .AddFieldsRawValuesByNameToSet("field2","VaLuE2")
-        .Build();
-
-      IUpdateItemByIdRequest autocompletedRequest = this.requestMerger.FillUpdateItemByIdGaps(request);
-
-      string result = this.builder.GetUrlForRequest(autocompletedRequest);
-      string expected = "http://mobiledev1ua1.dk.sitecore.net:7119/-/item/v234%2fsitecore%2fshell"+
-        "?sc_database=web"+
-        "&language=en"+
-        "&sc_itemid=%7b110d559f-dea5-42ea-9c1c-8a5df7e70ef9%7d";
-
-      string fieldsResult = this.builder.GetFieldValuesList(autocompletedRequest);
-      string expectedFieldsResult = "field1=VaLuE1&field2=VaLuE2";
-
-      Assert.AreEqual(expected, result);
-      Assert.AreEqual(expectedFieldsResult, fieldsResult);
-    }
-
-    [Test]
-    public void TestItemNameIsCaseInsensitive()
-    {
-      IUpdateItemByIdRequest request = ItemWebApiRequestBuilder.UpdateItemRequestWithId("{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}")
-        .AddFieldsRawValuesByNameToSet("field1","VaLuE1")
-        .AddFieldsRawValuesByNameToSet("field2","VaLuE2")
-        .Build();
-
-      IUpdateItemByIdRequest autocompletedRequest = this.requestMerger.FillUpdateItemByIdGaps (request);
-
-      string result = this.builder.GetUrlForRequest(autocompletedRequest);
-      string expected = "http://mobiledev1ua1.dk.sitecore.net:7119/-/item/v234%2fsitecore%2fshell"+
-        "?sc_database=web"+
-        "&language=en"+
-        "&sc_itemid=%7b110d559f-dea5-42ea-9c1c-8a5df7e70ef9%7d";
-
-      string fieldsResult = this.builder.GetFieldValuesList(autocompletedRequest);
-      string expectedFieldsResult = "field1=VaLuE1&field2=VaLuE2";
-
-      Assert.AreEqual(expected, result);
-      Assert.AreEqual(expectedFieldsResult, fieldsResult);
-    }
-
-    [Test]
-    public void TestFieldWithDuplicatedKeyWillCrash()
-    {
-      var requestBuilder = ItemWebApiRequestBuilder.UpdateItemRequestWithId("{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}")
-        .AddFieldsRawValuesByNameToSet("field1", "VaLuE1")
-        .AddFieldsRawValuesByNameToSet("field2", "VaLuE2");
-        
-      TestDelegate action = () => requestBuilder.AddFieldsRawValuesByNameToSet("field1","VaLuE3");
-      Assert.Throws<InvalidOperationException>(action);
-    }
-
-    [Test]
-    public void TestAppendingFields()
-    {
-      var requestBuilder = ItemWebApiRequestBuilder.UpdateItemRequestWithId("{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}")
-        .AddFieldsRawValuesByNameToSet("field1", "VaLuE1")
-        .AddFieldsRawValuesByNameToSet("field2", "VaLuE2");
-
-      TestDelegate action = () => requestBuilder.AddFieldsRawValuesByNameToSet("field1","VaLuE3");
-      Assert.Throws<InvalidOperationException>(action);
-    }
-
-    [Test]
-    public void TestFieldsAppending()
-    {
-      Dictionary<string,string> fields = new Dictionary<string,string>();
-      fields.Add("field1","VaLuE1");
-      fields.Add("field2","VaLuE2");
-
-      IUpdateItemByIdRequest request = ItemWebApiRequestBuilder.UpdateItemRequestWithId("{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}")
-        .Database("db")
-        .Language("lg")
-        .Payload(PayloadType.Full)
-        .AddFieldsRawValuesByNameToSet(fields)
-        .AddFieldsRawValuesByNameToSet("field3","VaLuE3")
-        .Build();
-
-      IUpdateItemByIdRequest autocompletedRequest = this.requestMerger.FillUpdateItemByIdGaps (request);
-
-      string result = this.builder.GetUrlForRequest(autocompletedRequest);
-      string expected = 
-        "http://mobiledev1ua1.dk.sitecore.net:7119/-/item/v234%2fsitecore%2fshell"+
-        "?sc_database=db"+
-        "&language=lg"+
-        "&payload=full"+
-        "&sc_itemid=%7b110d559f-dea5-42ea-9c1c-8a5df7e70ef9%7d";
-
-      string fieldsResult = this.builder.GetFieldValuesList(autocompletedRequest);
-      string expectedFieldsResult = "field1=VaLuE1&field2=VaLuE2&field3=VaLuE3";
-
-      Assert.AreEqual(expected, result);
-      Assert.AreEqual(expectedFieldsResult, fieldsResult);
-    }
   }
 }
