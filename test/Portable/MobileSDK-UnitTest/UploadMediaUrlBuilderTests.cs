b580190f8488ad3aa765a46b2c9c98ed906c9254
Igor <igor.kh.mail@gmail.com>
2014-10-09 20:00:09 +0300

    tests added

--- test/Portable/MobileSDK-UnitTest/UploadMediaUrlBuilderTests.cs
+++ test/Portable/MobileSDK-UnitTest/UploadMediaUrlBuilderTests.cs
@@ -3,6 +3,8 @@ namespace Sitecore.MobileSdkUnitTest
 {
   using System;
   using System.Collections.Generic;
+  using System.IO;
+
   using NUnit.Framework;
 
   using MobileSDKUnitTest.Mock;
@@ -18,7 +20,7 @@ namespace Sitecore.MobileSdkUnitTest
   using Sitecore.MobileSDK.UrlBuilder.MediaItem;
   using Sitecore.MobileSDK.UrlBuilder.Rest;
   using Sitecore.MobileSDK.UrlBuilder.WebApi;
-
+  using Sitecore.MobileSDK.API.MediaItem;
 
   [TestFixture]
   public class UploadMediaUrlBuilderTests
@@ -32,8 +34,6 @@ namespace Sitecore.MobileSdkUnitTest
       IRestServiceGrammar restGrammar = RestServiceGrammar.ItemWebApiV2Grammar();
       IWebApiUrlParameters webApiGrammar = WebApiUrlParameters.ItemWebApiV2UrlParameters();
 
-      this.builder = new UploadMediaUrlBuilder(restGrammar, webApiGrammar);
-
       SessionConfigPOD mutableSessionConfig = new SessionConfigPOD();
       mutableSessionConfig.ItemWebApiVersion = "v234";
       mutableSessionConfig.InstanceUrl = "mobiledev1ua1.dk.sitecore.net:7119";
@@ -42,6 +42,9 @@ namespace Sitecore.MobileSdkUnitTest
       ItemSource source = LegacyConstants.DefaultSource();
       this.requestMerger = new UserRequestMerger(mutableSessionConfig, source);
 
+      IMediaLibrarySettings mediaSettings = mutableSessionConfig;
+
+      this.builder = new UploadMediaUrlBuilder (restGrammar, webApiGrammar, mutableSessionConfig, mediaSettings);
     }
 
     [TearDown]
@@ -51,27 +54,169 @@ namespace Sitecore.MobileSdkUnitTest
     }
       
     [Test]
-    public void TestCorrectParams()
+    public void TestCorrectParamsForParentId()
     {
-      Stream stream = new Stream
-      IUpdateItemByIdRequest request = ItemWebApiRequestBuilder.UploadResourceRequest()
+      Stream stream = this.GenerateFakeStream();
+      IMediaResourceUploadRequest request = ItemWebApiRequestBuilder.UploadResourceRequestWithParentId("{BC1BAE61-ADC6-4B37-B36E-01059B26CF84}")
+        .ContentType("image/jpg")
+        .ItemName("name1")
+        .FileName("bugaga.jpg")
+        .ItemTemplatePath("System/Media/Unversioned/Image")
+        .ItemDataStream(stream)
+        .Build();
 
-      IUpdateItemByIdRequest autocompletedRequest = this.requestMerger.FillUpdateItemByIdGaps (request);
+      IMediaResourceUploadRequest autocompletedRequest = this.requestMerger.FillUploadMediaGaps(request);
 
       string result = this.builder.GetUrlForRequest(autocompletedRequest);
-      string expected = "http://mobiledev1ua1.dk.sitecore.net:7119/-/item/v234%2fsitecore%2fshell"+
-        "?sc_database=web"+
-        "&language=en"+
-        "&sc_itemid=%7b110d559f-dea5-42ea-9c1c-8a5df7e70ef9%7d";
-
-      string fieldsResult = this.builder.GetFieldValuesList(autocompletedRequest);
-      string expectedFieldsResult = "";
+      string expected = "http://mobiledev1ua1.dk.sitecore.net:7119/" +
+        "-/item/v234%2f" +
+        "sitecore%2fshell" +
+        "?" +
+        "sc_database=web&" +
+        "language=en&" +
+        "name=name1&" +
+        "template=System%2fMedia%2fUnversioned%2fImage&" +
+        "sc_itemid=%7BBC1BAE61-ADC6-4B37-B36E-01059B26CF84%7D";
 
       Assert.AreEqual(expected, result);
-      Assert.AreEqual(expectedFieldsResult, fieldsResult);
     }
 
+    [Test]
+    public void TestEmptyParentId()
+    {
+      Assert.Throws<ArgumentException>(() => ItemWebApiRequestBuilder.UploadResourceRequestWithParentId (""));
+    }
 
+    [Test]
+    public void TestNullParentId()
+    {
+      Assert.Throws<ArgumentNullException>(() => ItemWebApiRequestBuilder.UploadResourceRequestWithParentId (null));
+    }
+   
+    [Test]
+    public void TestEmptyParentPath()
+    {
+      var builder = ItemWebApiRequestBuilder.UploadResourceRequestWithParentPath("");
+      Assert.NotNull (builder, "empty path should be available");
+    }
 
+    [Test]
+    public void TestNullParentPath()
+    {
+      var builder = ItemWebApiRequestBuilder.UploadResourceRequestWithParentPath(null);
+      Assert.NotNull (builder, "null path should be available");
+    }
+
+    [Test]
+    public void TestItemDataStreamRequired()
+    {
+      var builder = ItemWebApiRequestBuilder.UploadResourceRequestWithParentPath("/some folder/path")
+        .ContentType("image/jpg")
+        .ItemName("name1")
+        .FileName("bugaga.jpg")
+        .ItemTemplatePath("System/Media/Unversioned/Image");
+
+      Assert.Throws<ArgumentNullException>(() => builder.Build());
+    }
+
+    [Test]
+    public void TestFileNameRequired()
+    {
+      Stream stream = this.GenerateFakeStream();
+      var builder = ItemWebApiRequestBuilder.UploadResourceRequestWithParentPath ("/some folder/path")
+        .ContentType ("image/jpg")
+        .ItemName ("name1")
+        .ItemDataStream (stream);
+
+      Assert.Throws<ArgumentNullException>(() => builder.Build());
+    }
+
+    [Test]
+    public void TestItemNameRequired()
+    {
+      Stream stream = this.GenerateFakeStream();
+      var builder = ItemWebApiRequestBuilder.UploadResourceRequestWithParentPath ("/some folder/path")
+        .ContentType ("image/jpg")
+        .FileName("bugaga.jpg")
+        .ItemDataStream (stream);
+
+      Assert.Throws<ArgumentNullException>(() => builder.Build());
+    }
+
+    [Test]
+    public void TestContentTypeRequired()
+    {
+      Stream stream = this.GenerateFakeStream();
+      var builder = ItemWebApiRequestBuilder.UploadResourceRequestWithParentPath ("/some folder/path")
+        .ItemName("name1")
+        .FileName("bugaga.jpg")
+        .ItemDataStream (stream);
+
+      Assert.Throws<ArgumentNullException>(() => builder.Build());
+    }
+
+    [Test]
+    public void TestCorrectParamsForParentPath()
+    {
+      Stream stream = this.GenerateFakeStream();
+      IMediaResourceUploadRequest request = ItemWebApiRequestBuilder.UploadResourceRequestWithParentPath("/some folder/path")
+        .ContentType("image/jpg")
+        .ItemName("name1")
+        .FileName("bugaga.jpg")
+        .ItemTemplatePath("System/Media/Unversioned/Image")
+        .ItemDataStream(stream)
+        .Build();
+
+      IMediaResourceUploadRequest autocompletedRequest = this.requestMerger.FillUploadMediaGaps(request);
+
+      string result = this.builder.GetUrlForRequest(autocompletedRequest);
+      string expected = "http://mobiledev1ua1.dk.sitecore.net:7119/" +
+                        "-/item/v234%2f" +
+                        "sitecore%2fshell" +
+                        "%2fsome%20folder%2fpath" +
+                        "?" +
+                        "sc_database=web&" +
+                        "language=en&" +
+                        "name=name1&" +
+                        "template=System%2fMedia%2fUnversioned%2fImage";
+
+      Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void TestTemplateIsOptionalParams()
+    {
+      Stream stream = this.GenerateFakeStream();
+      IMediaResourceUploadRequest request = ItemWebApiRequestBuilder.UploadResourceRequestWithParentPath("/some folder/path")
+        .ContentType("image/jpg")
+        .ItemName("name1")
+        .FileName("bugaga.jpg")
+        .ItemDataStream(stream)
+        .Build();
+
+      IMediaResourceUploadRequest autocompletedRequest = this.requestMerger.FillUploadMediaGaps(request);
+
+      string result = this.builder.GetUrlForRequest(autocompletedRequest);
+      string expected = "http://mobiledev1ua1.dk.sitecore.net:7119/" +
+        "-/item/v234%2f" +
+        "sitecore%2fshell" +
+        "%2fsome%20folder%2fpath" +
+        "?" +
+        "sc_database=web&" +
+        "language=en&" +
+        "name=name1";
+
+      Assert.AreEqual(expected, result);
+    }
+
+    public Stream GenerateFakeStream()
+    {
+      MemoryStream stream = new MemoryStream();
+      StreamWriter writer = new StreamWriter(stream);
+      writer.Write("bla");
+      writer.Flush();
+      stream.Position = 0;
+      return stream;
+    }
   }
 }
