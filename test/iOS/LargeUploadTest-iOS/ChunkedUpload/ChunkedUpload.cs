163615181db7db1d785a4b135b3cdbeca1a24e02
Igor <igor.kh.mail@gmail.com>
2014-10-23 15:18:41 +0300

    classes for chunked upload

---
+++ test/iOS/LargeUploadTest-iOS/ChunkedUpload/ChunkedUpload.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LargeUploadTestiOS
+{
+  public class ChunkedUpload
+  {
+    public ChunkedUpload()
+    {
+    }
+
+    //make async
+    public void UploadResource(IChunkedRequest request)
+    {
+
+    }
+  }
+}
+
---
+++ test/iOS/LargeUploadTest-iOS/ChunkedUpload/IChunkedRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace LargeUploadTestiOS
+{
+  public interface IChunkedRequest
+  {
+    /// <summary>
+    /// Specifies media resource data.
+    /// </summary>
+    /// <returns>Data Stream</returns>
+    Stream DataStream { get; }
+
+    /// <summary>
+    /// Specifies file's name, must include file extension.
+    /// For example: image.jpg.
+    /// </summary>
+    /// <returns>File name</returns>
+    string FileName { get; }
+
+    /// <summary>
+    /// Specifies items's name.
+    /// For example: image.
+    /// </summary>
+    /// <returns>Item name</returns>
+    string ItemName { get; }
+
+    /// <summary>
+    /// Sepcifies the media item's file content type.
+    /// For example: image/jpg
+    /// </summary>
+    /// <returns>Content type.</returns>
+    string ContentType { get; }
+
+    string RequestUrl { get; }
+  }
+}
+
