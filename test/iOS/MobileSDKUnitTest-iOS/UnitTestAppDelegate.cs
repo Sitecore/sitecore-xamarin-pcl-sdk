5548a6a21aa6701d23c66600c7ba486fe3d0d7c5
dodikk <dodikk88.reg@gmail.com>
2014-10-28 10:30:03 +0200

    [CI] Added a flag to prevent the device from sleeping during the test run

--- test/iOS/MobileSDKUnitTest-iOS/UnitTestAppDelegate.cs
+++ test/iOS/MobileSDKUnitTest-iOS/UnitTestAppDelegate.cs
@@ -31,6 +31,8 @@
     //
     public override bool FinishedLaunching(UIApplication app, NSDictionary options)
     {
+      app.IdleTimerDisabled = true;
+
       // create a new window instance based on the screen size
       this.window = new UIWindow(UIScreen.MainScreen.Bounds);
       this.runner = new TouchRunner(this.window);
