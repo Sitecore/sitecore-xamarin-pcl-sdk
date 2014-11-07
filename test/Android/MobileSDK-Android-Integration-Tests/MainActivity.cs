3055cce494428e651d044a5e28d3026114e19bfd
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2014-11-07 12:15:30 +0200

    [tests] Updated Integration tests for android.

--- test/Android/MobileSDK-Android-Integration-Tests/MainActivity.cs
+++ test/Android/MobileSDK-Android-Integration-Tests/MainActivity.cs
@@ -1,46 +1,10 @@
 ﻿namespace MobileSDKAndroidTests
 {
-  using System;
-  using System.IO;
   using Android.App;
-  using Android.Util;
-  using MobileSDKAndroidIntegrationTests;
 
   [Activity(Label = "MobileSDK-Android-Tests", MainLauncher = true)]
   public class MainActivity : ConfigurableTestActivity
   {
-    private const string TouchServerHost = "10.45.110.119";
-    private const int TouchServerPort = 55555;
-
-    protected override bool IsAutomated
-    {
-      get
-      {
-        return true;
-      }
-    }
-
-    protected override TextWriter TargetTextWriter
-    {
-      get
-      {
-        var message = string.Format("[{0}] Sending results to {1}:{2}", DateTime.Now, TouchServerHost, TouchServerPort);
-
-        Log.Debug(this.GetType().Name, message);
-        try
-        {
-          return new TcpTextWriter(hostName: TouchServerHost, port: TouchServerPort);
-        }
-        catch (System.Exception exception)
-        {
-          var debugMessage = string.Format("Failed to connect to {0}:{1}.\n{2}", TouchServerHost, TouchServerPort,
-            exception);
-
-          Log.Debug(GetType().Name, debugMessage);
-          throw;
-        }
-      }
-    }
   }
 }
 
