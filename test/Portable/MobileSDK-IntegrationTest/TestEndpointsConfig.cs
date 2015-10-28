1692b347d7b38670e302d035074f00c065812c30
Igor <igor.kh.mail@gmail.com>
2015-10-28 17:39:20 +0200

    additional hosts for testing

--- test/Portable/MobileSDK-IntegrationTest/TestEndpointsConfig.cs
+++ test/Portable/MobileSDK-IntegrationTest/TestEndpointsConfig.cs
@@ -2,7 +2,12 @@
 {
   public class TestEndpointsConfig
   {
-    public const string InstanceUrl = "http://cms80full.build.test24dk1.dk.sitecore.net";
+//    public const string InstanceUrl = "http://cms75release.test24dk1.dk.sitecore.net/";
+//    public const string InstanceUrl = "http://cms80full.build.test24dk1.dk.sitecore.net";
+//    public const string InstanceUrl = "http://cms80u1.test24dk1.dk.sitecore.net/";
+    public const string InstanceUrl = "http://sc81151003.test24dk1.dk.sitecore.net/";
+//    public const string InstanceUrl = "http://cms80u5.test24dk1.dk.sitecore.net/";
+//    public const string InstanceUrl = "http://cms75u5.test24dk1.dk.sitecore.net/";
     public const bool IsMediaHashesSupported = true;
   }
 }
