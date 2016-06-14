2c82854998b3511a6f23f1db42826dea62a5604e
Igor Khomich <igk@sitecore.net>
2016-06-14 15:17:52 +0300

    test instance url changed

--- test/Portable/MobileSDK-IntegrationTest/TestEndpointsConfig.cs
+++ test/Portable/MobileSDK-IntegrationTest/TestEndpointsConfig.cs
@@ -2,7 +2,7 @@
 {
   public class TestEndpointsConfig
   {
-    public const string InstanceUrl = "http://cms80.test24dk1.dk.sitecore.net";
+    public const string InstanceUrl = "http://cms80u2.test24dk1.dk.sitecore.net";
     public const bool IsMediaHashesSupported = true;
   }
 }
